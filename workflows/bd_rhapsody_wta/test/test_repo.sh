90fb103ffd9a3ad5b82f198895d7eaac551d9b4d
rcannood <rcannood@users.noreply.github.com>
2021-06-01 09:48:14 +0000

    deploy: 744b5a7a78ebd2307ade11ffb7287c820d533945

---
+++ workflows/bd_rhapsody_wta/test/test_repo.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# get the root of the directory
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+# collection of commands for testing workflows in repos
+
+export NXF_VER=21.04.1
+
+PAR_GENOME="http://bd-rhapsody-public.s3.amazonaws.com/Rhapsody-WTA/GRCh38-PhiX-gencodev29/GRCh38-PhiX-gencodev29-20181205.tar.gz"
+PAR_TRANSCRIPTOME="http://bd-rhapsody-public.s3.amazonaws.com/Rhapsody-WTA/GRCh38-PhiX-gencodev29/gencodev29-20181205.gtf"
+PAR_INPUTS="http://bd-rhapsody-public.s3.amazonaws.com/Rhapsody-WTA-test-data/sample_R1_.fastq.gz;http://bd-rhapsody-public.s3.amazonaws.com/Rhapsody-WTA-test-data/sample_R2_.fastq.gz"
+
+nextflow drop https://github.com/openpipeline-bio/openpipeline.git
+
+nextflow \
+  run https://github.com/openpipeline-bio/openpipeline.git \
+  -r target_main \
+  -main-script workflows/bd_rhapsody_wta/main.nf \
+  -entry bd_rhapsody_wta_wf \
+  --id "sample_RSEC" \
+  --input "$PAR_INPUTS" \
+  --reference_genome "$PAR_GENOME" \
+  --transcriptome_annotation "$PAR_TRANSCRIPTOME" \
+  --output output/ \
+  -resume
+  
+nextflow \
+  run . \
+  -main-script workflows/bd_rhapsody_wta/main.nf \
+  -entry bd_rhapsody_wta_wf \
+  --id "sample_RSEC" \
+  --input "$PAR_INPUTS" \
+  --reference_genome "$PAR_GENOME" \
+  --transcriptome_annotation "$PAR_TRANSCRIPTOME" \
+  --output output/ \
+  -resume
+  
+  
+nextflow \
+  run https://github.com/openpipeline-bio/openpipeline.git \
+  -r target_main \
+  -latest \
+  -main-script workflows/debug.nf
+  
+nextflow \
+  run . \
+  -latest \
+  -main-script workflows/debug.nf
