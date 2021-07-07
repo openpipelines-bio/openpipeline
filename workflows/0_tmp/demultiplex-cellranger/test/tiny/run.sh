7f6370e656a76f8876d3894200bfbd44534ca4d9
rcannood <rcannood@users.noreply.github.com>
2021-07-07 09:37:45 +0000

    deploy: 9988c0b50d006e714489766b1f175eca5f609d69

---
+++ workflows/0_tmp/demultiplex-cellranger/test/tiny/run.sh
@@ -0,0 +1,14 @@
+rm -rf out
+mkdir out
+mkdir -p work/bcl
+tar -xvzf "$(pwd)/resources/test/cellranger-tiny-bcl-1.2.0/cellranger-tiny-bcl-1.2.0.tar.gz" -C "$(pwd)/work/bcl"
+
+NXF_VER=20.12.1-edge ./bin/nextflow run ./workflows/demultiplex-cellranger/demultiplex-cellranger.nf \
+  --input "$(pwd)/work/bcl/cellranger-tiny-bcl-1.2.0/" \
+  --samplesheet "$(pwd)/resources/test/cellranger-tiny-bcl-1.2.0/cellranger-tiny-bcl-simple-1.2.0.csv" \
+  --output "$(pwd)/out" \
+  -resume  \
+  --mkfastq__memory "auto" \
+  --mkfastq__cores "auto"     -c local_nxf.config              
+
+./target/docker/test/integration_test/integration_test  --test "./workflows/demultiplex-cellranger/test/tiny/test.py" --input "$(pwd)/out" --output "$(pwd)/out" 
