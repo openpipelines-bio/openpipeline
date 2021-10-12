be9ecf6f7e6ab3537e384b28e38cb2e2cb327971
Dries De Maeyer <ddemaey1@its.jnj.com>
2021-10-12 08:56:45 +0200

    Update design docs

--- docs/generate-docs.sh
+++ docs/generate-docs.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+
 ../bin/viash build ../src/report/mermaid/config.vsh.yaml -o ../target/report/mermaid/
 
-../target/report/mermaid/mermaid -i pipelines-target-p1.mermaid -o pipelines-target-p1.png
-../target/report/mermaid/mermaid -i pipelines-target-p2.mermaid -o pipelines-target-p2.png
-../target/report/mermaid/mermaid -i pipelines-target-p3.mermaid -o pipelines-target-p3.png
+rm -rf figures
+mkdir figures
+
+for figure in $(ls -1 ./mermaid/ | grep .mermaid | sed 's/\.[^.]*$//'); do
+    echo "Generating mermaid figure ${figure}"
+    ../target/report/mermaid/mermaid -i ./mermaid/${figure}.mermaid -o ./figures/${figure}.png
+done
