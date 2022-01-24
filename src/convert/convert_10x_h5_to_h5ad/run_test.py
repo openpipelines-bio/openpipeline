2d3ada09e0331de25f9f834f1de12bf6864f9b88
Dries De Maeyer <ddemaeyer@gmail.com>
2022-01-24 08:34:41 +0100

    Redesign h5toh5ad into convert_10x_h5_to_h5ad

--- src/convert/convert_10x_h5_to_h5ad/run_test.py
+++ src/convert/convert_10x_h5_to_h5ad/run_test.py
@@ -9,16 +9,15 @@ import numpy as np
 class TestH5toH5AD(unittest.TestCase):
     def test_simple_conversion(self):
         out = subprocess.check_output([
-              "./h5toh5ad", "--input", "pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5",  "--output=output-py1.h5ad"]
+              "./convert_10x_h5_to_h5ad", 
+              "--input=pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5",  
+              "--output=output-py1-rna.h5ad"]
             ).decode("utf-8")
        
-        self.assertTrue(path.exists("output-py1.h5ad"))
+        self.assertTrue(path.exists("output-py1-rna.h5ad"))
         
-        data = sc.read_h5ad("output-py1.h5ad")
+        data = sc.read_h5ad("output-py1-rna.h5ad")
 
         self.assertEqual(set(["Gene Expression"]), set(data.var["feature_types"].unique()))
-
-        self.assertTrue("counts_antibody" in data.obsm)
-        self.assertTrue("CD3_TotalSeqB" in data.obsm["counts_antibody"].columns)
 
 unittest.main()
---
+++ src/convert/convert_10x_h5_to_h5ad/script.py
@@ -0,0 +1,26 @@
+### VIASH START
+
+par = {
+	"input": "./test/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5",
+	"output": "./test/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5ad"
+}
+### VIASH END
+
+
+import argparse
+import scanpy as sc
+
+
+print("Converting " + par["input"] + " to " + par["output"])
+
+data = sc.read_10x_h5(par["input"], gex_only = False)       
+
+d = data[:, data.var["feature_types"] == par["modality"]]
+
+try: 
+	d.var_names_make_unique()
+except:
+	pass
+d.raw = d
+
+d.write(par["output"], compression = par["compression"])
