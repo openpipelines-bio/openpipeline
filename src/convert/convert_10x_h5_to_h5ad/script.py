dbb9e4de98ed786e1ae79d4797a7e220ce649df8
Robrecht Cannoodt <rcannood@gmail.com>
2022-02-01 11:52:46 +0100

    clean up 10xh5 to h5ad

--- src/convert/convert_10x_h5_to_h5ad/script.py
+++ src/convert/convert_10x_h5_to_h5ad/script.py
@@ -1,24 +1,24 @@
-
+import scanpy as sc
 
 ### VIASH START
-
 par = {
 	"input": "./test/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5",
-	"output": "./test/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5ad"
+	"output": "./test/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5ad",
+	"modality": "Gene Expression",
+	"compression": "gzip",
 }
 ### VIASH END
 
-import scanpy as sc
+data = sc.read_10x_h5(par["input"], gex_only=False)       
 
-data = sc.read_10x_h5(par["input"], gex_only = False)       
 
-d = data[:, data.var["feature_types"] == par["modality"]]
+out = data[:, data.var["feature_types"] == par["modality"]]
 
 try: 
-	d.var_names_make_unique()
+	out.var_names_make_unique()
 except:
 	pass
 
-d.raw = d
+out.raw = out
 
-d.write(par["output"], compression = par["compression"])
+out.write_h5ad(par["output"], compression=par["compression"])
