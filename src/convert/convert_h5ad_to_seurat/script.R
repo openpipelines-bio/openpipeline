d738bacfb3a737a76e7c19937f54a9c4a131997b
Robrecht Cannoodt <rcannood@gmail.com>
2022-02-01 11:49:52 +0100

    clean up component

--- src/convert/convert_h5ad_to_seurat/script.R
+++ src/convert/convert_h5ad_to_seurat/script.R
@@ -1,13 +1,12 @@
+library(SeuratDisk)
+
 ### VIASH START
 par <- list(
   input = "test/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5ad",
-  output = "output.rds"
+  output = "output.rds",
+  verbose = TRUE
 )
 ### VIASH END
-
-library(SeuratDisk)
-
-print(par$input)
 
 # SeuratDisk needs to convert to h5seurat first
 tempfile <- paste0(gsub("\\..*$", "", par$output), ".tmp.h5seurat")
@@ -18,16 +17,17 @@ on.exit(file.remove(tempfile))
 
 # connect to input file
 # ignore the warning on "unknown file type: h5ad"
-# cat("connect to input file\n")
+if (par$verbose) cat("connect to input file\n")
 hfile <- Connect(par$input, type = "h5ad", force = TRUE, mode = "r")
 
 # convert h5ad to h5seurat
-# cat("convert h5ad to h5seurat\n")
+if (par$verbose) cat("convert h5ad to h5seurat\n")
 converted <- Convert(hfile, dest = tempfile, overwrite = TRUE)
 
 # load h5seurat
-# cat("load h5seurat\n")
+if (par$verbose) cat("load h5seurat\n")
 seuratObject <- LoadH5Seurat(converted)
 
 # save as rds
+if (par$verbose)
 saveRDS(seuratObject, file = par$output, compress = TRUE)
