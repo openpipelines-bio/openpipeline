9f960186a03c55022851ff28da88e900f54b8cd4
Robrecht Cannoodt <rcannood@gmail.com>
2022-02-14 14:46:00 +0100

    also download matrices

--- workflows/generate_test_resources/pbmc_1k_protein_v3.sh
+++ workflows/generate_test_resources/pbmc_1k_protein_v3.sh
@@ -14,14 +14,24 @@ S3DIR=`echo "$DIR" | sed 's#resources/test#s3://openpipelines-data#'`
 
 target/docker/download/download_file/download_file \
   --input https://cf.10xgenomics.com/samples/cell-exp/3.0.0/pbmc_1k_protein_v3/pbmc_1k_protein_v3_metrics_summary.csv \
-  --output ${OUT}_metrics_summary.csv
+  --output "${OUT}_metrics_summary.csv"
 
 target/docker/download/download_file/download_file \
   --input https://cf.10xgenomics.com/samples/cell-exp/3.0.0/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5 \
-  --output ${OUT}_filtered_feature_bc_matrix.h5
+  --output "${OUT}_filtered_feature_bc_matrix.h5"
+
+target/docker/download/download_file/download_file \
+  --input https://cf.10xgenomics.com/samples/cell-exp/3.0.0/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.tar.gz \
+  --output "${OUT}_filtered_feature_bc_matrix.tar.gz"
+
+mkdir -p "${OUT}_filtered_feature_bc_matrix"
+tar -xvf "${OUT}_filtered_feature_bc_matrix.tar.gz" \
+  -C "${OUT}_filtered_feature_bc_matrix" \
+  --strip-components 1
+rm "${OUT}_filtered_feature_bc_matrix.tar.gz"
   
 target/docker/convert/convert_10x_h5_to_h5mu/convert_10x_h5_to_h5mu \
-  --input ${OUT}_filtered_feature_bc_matrix.h5 \
-  --output ${OUT}_filtered_feature_bc_matrix.h5mu
+  --input "${OUT}_filtered_feature_bc_matrix.h5" \
+  --output "${OUT}_filtered_feature_bc_matrix.h5mu"
 
 aws s3 sync --profile xxx "$DIR" "$S3DIR"
