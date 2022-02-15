9a9ac756cc0bc8a0d731b253a8206e338f7d74be
Eric Van de Kerckhove <blackdragon.be@gmail.com>
2022-02-15 14:02:56 +0100

    Reformat arguments + add examples + remove unused imports + add example arguments to script

--- src/cluster/leiden/script.py
+++ src/cluster/leiden/script.py
@@ -13,14 +13,22 @@ import scanpy as sc
 print("Reading", par["input"])
 data = sc.read_h5ad(par["input"])
 
+<<<<<<< HEAD
 print("Clustering cells using Leiden algorithm")
+=======
+>>>>>>> Reformat arguments + add examples + remove unused imports + add example arguments to script
 sc.tl.leiden(
     data, resolution=float(par["resolution"]), key_added=par["cluster_column_name"]
 )
 
+<<<<<<< HEAD
 print("Writing file as", par["output_format"], "format", "to", par["output"])
 if par["output_format"] == "h5ad":
     data.write_h5ad(par["output"], compression="lzf")
+=======
+if par["output_format"] == "h5ad":
+    data.write_h5ad()(par["output"], compression="lzf")
+>>>>>>> Reformat arguments + add examples + remove unused imports + add example arguments to script
 elif par["output_format"] == "csv":
     data.obs[par["cluster_column_name"]].to_csv(par["output"])
 else:
