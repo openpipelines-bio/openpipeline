0006312d2833ea9468320edb05068c680a7a60f4
Eric Van de Kerckhove <blackdragon.be@gmail.com>
2022-02-15 14:16:10 +0100

    Remove compression (not supported by muon) + clarify input

--- src/convert/convert_10x_mtx_to_h5mu/script.py
+++ src/convert/convert_10x_mtx_to_h5mu/script.py
@@ -6,6 +6,7 @@ par = {
     "output": "5k_pbmc_protein_v3_nextgem_filtered_feature_bc_matrix.h5mu",
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 }
 ## VIASH END
 
@@ -25,10 +26,16 @@ mdata.write_h5mu(par["output"], compression=par["compression"])
 >>>>>>> Add test to convert_10x_mtx_to_h5mu + correct description and example of --output + fix example input in script
 =======
     "compression": "gzip",
+=======
+>>>>>>> Remove compression (not supported by muon) + clarify input
 }
 ## VIASH END
 
 mdata = mu.read_10x_mtx(par["input"])
+<<<<<<< HEAD
 
 mdata.write_h5mu(par["output"], compression=par["compression"])
 >>>>>>> Add test to convert_10x_mtx_to_h5mu + correct description and example of --output + fix example input in script
+=======
+mdata.write_h5mu(filename=par["output"])
+>>>>>>> Remove compression (not supported by muon) + clarify input
