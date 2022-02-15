27f72fab08b424392d4af4af439ae0db60e18ac9
Eric Van de Kerckhove <blackdragon.be@gmail.com>
2022-02-15 14:16:10 +0100

    Remove compression (not supported by muon) + clarify input

--- src/convert/convert_10x_mtx_to_h5mu/script.py
+++ src/convert/convert_10x_mtx_to_h5mu/script.py
@@ -4,10 +4,8 @@ import muon as mu
 par = {
     "input": "5k_pbmc_protein_v3_nextgem_filtered_feature_bc_matrix",
     "output": "5k_pbmc_protein_v3_nextgem_filtered_feature_bc_matrix.h5mu",
-    "compression": "gzip",
 }
 ## VIASH END
 
 mdata = mu.read_10x_mtx(par["input"])
-
-mdata.write_h5mu(par["output"], compression=par["compression"])
+mdata.write_h5mu(filename=par["output"])
