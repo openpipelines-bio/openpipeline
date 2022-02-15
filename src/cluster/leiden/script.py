12b96ce83ee9191ab34c681bc561039100d83071
Eric Van de Kerckhove <blackdragon.be@gmail.com>
2022-02-15 14:02:56 +0100

    Reformat arguments + add examples + remove unused imports + add example arguments to script

--- src/cluster/leiden/script.py
+++ src/cluster/leiden/script.py
@@ -1,19 +1,24 @@
 ### VIASH START
-
 par = {
+    "input": "resources/test/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.norm.hvg.pca.nn.umap.h5ad",
+    "output": "output.h5ad",
+    "output_format": "h5ad",
+    "cluster_column_name": "leiden.res.0.25",
+    "resolution": float("0.25"),
 }
 ### VIASH END
 
 import scanpy as sc
-import pandas as pd
 
 data = sc.read_h5ad(par["input"])
 
-sc.tl.leiden(data, resolution = float(par["resolution"]), key_added = par["clusterColumnName"])
+sc.tl.leiden(
+    data, resolution=float(par["resolution"]), key_added=par["cluster_column_name"]
+)
 
-if (par["outputFormat"] == "h5ad"):
-     data.write_h5ad(par["output"], compression = "lzf")
-elif (par["outputFormat"] == "csv"):
-     data.obs[par["clusterColumnName"]].to_csv(par["output"])
-else: 
-     raise ValueError("An unrecognized output format was specified.")
+if par["output_format"] == "h5ad":
+    data.write_h5ad()(par["output"], compression="lzf")
+elif par["output_format"] == "csv":
+    data.obs[par["cluster_column_name"]].to_csv(par["output"])
+else:
+    raise ValueError("An unrecognized output format was specified.")
