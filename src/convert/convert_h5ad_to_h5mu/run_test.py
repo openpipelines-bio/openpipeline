b2fc592557610d661297026646f9d6e6c4073ebe
Eric Van de Kerckhove <blackdragon.be@gmail.com>
2022-02-21 14:13:17 +0100

    Change test + add examples to convert_h5ad_to_h5mu

--- src/convert/convert_h5ad_to_h5mu/run_test.py
+++ src/convert/convert_h5ad_to_h5mu/run_test.py
@@ -1,39 +1,31 @@
-import unittest
-import os
-from os import path
 import subprocess
-import scanpy as sc
+from os import path
 import muon as mu
-import pandas
-import numpy as np
 
-class TestH5toH5AD(unittest.TestCase):
-    def test_simple_conversion(self):
-        preData = sc.read_h5ad("pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5ad")
+cmd_pars = [
+    "./" + meta["functionality_name"],
+    "--input",
+    meta["resources_dir"]
+    + "pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5ad",
+    "--output=output.h5mu",
+]
+out = subprocess.check_output(cmd_pars).decode("utf-8")
 
-        preData.obsm["counts_antibody"] = preData.to_df().iloc[:,0:10]
-        preData.obsm["counts_crispr"] = preData.to_df().iloc[:,0:10]
-        preData.obsm["counts_custom"] = preData.to_df().iloc[:,0:10]
+# check if file exists
+assert path.exists("output.h5mu"), "No output was created."
 
-        preData.write_h5ad("input-1.h5ad")
+# read it with scanpy
+data = mu.read_h5mu("output.h5mu")
 
-        out = subprocess.check_output([
-              "./convert_h5ad_to_h5mu", 
-              "--input=input-1.h5ad",  
-              "--output=output-py1-rna.h5mu"]
-            ).decode("utf-8")
-       
-        self.assertTrue(path.exists("output-py1-rna.h5mu"))
-        
-        data = mu.read_h5mu("output-py1-rna.h5mu")
+# check whether gex was found
+assert data.mod["rna"].var["feature_types"].unique() == [
+    "Gene Expression"
+], "Output X should only contain Gene Expression vars."
 
-        self.assertTrue("rna" in data.mod)
-        self.assertTrue("prot" in data.mod)
-        self.assertTrue("custom" in data.mod)
+# check whether ab counts were found
+assert "prot" in data.mod, 'Output should contain data.mod["rna"].'
 
-        self.assertFalse("counts_antibody" in data.mod["rna"].obsm)
-        self.assertFalse("counts_custom" in data.mod["rna"].obsm)
-        self.assertTrue("counts_crispr" in data.mod["rna"].obsm)
-
-
-unittest.main()
+# check whether gene was found
+assert (
+    "CD3_TotalSeqB" in data.mod["prot"].var_names
+), 'Output should contain antibody column "CD3_TotalSeqB".'
