59f09efa824d4668c50f26440633dac656b399e4
Eric Van de Kerckhove <blackdragon.be@gmail.com>
2022-02-21 14:13:52 +0100

    Fix import placement

--- src/convert/convert_h5ad_to_h5mu/script.py
+++ src/convert/convert_h5ad_to_h5mu/script.py
@@ -1,31 +1,28 @@
-
-
-### VIASH START
-
-par = {
-	"input": "./test/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5",
-	"output": "./test/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5ad",
-	"compression": "gzip",
-	"conversions-obsm": "{'counts_antibody': 'adt'}"
-}
-### VIASH END
-
 import muon as mu
 import anndata
 import json
 
-data =  anndata.read_h5ad(par["input"])       
+### VIASH START
+par = {
+    "input": "resources/test/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5ad",
+    "output": "output.h5mu",
+    "compression": "gzip",
+    "conversions_obsm": '{"counts_antibody":"prot", "counts_custom": "custom"}',
+}
+### VIASH END
 
-try: 
-	data.var_names_make_unique()
+data = anndata.read_h5ad(par["input"])
+
+try:
+    data.var_names_make_unique()
 except:
-	pass
+    pass
 
 muon = mu.MuData({"rna": data})
 
 for key, value in json.loads(par["conversions_obsm"]).items():
-	if key in data.obsm:
-		muon.mod[value] = anndata.AnnData(data.obsm[key])
-		del muon["rna"].obsm[key]
+    if key in data.obsm:
+        muon.mod[value] = anndata.AnnData(data.obsm[key])
+        del muon["rna"].obsm[key]
 
-muon.write_h5mu(par["output"], compression = par["compression"])
+muon.write_h5mu(par["output"], compression=par["compression"])
