5d6978beeba1c44211f8d7ae819c816a737d4f9b
Eric Van de Kerckhove <blackdragon.be@gmail.com>
2022-02-21 16:15:35 +0100

    Update pca component

--- src/dimred/pca/script.py
+++ src/dimred/pca/script.py
@@ -1,25 +1,20 @@
-### VIASH START
-
-par ={
-    "input": "./test/pbmc_1k_protein_v3_filtered_feature_bc_matrix.norm.hvg.h5ad",
-    "output": "./test/pbmc_1k_protein_v3_filtered_feature_bc_matrix.norm.hvg.pca.h5ad",
-    "nPCs": 20
-}
-### VIASH END
-
-import argparse
 import anndata
 import scanpy as sc
 
+### VIASH START
+par = {
+    "input": "resources/test/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.norm.hvg.h5ad",
+    "output": "output.h5ad",
+    "nPCs": 25,
+}
+### VIASH END
+
+print("Reading", par["input"])
 data = anndata.read_h5ad(par["input"])
 
-        
-sc.tl.pca(data, 
-          n_comps = par["nPCs"])
+print("Computing PCA coordinates")
+sc.tl.pca(data, n_comps=par["nPCs"])
+data.uns["pcaParameters"] = {"PCA: nPCs": par["nPCs"]}
 
-data.uns["pcaParameters"] = {
-    "PCA: nPCs": par["nPCs"]
-}
-
-
-data.write(par["output"], compression = "lzf")
+print("Writing".par["output"])
+data.write(par["output"], compression="lzf")
