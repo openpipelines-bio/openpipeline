6d0de62c1c247d2ac30bfe704f5a94917a20d72c
Eric Van de Kerckhove <blackdragon.be@gmail.com>
2022-02-21 13:39:42 +0100

    Fix git shenenigans after rebase

--- src/convert/convert_10x_mtx_to_h5mu/script.py
+++ src/convert/convert_10x_mtx_to_h5mu/script.py
@@ -4,9 +4,6 @@ import muon as mu
 par = {
     "input": "5k_pbmc_protein_v3_nextgem_filtered_feature_bc_matrix",
     "output": "5k_pbmc_protein_v3_nextgem_filtered_feature_bc_matrix.h5mu",
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 }
 ## VIASH END
 
@@ -15,27 +12,3 @@ mdata = mu.read_10x_mtx(par["input"])
 
 print("Writing", par["output"])
 mdata.write_h5mu(filename=par["output"])
-=======
-    "compression": "gzip",
-}
-## VIASH END
-
-mdata = mu.read_10x_mtx(par["input"])
-
-mdata.write_h5mu(par["output"], compression=par["compression"])
->>>>>>> Add test to convert_10x_mtx_to_h5mu + correct description and example of --output + fix example input in script
-=======
-    "compression": "gzip",
-=======
->>>>>>> Remove compression (not supported by muon) + clarify input
-}
-## VIASH END
-
-mdata = mu.read_10x_mtx(par["input"])
-<<<<<<< HEAD
-
-mdata.write_h5mu(par["output"], compression=par["compression"])
->>>>>>> Add test to convert_10x_mtx_to_h5mu + correct description and example of --output + fix example input in script
-=======
-mdata.write_h5mu(filename=par["output"])
->>>>>>> Remove compression (not supported by muon) + clarify input
