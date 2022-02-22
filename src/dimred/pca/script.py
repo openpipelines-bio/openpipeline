c75be695e2ed22cbc2cc75c3d9c93da88fcc9285
Eric Van de Kerckhove <blackdragon.be@gmail.com>
2022-02-22 14:13:18 +0100

    Make dimred/pca use h5mu files + add python test

--- src/dimred/pca/script.py
+++ src/dimred/pca/script.py
@@ -1,20 +1,20 @@
-import anndata
 import scanpy as sc
+import muon as mu
 
 ### VIASH START
 par = {
-    "input": "resources/test/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.norm.hvg.h5ad",
-    "output": "output.h5ad",
+    "input": "resources/test/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5mu",
+    "output": "output.h5mu",
     "nPCs": 25,
 }
 ### VIASH END
 
 print("Reading", par["input"])
-data = anndata.read_h5ad(par["input"])
+mdata = mu.read_h5mu(par["input"])
 
 print("Computing PCA coordinates")
-sc.tl.pca(data, n_comps=par["nPCs"])
-data.uns["pcaParameters"] = {"PCA: nPCs": par["nPCs"]}
+sc.tl.pca(mdata.mod["rna"], n_comps=par["nPCs"])
+mdata.uns["pcaParameters"] = {"PCA: nPCs": par["nPCs"]}
 
-print("Writing".par["output"])
-data.write(par["output"], compression="lzf")
+print("Writing", par["output"])
+mdata.write_h5mu(filename=par["output"])
