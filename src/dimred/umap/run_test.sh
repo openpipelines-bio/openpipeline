b39e76323a56a5c58b6625353bd09ca4c8cce6aa
Eric Van de Kerckhove <blackdragon.be@gmail.com>
2022-02-22 14:23:07 +0100

    Make dimred/umap use h5mu files + add test + add examples

--- src/dimred/umap/run_test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-./umap --input pbmc_1k_protein_v3_filtered_feature_bc_matrix.norm.hvg.pca.nn.umap.h5ad --output output.h5ad
-[[ ! -f output.h5ad ]] && echo "Output file could not be found!" && exit 1
-
-echo ">>> Test finished successfully"
