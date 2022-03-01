7d8e48aa40e532631d6c919ba434494cc4e157f2
Eric Van de Kerckhove <blackdragon.be@gmail.com>
2022-03-01 10:37:51 +0100

    Use h5mu instead of h5ad for hvg_scanpy

--- src/hvg/hvg_scanpy/run_test.py
+++ src/hvg/hvg_scanpy/run_test.py
@@ -6,6 +6,7 @@ import scanpy as sc
 import pandas
 import numpy as np
 
+
 class TestHVGSelection(unittest.TestCase):
     def test_simple_hvg(self):
         inputData = sc.read_h5ad("pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5ad")
@@ -14,16 +15,14 @@ class TestHVGSelection(unittest.TestCase):
 
         self.assertTrue("highly_variable" not in inputData.var.columns)
 
-        out = subprocess.check_output([
-              "./hvg_scanpy", "--input", "lognormed.h5ad",  
-			  "--output=output-py1.h5ad"]
-            ).decode("utf-8")
-       
+        out = subprocess.check_output(
+            ["./hvg_scanpy", "--input", "lognormed.h5ad", "--output=output-py1.h5ad"]
+        ).decode("utf-8")
+
         self.assertTrue(path.exists("output-py1.h5ad"))
         data = sc.read_h5ad("output-py1.h5ad")
 
         self.assertTrue("highly_variable" in data.var.columns)
-        
 
     def test_simple_hvg_with_exclusion(self):
         inputData = sc.read_h5ad("pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5ad")
@@ -32,13 +31,17 @@ class TestHVGSelection(unittest.TestCase):
 
         self.assertTrue("highly_variable" not in inputData.var.columns)
 
-        out = subprocess.check_output([
-              "./hvg_scanpy", "--input", "lognormed.h5ad",  
-			  "--output=output-py1.h5ad", 
-              "--excludedGenes", "GNAI2,RHOA"]
-            ).decode("utf-8")
+        out = subprocess.check_output(
+            [
+                "./hvg_scanpy",
+                "--input",
+                "lognormed.h5ad",
+                "--output=output-py1.h5ad",
+                "--excluded_genes",
+                "GNAI2,RHOA",
+            ]
+        ).decode("utf-8")
 
-       
         self.assertTrue(path.exists("output-py1.h5ad"))
         data = sc.read_h5ad("output-py1.h5ad")
 
@@ -47,5 +50,6 @@ class TestHVGSelection(unittest.TestCase):
         self.assertFalse(data[:, data.var_names == "RHOA"].var["highly_variable"][0])
 
         self.assertFalse(data[:, data.var_names == "JUN"].var["highly_variable"][0])
+
 
 unittest.main()
