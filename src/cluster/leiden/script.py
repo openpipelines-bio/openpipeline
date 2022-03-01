16fd7085dc9855f6e5afc45a89bc8eae6222b1b0
Eric Van de Kerckhove <blackdragon.be@gmail.com>
2022-03-01 12:48:19 +0100

    Add prints to leiden + reformat

--- src/cluster/leiden/script.py
+++ src/cluster/leiden/script.py
@@ -1,6 +1,7 @@
+import muon as mu
 import scanpy as sc
 
-### VIASH START
+# VIASH START
 par = {
     "input": "resources_test/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.norm.hvg.pca.nn.umap.h5mu",
     "output": "output.h5mu",
@@ -8,18 +9,17 @@ par = {
     "cluster_column_name": "leiden.res.0.25",
     "resolution": float("0.25"),
 }
-### VIASH END
-
-import scanpy as sc
-import muon as mu
+# VIASH END
 
 print("Reading", par["input"])
 mdata = mu.read_h5mu(par["input"])
 
+print("Cluster cells using the Leiden algorithm")
 sc.tl.leiden(
     mdata.mod["rna"],
     resolution=float(par["resolution"]),
     key_added=par["cluster_column_name"],
 )
 
+print("Writing", par["output"])
 mdata.write_h5mu(filename=par["output"])
