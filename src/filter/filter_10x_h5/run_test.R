3c580957aaa93238e4aa51909bf5791ae706edef
Eric Van de Kerckhove <blackdragon.be@gmail.com>
2022-03-01 09:36:17 +0100

    Use resources folder for test

--- src/filter/filter_10x_h5/run_test.R
+++ src/filter/filter_10x_h5/run_test.R
@@ -3,7 +3,7 @@ requireNamespace("hdf5r", quietly = TRUE)
 
 system(paste0(
   "./", meta$functionality_name, " ",
-  "--input '", meta$resources_dir, "/pbmc_1k_protein_v3_raw_feature_bc_matrix.h5' ",
+  "--input '", meta$resources_dir, "pbmc_1k_protein_v3/pbmc_1k_protein_v3_raw_feature_bc_matrix.h5' ",
   "--output pbmc_1k_protein_v3_raw_feature_bc_matrix_filtered.h5 ",
   "--min_library_size 1000 ",
   "--min_cells_per_gene 300 ",
