41cf19c47946ee17068b17ef63b833d506715ec2
Eric Van de Kerckhove <blackdragon.be@gmail.com>
2022-03-08 10:08:16 +0100

    Rewrote and tested tar_extract test

--- src/tar_extract/run_test.sh
+++ src/tar_extract/run_test.sh
@@ -2,19 +2,25 @@
 
 set -ex
 
-test_file="$meta_resources_dir/LICENSE"
+INPUT_FILE="LICENSE"
+OUTPUT_DIR="output/files/"
+OUTPUT_FILE="${OUTPUT_DIR}${INPUT_FILE}"
 
 echo ">>> Check whether test file exists"
-[[ ! -f $test_file ]] && echo "Test file could not be found!" && exit 1
+[[ ! -f ${INPUT_FILE} ]] && echo "Test file could not be found!" && exit 1
 
 echo ">>> Creating tar.gz..."
-tar czvf $test_file.tar.gz $test_file
+tar czvf ${INPUT_FILE}.tar.gz ${INPUT_FILE}
 
 echo ">>> Check whether tar.gz can be extracted"
 ./$meta_functionality_name \
-   --input "$test_file.tar.gz" \
-   --output output_dir
+   --input "${INPUT_FILE}.tar.gz" \
+   --output "$OUTPUT_DIR"
 
-[[ ! -d output_dir ]] && echo "Output directory could not be found!" && exit 1
+echo ">>> Check whether extracted file exists"
+[[ ! -f $OUTPUT_FILE ]] && echo "Output file could not be found!" && exit 1
+
+echo ">>> Check whether input and output file are the same"
+cmp $INPUT_FILE $OUTPUT_FILE || (echo "Input and output files are different!" && exit 1)
 
 echo ">>> Test finished successfully"
