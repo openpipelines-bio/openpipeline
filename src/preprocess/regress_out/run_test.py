03c782fe1d10762e50908118e4cc8c3cb87df1fd
Robrecht Cannoodt <rcannood@gmail.com>
2022-03-22 08:22:33 +0100

    rework lognorm and add regress_out

--- src/preprocess/regress_out/run_test.py
+++ src/preprocess/regress_out/run_test.py
@@ -10,13 +10,23 @@ meta = {
 }
 ## VIASH END
 
-input = f"{meta['resources_dir']}/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5mu"
+orig_input = f"{meta['resources_dir']}/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5mu"
+input = "input.h5mu"
 output = "output.h5mu"
 
+# add column to obs
+mu_orig_input = mu.read_h5mu(orig_input)
+mu_orig_input.obs["var"] = np.random.rand(mu_orig_input.n_obs)
+mu_orig_input.mod["rna"].obs["var"] = mu_orig_input.obs["var"]
+mu_orig_input.mod["prot"].obs["var"] = mu_orig_input.obs["var"]
+mu_orig_input.write_h5mu(input)
+
+# execute command
 cmd_pars = [
     f"./{meta['functionality_name']}",
     "--input", input,
     "--output", output,
+    "--obs_keys", "var"
 ]
 out = subprocess.check_output(cmd_pars).decode("utf-8")
 
@@ -42,12 +52,5 @@ assert prot_in.shape == prot_out.shape, "Should have same shape as before"
 
 print("> Check if expression has changed.")
 assert np.mean(rna_in.X) != np.mean(rna_out.X), "Expression should have changed"
-
-print("> Checking row-wise and column-wise correlation.")
-nz_row, nz_col = rna_in.X.nonzero()
-row_corr = np.corrcoef(rna_in.X[nz_row[0],:].toarray().flatten(), rna_out.X[nz_row[0],:].toarray().flatten())[0,1]
-col_corr = np.corrcoef(rna_in.X[:,nz_col[0]].toarray().flatten(), rna_out.X[:,nz_col[0]].toarray().flatten())[0,1]
-assert row_corr > .1
-assert col_corr > .1
 
 print(">> All tests succeeded!")
