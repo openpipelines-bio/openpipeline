f65155147a8f675d97ad3aef7abcce35f3ae1a80
Robrecht Cannoodt <rcannood@gmail.com>
2022-04-05 05:44:03 +0200

    refactor find_neighbors component

--- src/neighbors/find_neighbors/run_test.py
+++ src/neighbors/find_neighbors/run_test.py
@@ -2,30 +2,46 @@ import subprocess
 from os import path
 import muon as mu
 
+
+## VIASH START
+meta = {
+    'functionality_name': 'find_neighbors',
+    'resources_dir': 'resources_test/'
+}
+## VIASH END
+
+input = f"{meta['resources_dir']}/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.tx_processing.h5mu"
+output = "output.h5mu"
 cmd_pars = [
     "./" + meta["functionality_name"],
-    "--input",
-    meta["resources_dir"]
-    + "/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5mu",
-    "--output=output.h5mu",
+    "--input", input,
+    "--output", output,
+    "--obsp_name_prefix", "foo"
 ]
 out = subprocess.check_output(cmd_pars).decode("utf-8")
 
-# check if file exists
-assert path.exists("output.h5mu"), "No output was created."
+print("> Check if output was created.")
+assert path.exists(output), "No output was created."
 
-# read it with scanpy
-data = mu.read_h5mu("output.h5mu")
+print("> Reading mudata files.")
+mu_input = mu.read_h5mu(input)
+mu_output = mu.read_h5mu(output)
 
-# check whether gex was found
-assert data.mod["rna"].var["feature_types"].unique() == [
-    "Gene Expression"
-], "Output X should only contain Gene Expression vars."
+print("> Check whether output contains right modalities.")
+assert "rna" in mu_output.mod, 'Output should contain data.mod["prot"].'
+assert "prot" in mu_output.mod, 'Output should contain data.mod["prot"].'
 
-# check whether ab counts were found
-assert "prot" in data.mod, 'Output should contain data.mod["rna"].'
+rna_in = mu_input.mod["rna"]
+rna_out = mu_output.mod["rna"]
+prot_in = mu_input.mod["prot"]
+prot_out = mu_output.mod["prot"]
 
-# check whether gene was found
-assert (
-    "CD3_TotalSeqB" in data.mod["prot"].var_names
-), 'Output should contain antibody column "CD3_TotalSeqB".'
+print("> Check shape of outputs.")
+assert rna_in.shape == rna_out.shape, "Should have same shape as before"
+assert prot_in.shape == prot_out.shape, "Should have same shape as before"
+
+print("> Check existence of output fields.")
+assert "foo_connectivities" in rna_out.obsp, "Output should have .obsp['foo_connectivities']"
+assert "foo_distances" in rna_out.obsp, "Output should have .obsp['foo_distances']"
+assert "foo_connectivities" not in rna_in.obsp, "Input should not have .obsp['foo_connectivities']"
+assert "foo_distances" not in rna_in.obsp, "Input should not have .obsp['foo_distances']"
