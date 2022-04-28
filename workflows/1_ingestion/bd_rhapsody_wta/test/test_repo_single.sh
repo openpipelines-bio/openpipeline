6a84995203b9dad0271e534e1d490643a8a7337d
rcannood <rcannood@users.noreply.github.com>
2022-04-28 14:09:34 +0000

    deploy: a86ae3aefa9ebdf153467ffed15e8e18d1bbcf56

--- workflows/1_ingestion/bd_rhapsody_wta/test/test_repo_single.sh
+++ workflows/1_ingestion/bd_rhapsody_wta/test/test_repo_single.sh
@@ -3,12 +3,12 @@
 # collection of commands for testing workflows in repos
 
 export NXF_VER=21.04.1
+export VIASH_TEMP=$HOME/workspace/viash_temp/
+export NXF_TEMP=$HOME/workspace/nxf_temp/
 
 PAR_GENOME="resources_test/bd_rhapsody_wta_test/raw/GRCh38-PhiX-gencodev29-20181205.tar.gz"
 PAR_TRANSCRIPTOME="resources_test/bd_rhapsody_wta_test/raw/gencodev29-20181205.gtf"
 PAR_INPUTS='resources_test/bd_rhapsody_wta_test/raw/sample_R1_.fastq.gz;resources_test/bd_rhapsody_wta_test/raw/sample_R2_.fastq.gz'
-
-nextflow drop https://github.com/openpipelines-bio/openpipeline.git
 
 nextflow \
   run https://github.com/openpipelines-bio/openpipeline.git \
