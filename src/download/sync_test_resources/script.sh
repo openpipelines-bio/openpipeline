73eefb186ca541fbc3a1c28e3f28b15738568873
Robrecht Cannoodt <rcannood@gmail.com>
2022-05-19 13:58:13 +0200

    try to fix awscli component

--- src/download/sync_test_resources/script.sh
+++ src/download/sync_test_resources/script.sh
@@ -11,4 +11,9 @@ par_output='resources_test'
 #   extra_params+=( "--quiet" )
 # fi
 
+# Disable the use of the Amazon EC2 instance metadata service (IMDS).
+# see https://florian.ec/blog/github-actions-awscli-errors/
+# or https://github.com/aws/aws-cli/issues/5234#issuecomment-705831465
+export AWS_EC2_METADATA_DISABLED=true
+
 aws s3 sync "$par_input" "$par_output" --no-sign-request #"${extra_params[@]}"
