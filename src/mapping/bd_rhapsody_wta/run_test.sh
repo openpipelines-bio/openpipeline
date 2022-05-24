d5f6475ab867c3d8d610c590a02790fa2db6827b
rcannood <rcannood@users.noreply.github.com>
2022-05-24 21:11:33 +0000

    deploy: a90097f4c792c76db67e0bffe3db10141933e465

--- src/mapping/bd_rhapsody_wta/run_test.sh
+++ src/mapping/bd_rhapsody_wta/run_test.sh
@@ -5,8 +5,8 @@ mkdir output
 "./$meta_functionality_name" \
   -i "$meta_resources_dir/bd_rhapsody_wta_test/raw/sample_R1_.fastq.gz" \
   -i "$meta_resources_dir/bd_rhapsody_wta_test/raw/sample_R2_.fastq.gz"  \
-  -r "$meta_resources_dir/bd_rhapsody_wta_test/raw/GRCh38-PhiX-gencodev29-20181205.tar.gz" \
-  -t "$meta_resources_dir/bd_rhapsody_wta_test/raw/gencodev29-20181205.gtf" \
+  -r "$meta_resources_dir/bd_rhapsody_wta_test/raw/GRCh38_primary_assembly_genome_chr1.tar.gz" \
+  -t "$meta_resources_dir/bd_rhapsody_wta_test/raw/gencode_v40_annotation_chr1.gtf" \
   --subsample 0.2 \
   -o output/
 
