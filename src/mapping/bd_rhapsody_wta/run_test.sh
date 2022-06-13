2e0c444b21ca57c62362c23b231fd1c0c7125693
rcannood <rcannood@users.noreply.github.com>
2022-06-13 08:05:15 +0000

    deploy: 904b109c1fa7e45a848471d34778fac2fb298af4

--- src/mapping/bd_rhapsody_wta/run_test.sh
+++ src/mapping/bd_rhapsody_wta/run_test.sh
@@ -2,14 +2,20 @@
 
 mkdir output
 
+# temporarily decrease RAM requirements to be able to work on CI systems
+sed -i 's#"ramMin": [^,]*,#"ramMin": 4000,#' "$meta_resources_dir/rhapsody_wta_1.9.1_nodocker.cwl"
+
+echo ">> Running $meta_functionality_name"
 "./$meta_functionality_name" \
   -i "$meta_resources_dir/bd_rhapsody_wta_test/raw/sample_R1_.fastq.gz" \
   -i "$meta_resources_dir/bd_rhapsody_wta_test/raw/sample_R2_.fastq.gz"  \
   -r "$meta_resources_dir/bd_rhapsody_wta_test/raw/GRCh38_primary_assembly_genome_chr1.tar.gz" \
   -t "$meta_resources_dir/bd_rhapsody_wta_test/raw/gencode_v40_annotation_chr1.gtf" \
   --subsample 0.2 \
-  -o output/
+  -o output/ \
+  --parallel false
 
+echo ">> Checking whether output can be found"
 [[ ! -f output/sample_RSEC_ReadsPerCell_Unfiltered.csv.gz ]] && echo "Output file could not be found!" && exit 1
 
 echo ">>> Test finished successfully"
