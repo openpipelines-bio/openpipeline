f97db580b5119e3aa81eac363938033181d29ce4
Toni Verbeiren <toni.verbeiren@gmail.com>
2022-06-16 14:35:21 +0200

    One update forgotten

--- src/demux/bcl_convert/script.sh
+++ src/demux/bcl_convert/script.sh
@@ -4,5 +4,5 @@ bcl-convert \
   --force \
   --bcl-input-directory "$par_input" \
   --output-directory "$par_output" \
-  --sample-sheet "$par_samplesheet" \
+  --sample-sheet "$par_sample_sheet" \
   --first-tile-only $par_test_mode
