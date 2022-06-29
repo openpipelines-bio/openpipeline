348fc79ec7bd8eff5d464c9dceaa815ca8955ad3
Robrecht Cannoodt <rcannood@gmail.com>
2022-06-29 09:36:45 +0200

    fix paths in helper scripts

--- workflows/ingestion/bd_rhapsody_wta/test/integration_test.sh
+++ workflows/ingestion/bd_rhapsody_wta/test/integration_test.sh
@@ -10,7 +10,7 @@ export NXF_VER=21.10.6
 
 bin/nextflow \
   run . \
-  -main-script workflows/1_ingestion/bd_rhapsody_wta/main.nf \
+  -main-script workflows/ingestion/bd_rhapsody_wta/main.nf \
   -entry test_wf \
   -resume \
   -with-docker \
