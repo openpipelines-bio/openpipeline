a78771ce3d9eec03f81f9f347b2887eb098fdc54
Dries Schaumont <5946712+DriesSchaumont@users.noreply.github.com>
2022-07-04 15:39:08 +0200

    Fix BD Rhapsody 1.10 tests and test data.

--- workflows/resources_test_scripts/bd_rhapsody_wta_1_10_test.sh
+++ workflows/resources_test_scripts/bd_rhapsody_wta_1_10_test.sh
@@ -12,9 +12,14 @@ mkdir -p "$raw_dir"
 wget http://bd-rhapsody-public.s3.amazonaws.com/Rhapsody-Demo-Data-Inputs/12WTA-ABC-SMK-EB-5kJRT.tar -O "$raw_dir/12WTA-ABC-SMK-EB-5kJRT.tar"
 mkdir "$raw_dir/12WTA-ABC-SMK-EB-5kJRT"
 tar -xvf  "$raw_dir/12WTA-ABC-SMK-EB-5kJRT.tar" -C "$raw_dir/12WTA-ABC-SMK-EB-5kJRT" --strip-components=1
-cp "$raw_dir/12WTA-ABC-SMK-EB-5kJRT/12SMK_S1_L432_R1_001.fastq.gz" "$raw_dir/sample_R1_.fastq.gz"
-cp "$raw_dir/12WTA-ABC-SMK-EB-5kJRT/12SMK_S1_L432_R2_001.fastq.gz" "$raw_dir/sample_R2_.fastq.gz"
+seqkit sample -s100 -p 0.01 "$raw_dir/12WTA-ABC-SMK-EB-5kJRT/12SMK_S1_L432_R1_001.fastq.gz" | gzip -9 > "$raw_dir/12SMK_S1_L432_R1_001.fastq.gz"
+seqkit sample -s100 -p 0.01 "$raw_dir/12WTA-ABC-SMK-EB-5kJRT/12SMK_S1_L432_R2_001.fastq.gz" | gzip -9 > "$raw_dir/12SMK_S1_L432_R2_001.fastq.gz"
+seqkit sample -s100 -p 0.01 "$raw_dir/12WTA-ABC-SMK-EB-5kJRT/12ABC_S1_L432_R1_001.fastq.gz" | gzip -9 > "$raw_dir/12ABC_S1_L432_R1_001.fastq.gz"
+seqkit sample -s100 -p 0.01 "$raw_dir/12WTA-ABC-SMK-EB-5kJRT/12ABC_S1_L432_R2_001.fastq.gz" | gzip -9 > "$raw_dir/12ABC_S1_L432_R2_001.fastq.gz"
+seqkit sample -s100 -p 0.01 "$raw_dir/12WTA-ABC-SMK-EB-5kJRT/12WTA_S1_L432_R1_001.fastq.gz" | gzip -9 > "$raw_dir/12WTA_S1_L432_R1_001.fastq.gz"
+seqkit sample -s100 -p 0.01 "$raw_dir/12WTA-ABC-SMK-EB-5kJRT/12WTA_S1_L432_R1_001.fastq.gz" | gzip -9 > "$raw_dir/12WTA_S1_L432_R1_001.fastq.gz"
 
+cp "$raw_dir/12WTA-ABC-SMK-EB-5kJRT/BDAbSeq_ImmuneDiscoveryPanel.fasta" "$raw_dir
 rm -r "$raw_dir/12WTA-ABC-SMK-EB-5kJRT"
 rm "$raw_dir/12WTA-ABC-SMK-EB-5kJRT.tar"
 
@@ -27,8 +32,8 @@ rm "$raw_dir/12WTA-ABC-SMK-EB-5kJRT.tar"
 # mkdir -p "$bdrhap_out"
 
 # target/docker/mapping/bd_rhapsody_wta_1_10_1/bd_rhapsody_wta_1_10_1 \
-#   --input "$raw_dir/sample_R1_.fastq.gz" \
-#   --input "$raw_dir/sample_R2_.fastq.gz" \
+#   --input "$raw_dir/12SMK_S1_L432_R1_001.fastq.gz" \
+#   --input "$raw_dir/12SMK_S1_L432_R2_001.fastq.gz" \
 #   --reference_genome "$raw_dir/GRCh38_primary_assembly_genome_chr1.tar.gz" \
 #   --transcriptome_annotation "$raw_dir/gencode_v40_annotation_chr1.gtf" \
 #   --output "$bdrhap_out"
