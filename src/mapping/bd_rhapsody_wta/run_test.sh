7a3c6892b50f22608455df268bc739b19cacd1b3
Dries Schaumont <5946712+DriesSchaumont@users.noreply.github.com>
2022-07-06 12:06:39 +0200

    BD Rhapsody: use new data in component tests.

--- src/mapping/bd_rhapsody_wta/run_test.sh
+++ src/mapping/bd_rhapsody_wta/run_test.sh
@@ -2,13 +2,11 @@
 
 echo ">> Checking whether requirement overrides work"
 "./$meta_functionality_name" \
-  -i "$meta_resources_dir/bd_rhapsody_wta_test/raw/12SMK_S1_L432_R1_001.fastq.gz" \
-  -i "$meta_resources_dir/bd_rhapsody_wta_test/raw/12SMK_S1_L432_R2_001.fastq.gz"  \
-  -r "$meta_resources_dir/bd_rhapsody_wta_test/raw/GRCh38_primary_assembly_genome_chr1.tar.gz" \
-  -t "$meta_resources_dir/bd_rhapsody_wta_test/raw/gencode_v40_annotation_chr1.gtf" \
-  --abseq_reference "$meta_resources_dir/bd_rhapsody_wta_test/raw/BDAbSeq_ImmuneDiscoveryPanel.fasta" \
-  --tag_names "1-Jurkat:2-Ramos:3-THP1" \
-  --sample_tags_version "hs" \
+  -i "$meta_resources_dir/bdrhap_5kjrt/raw/12WTA_S1_L432_R1_001_subset.fastq.gz" \
+  -i "$meta_resources_dir/bdrhap_5kjrt/raw/12WTA_S1_L432_R1_001_subset.fastq.gz"  \
+  -r "$meta_resources_dir/bdrhap_ref_gencodev40_chr1/GRCh38_primary_assembly_genome_chr1.tar.gz" \
+  -t "$meta_resources_dir/bdrhap_ref_gencodev40_chr1/gencode_v40_annotation_chr1.gtf" \
+  --abseq_reference "$meta_resources_dir/bdrhap_5kjrt/raw/BDAbSeq_ImmuneDiscoveryPanel.fasta" \
   --putative_cell_call "mRNA" \
   --override_min_cores 1234 \
   --override_min_ram 5678 \
@@ -28,13 +26,11 @@ fi
 
 echo ">> Running $meta_functionality_name"
 "$meta_executable" \
-  -i "$meta_resources_dir/bd_rhapsody_wta_test/raw/12SMK_S1_L432_R1_001.fastq.gz" \
-  -i "$meta_resources_dir/bd_rhapsody_wta_test/raw/12SMK_S1_L432_R2_001.fastq.gz"  \
-  -r "$meta_resources_dir/bd_rhapsody_wta_test/raw/GRCh38_primary_assembly_genome_chr1.tar.gz" \
-  -t "$meta_resources_dir/bd_rhapsody_wta_test/raw/gencode_v40_annotation_chr1.gtf" \
-  --abseq_reference "$meta_resources_dir/bd_rhapsody_wta_test/raw/BDAbSeq_ImmuneDiscoveryPanel.fasta" \
-  --tag_names "1-Jurkat:2-Ramos:3-THP1" \
-  --sample_tags_version "hs" \
+  -i "$meta_resources_dir/bdrhap_5kjrt/raw/12WTA_S1_L432_R1_001_subset.fastq.gz" \
+  -i "$meta_resources_dir/bdrhap_5kjrt/raw/12WTA_S1_L432_R1_001_subset.fastq.gz"  \
+  -r "$meta_resources_dir/bdrhap_ref_gencodev40_chr1/GRCh38_primary_assembly_genome_chr1.tar.gz" \
+  -t "$meta_resources_dir/bdrhap_ref_gencodev40_chr1/gencode_v40_annotation_chr1.gtf" \
+  --abseq_reference "$meta_resources_dir/bdrhap_5kjrt/raw/BDAbSeq_ImmuneDiscoveryPanel.fasta" \
   --putative_cell_call "mRNA" \
   --override_min_cores 1 \
   --override_min_ram 2 \
