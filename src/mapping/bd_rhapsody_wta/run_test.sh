828cc6f1b849923f4f63065f0e53a57511d67567
rcannood <rcannood@users.noreply.github.com>
2022-07-07 12:35:36 +0000

    deploy: 9aa22211d886e2e83718c712b71f3fe2788c6b24

--- src/mapping/bd_rhapsody_wta/run_test.sh
+++ src/mapping/bd_rhapsody_wta/run_test.sh
@@ -6,7 +6,6 @@ echo ">> Checking whether requirement overrides work"
   -i "$meta_resources_dir/bdrhap_5kjrt/raw/12WTA_S1_L432_R2_001_subset.fastq.gz"  \
   -r "$meta_resources_dir/bdrhap_ref_gencodev40_chr1/GRCh38_primary_assembly_genome_chr1.tar.gz" \
   -t "$meta_resources_dir/bdrhap_ref_gencodev40_chr1/gencode_v40_annotation_chr1.gtf" \
-  --abseq_reference "$meta_resources_dir/bdrhap_5kjrt/raw/BDAbSeq_ImmuneDiscoveryPanel.fasta" \
   --putative_cell_call "mRNA" \
   --override_min_cores 1234 \
   --override_min_ram 5678 \
@@ -31,7 +30,6 @@ echo ">> Running $meta_functionality_name"
   -i "$meta_resources_dir/bdrhap_5kjrt/raw/12WTA_S1_L432_R2_001_subset.fastq.gz"  \
   -r "$meta_resources_dir/bdrhap_ref_gencodev40_chr1/GRCh38_primary_assembly_genome_chr1.tar.gz" \
   -t "$meta_resources_dir/bdrhap_ref_gencodev40_chr1/gencode_v40_annotation_chr1.gtf" \
-  --abseq_reference "$meta_resources_dir/bdrhap_5kjrt/raw/BDAbSeq_ImmuneDiscoveryPanel.fasta" \
   --putative_cell_call "mRNA" \
   --override_min_cores 1 \
   --override_min_ram 2 \
