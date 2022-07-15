8674798920e321cc13db063ff3e80befefadaa57
rcannood <rcannood@users.noreply.github.com>
2022-07-15 09:31:30 +0000

    deploy: 1b57a91a36621cfb37bd4e018ff04f13d03fdea2

--- src/mapping/bd_rhapsody_wta/script.py
+++ src/mapping/bd_rhapsody_wta/script.py
@@ -25,9 +25,9 @@ meta = {
 }
 ## VIASH END
 
-if re.match("[^A-Za-z0-9]", par["id"]):
-  print("Warning: --id should only consist of letters, numbers or hyphens. Replacing all '[^A-Za-z0-9]' with '-'.")
-  par["id"] = re.sub("[^A-Za-z0-9\\-]", "-", par["id"])
+if re.match("[^A-Za-z0-9]", par["sample_prefix"]):
+  print("Warning: --sample_prefix should only consist of letters, numbers or hyphens. Replacing all '[^A-Za-z0-9]' with '-'.")
+  par["sample_prefix"] = re.sub("[^A-Za-z0-9\\-]", "-", par["sample_prefix"])
 
 def strip_margin(text):
   return re.sub('\n[ \t]*\|', '\n', text)
@@ -206,10 +206,10 @@ content_list.append(strip_margin(f"""\
   |"""
 ))
 
-if par["id"]:
+if par["sample_prefix"]:
   content_list.append(strip_margin(f"""\
     |## Run Name (optional) -  Specify a run name to use as the output file base name. Use only letters, numbers, or hyphens. Do not use special characters or spaces.
-    |Run_Name: {par["id"]}
+    |Run_Name: {par["sample_prefix"]}
     |"""))
 
 ## Write config to file
@@ -265,9 +265,9 @@ if not par["dryrun"]:
     )
 
   # look for counts file
-  if not par["id"]:
-    par["id"] = "sample"
-  counts_filename = par["id"] + "_RSEC_MolsPerCell.csv"
+  if not par["sample_prefix"]:
+    par["sample_prefix"] = "sample"
+  counts_filename = par["sample_prefix"] + "_RSEC_MolsPerCell.csv"
   
   if par["sample_tags_version"]:
     counts_filename = "Combined_" + counts_filename
@@ -277,7 +277,7 @@ if not par["dryrun"]:
     raise ValueError(f"Could not find output counts file '{counts_filename}'")
 
   # look for metrics file
-  metrics_filename = par["id"] + "_Metrics_Summary.csv"
+  metrics_filename = par["sample_prefix"] + "_Metrics_Summary.csv"
   metrics_file = os.path.join(par["output"], metrics_filename)
   if not os.path.exists(metrics_file):
     raise ValueError(f"Could not find output metrics file '{metrics_filename}'")
