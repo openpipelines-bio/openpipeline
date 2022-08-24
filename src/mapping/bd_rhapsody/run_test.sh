6fc7903790e8a3825121b2dd70235beee91cc9c8
Robrecht Cannoodt <rcannood@gmail.com>
2022-08-24 15:29:43 +0200

    fix unit tests

--- src/mapping/bd_rhapsody/run_test.sh
+++ src/mapping/bd_rhapsody/run_test.sh
@@ -2,6 +2,7 @@
 
 echo ">> Checking whether requirement overrides work"
 "./$meta_functionality_name" \
+  --mode wta \
   -i "$meta_resources_dir/bdrhap_5kjrt/raw/12WTA_S1_L432_R1_001_subset.fastq.gz" \
   -i "$meta_resources_dir/bdrhap_5kjrt/raw/12WTA_S1_L432_R2_001_subset.fastq.gz"  \
   -r "$meta_resources_dir/bdrhap_ref_gencodev40_chr1/GRCh38_primary_assembly_genome_chr1.tar.gz" \
@@ -24,8 +25,9 @@ if ! grep -q '"ramMin": 5678000,' output2/pipeline.cwl; then
 fi
 
 
-echo ">> Running $meta_functionality_name"
+echo ">> Running $meta_functionality_name in WTA mode"
 "$meta_executable" \
+  --mode wta \
   -i "$meta_resources_dir/bdrhap_5kjrt/raw/12WTA_S1_L432_R1_001_subset.fastq.gz" \
   -i "$meta_resources_dir/bdrhap_5kjrt/raw/12WTA_S1_L432_R2_001_subset.fastq.gz"  \
   -r "$meta_resources_dir/bdrhap_ref_gencodev40_chr1/GRCh38_primary_assembly_genome_chr1.tar.gz" \
@@ -38,6 +40,27 @@ echo ">> Running $meta_functionality_name"
 
 echo ">> Checking whether output can be found"
 [[ ! -f output/sample_RSEC_ReadsPerCell_Unfiltered.csv.gz ]] && echo "Output file could not be found!" && exit 1
+
+
+
+
+echo ">> Running $meta_functionality_name in Targeted mode"
+"$meta_executable" \
+  --mode targeted \
+  -i "$meta_resources_dir/bdrhap_vdj/raw/RhapVDJDemo-BCR_S1_L001_R1_001_subset.fastq.gz" \
+  -i "$meta_resources_dir/bdrhap_vdj/raw/RhapVDJDemo-BCR_S1_L001_R2_001_subset.fastq.gz" \
+  -i "$meta_resources_dir/bdrhap_vdj/raw/RhapVDJDemo-mRNA_S5_L001_R1_001_subset.fastq.gz" \
+  -i "$meta_resources_dir/bdrhap_vdj/raw/RhapVDJDemo-mRNA_S5_L001_R2_001_subset.fastq.gz" \
+  -i "$meta_resources_dir/bdrhap_vdj/raw/RhapVDJDemo-TCR_S3_L001_R1_001_subset.fastq.gz" \
+  -i "$meta_resources_dir/bdrhap_vdj/raw/RhapVDJDemo-TCR_S3_L001_R2_001_subset.fastq.gz" \
+  --reference "$meta_resources_dir/bdrhap_vdj/raw/BDAbSeq_ImmuneDiscoveryPanel.fasta" \
+  --putative_cell_call "mRNA" \
+  --override_min_cores 1 \
+  --override_min_ram 2 \
+  -o output_vdj/
+
+echo ">> Checking whether output can be found"
+[[ ! -f output_vdj/sample_RSEC_ReadsPerCell_Unfiltered.csv.gz ]] && echo "Output file could not be found!" && exit 1
 
 
 echo ">>> Test finished successfully"
