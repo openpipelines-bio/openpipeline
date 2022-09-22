773cbf0b45af373acb2e2363f9a421c3ee8f6e2c
Dries Schaumont <5946712+DriesSchaumont@users.noreply.github.com>
2022-09-22 14:13:18 +0200

    Do not use multiple inputs for 'modality' argument in components.

--- src/transform/normalize_total/script.py
+++ src/transform/normalize_total/script.py
@@ -7,7 +7,7 @@ par = {
     "input": "work/d9/3adbd080e0de618d44b59b1ec81685/run.output.h5mu",
     "output": "output.h5mu",
     "target_sum": 10000,
-    "modality": ["rna"],
+    "modality": "rna",
     "exclude_highly_expressed": False
 }
 meta = {"functionality_name": "lognorm"}
@@ -27,17 +27,17 @@ mdata.var_names_make_unique()
 
 logger.info(par)
 
-for mod in par["modality"]:
-    logger.info("Performing total normalization on modality %s", mod)
-    dat = mdata.mod[mod]
-    if par['input_layer'] and not par['input_layer'] in dat.layers.keys():
-        raise ValueError(f"Input layer {par['input_layer']} not found in {mod}")
-    output_data = sc.pp.normalize_total(dat,
-                                        layer=par["input_layer"],
-                                        copy=True if par["output_layer"] else False)
-    
-    if output_data:
-        dat.layers[par["output_layer"]] = output_data.X
+mod = par["modality"]
+logger.info("Performing total normalization on modality %s", mod)
+dat = mdata.mod[mod]
+if par['input_layer'] and not par['input_layer'] in dat.layers.keys():
+    raise ValueError(f"Input layer {par['input_layer']} not found in {mod}")
+output_data = sc.pp.normalize_total(dat,
+                                    layer=par["input_layer"],
+                                    copy=True if par["output_layer"] else False)
+
+if output_data:
+    dat.layers[par["output_layer"]] = output_data.X
 
 logger.info("Writing to file")
 mdata.write(filename=par["output"])
