658a419158a39bc9d66c6c9d55591fa3b6e90a26
Dries Schaumont <5946712+DriesSchaumont@users.noreply.github.com>
2022-09-23 11:02:50 +0200

    Do not use multiple inputs for 'modality' argument in components. (#95)
    * Do not use multiple inputs for 'modality' argument in components.
    
    * Add CHANGLOG entry

--- src/transform/delete_layer/script.py
+++ src/transform/delete_layer/script.py
@@ -6,7 +6,7 @@ import logging
 par = {
     "input": "resources_test/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5mu",
     "output": "output.h5mu",
-    "modality": ["rna"],
+    "modality": "rna",
     "layer": ['test'],
     "missing_ok": False
 }
@@ -23,16 +23,16 @@ logger.addHandler(console_handler)
 def main():
     logger.info('Reading input file %s.', par['input'])
     input_data = read_h5mu(par['input'])
-    logger.info('Processing modalities: %s.', ','.join(par['modality']))
-    for mod_name in par['modality']:
-        mod = input_data.mod[mod_name]
-        for layer in par['layer']:
-            if layer not in mod.layers:
-                if par['missing_ok']:
-                    continue
-                raise ValueError(f"Layer '{layer}' is not present in modality {mod_name}.")
-            logger.info('Deleting layer %s from modality %s.', layer, mod_name)
-            del mod.layers[layer]
+    logger.info('Processing modality: %s.', par['modality'])
+    mod_name = par['modality']
+    mod = input_data.mod[mod_name]
+    for layer in par['layer']:
+        if layer not in mod.layers:
+            if par['missing_ok']:
+                continue
+            raise ValueError(f"Layer '{layer}' is not present in modality {mod_name}.")
+        logger.info('Deleting layer %s from modality %s.', layer, mod_name)
+        del mod.layers[layer]
     logger.info('Writing output to %s.', par['output'])
     input_data.write_h5mu(par['output'])
     logger.info('Finished.')
--- src/transform/log1p/script.py
+++ src/transform/log1p/script.py
@@ -8,7 +8,7 @@ par = {
     "input": "resources_test/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5mu",
     "output": "output.h5mu",
     "base": None,
-    "modality": ["rna"],
+    "modality": "rna",
 }
 meta = {"functionality_name": "lognorm"}
 ## VIASH END
@@ -24,15 +24,14 @@ logger.info("Reading input mudata")
 mdata = mu.read_h5mu(par["input"])
 mdata.var_names_make_unique()
 
-
-for mod in par["modality"]:
-    logger.info("Performing log transformation on modality %s", mod)
-    data = mdata.mod[mod]
-    new_layer = sc.pp.log1p(data,
-                            base=par["base"],
-                            copy=True if par['output_layer'] else False)
-    if new_layer:
-        data.layers[par['output_layer']] = new_layer.X
+mod = par["modality"]
+logger.info("Performing log transformation on modality %s", mod)
+data = mdata.mod[mod]
+new_layer = sc.pp.log1p(data,
+                        base=par["base"],
+                        copy=True if par['output_layer'] else False)
+if new_layer:
+    data.layers[par['output_layer']] = new_layer.X
 
 logger.info("Writing to file %s", par["output"])
 mdata.write(filename=par["output"])
