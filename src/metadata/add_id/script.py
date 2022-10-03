09d9b775e3fc0749891f9c01e74260bfd94786dd
Dries Schaumont <5946712+DriesSchaumont@users.noreply.github.com>
2022-10-03 14:58:58 +0200

    Implement metadata/add_id component (#109)
    * Implement metadata/add_id component
    
    * Update CHANGELOG

---
+++ src/metadata/add_id/script.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+import logging
+from mudata import read_h5mu, MuData
+from sys import stdout
+from collections.abc import Iterable
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+console_handler = logging.StreamHandler(stdout)
+logFormatter = logging.Formatter("%(asctime)s %(levelname)-8s %(message)s")
+console_handler.setFormatter(logFormatter)
+logger.addHandler(console_handler)
+
+### VIASH START
+par = {
+    "input": "resources_test/concat_test_data/e18_mouse_brain_fresh_5k_filtered_feature_bc_matrix_subset.h5mu",
+    "output": "foo.h5mu",
+    "input_id": "mouse",
+    "make_observation_keys_unique": True
+}
+### VIASH END
+
+
+def make_observation_keys_unique(sample_id: str, sample: MuData) -> None:
+    """
+    Make the observation keys unique across all samples. At input,
+    the observation keys are unique within a sample. By adding the sample name
+    (unique for a sample) to each observation key, the observation key is made
+    unique across all samples as well.
+    """
+    logger.info('Making observation keys unique across all samples.')
+    sample.obs.index = f"{sample_id}_" + sample.obs.index
+    make_observation_keys_unique_per_mod(sample_id, sample)
+
+
+def make_observation_keys_unique_per_mod(sample_id: str, sample: MuData) -> None:
+    """
+    Updating MuData.obs_names is not allowed (it is read-only).
+    So the observation keys for each modality has to be updated manually.
+    """
+    for mod in sample.mod.values():
+        mod.obs_names = f"{sample_id}_" + mod.obs_names
+
+def main():
+    input_data = read_h5mu(par["input"])
+    input_data.obs[par["obs_output"]] = par["input_id"]
+    if par["make_observation_keys_unique"]:
+        make_observation_keys_unique(par["input_id"], input_data)
+    logger.info("Writing out data to '%s'.", par["output"])
+    input_data.write(par["output"])
+
+if __name__ == '__main__':
+    main()
