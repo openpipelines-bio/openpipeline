d044be9f999002b29ab8bdae0639dc1b272bf952
Matthias Beyens <matthias.beyens@gmail.com>
2022-10-07 17:20:33 +0200

    initial development on popv component

---
+++ src/annotate/popv/script.py
@@ -0,0 +1,17 @@
+import mudata
+import popv
+from torch.cuda import is_available as cuda_is_available
+try:
+    from torch.backends.mps import is_available as mps_is_available
+except ModuleNotFoundError:
+    # Older pytorch versions
+    # MacOS GPUs
+    def mps_is_available():
+        return False
+
+### VIASH START
+par = {
+    "input": "resources_test/pbmc_1k_protein_v3/pbmc_1k_protein_v3_mms.h5mu"
+    }
+### VIASH END
+
