18787cfe9e7939bb174b6aabc63b4d6de046ff3d
Matthias Beyens <matthias.beyens@gmail.com>
2022-10-11 17:34:52 +0200

    added process_query and annotate_data, added ability user-provided reference

--- src/annotate/popv/script.py
+++ src/annotate/popv/script.py
@@ -5,6 +5,7 @@ import logging
 import scanpy
 import popv
 from torch.cuda import is_available as cuda_is_available
+import numpy as np
 
 try:
     from torch.backends.mps import is_available as mps_is_available
@@ -26,17 +27,19 @@ logger.addHandler(console_handler)
 ### VIASH START
 par = {
     # I/O params
-    "input": "resources_test/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix_rna_anndata.h5ad",
+    "input": "./resources_test/annotation_test_data/pbmc_1k_protein_v3_filtered_feature_bc_matrix_rna_anndata.h5ad",
     "output": "foo.h5ad",
     "compression": "gzip",
     # Component params
-    "tissue": "Blood",
+    "tissue_tabula_sapiens": "Blood",
+    "tissue_reference_file": "./resources_test/annotation_test_data/TS_Blood-subsampled.h5ad",
     "methods": ["scvi", "scanvi"],
-    "obs_covariate": "batch",
-    "obs_cell_type_key": "none",
-    "obs_cell_type_unknown_label": "unknown",
-    "reference_cell_type_key": "cell_ontology_class",
-    "reference_covariate": ["donor", "method"]
+    "query_obs_covariate": ["batch"],
+    "query_obs_cell_type_key": "none",
+    "query_obs_cell_type_unknown_label": "unknown",
+    "reference_obs_cell_type_key": "cell_ontology_class",
+    "reference_obs_covariate": ["donor", "method"],
+    "ontology_files_path": "./resources_test/annotation_test_data/popv_cl_ontology/"
     }
 ### VIASH END
 
@@ -111,18 +114,26 @@ def main():
     logger.info('Prepping input data for cell typing')
     
     # Query data
-    adata_query = scanpy.read_h5ad(par["input"].strip())    
-    adata_query.obs_names_make_unique()
-    # Disclaimer: Hack around cross-species mouse-human, so be cautious
+    adata_query = scanpy.read_h5ad(par["input"].strip())
+    # TODO: Hack around cross-species mouse-human, so be cautious
     adata_query.var_names = adata_query.var_names.str.upper()
+    adata_query.obs_names_make_unique()
+    adata_query.var_names_make_unique()
     adata_query = check_nonnegative_integers(adata_query)
 
     # Reference data
-    logger.info('Prepping Tabula Sapiens {} reference data for cell typing'.format(par["tissue"]))
-    refdata_url = get_tabula_sapiens_reference_url(par["tissue"])
-    local_refdata = 'tabula_sapiens_reference_{}.h5ad'.format(par["tissue"])
-    download_tabula_sapiens_reference_h5ad(refdata_url, local_refdata)
-    adata_reference = scanpy.read_h5ad(local_refdata)
+    # TODO: add possibility to come up with own reference data e.g. atlasses
+    if own_data:
+        logger.info('Prepping user-provided {} reference data for cell typing'.format(par["tissue_reference_file"].strip()))
+        adata_reference = scanpy.read_h5ad(par["tissue_reference_file"].strip())
+    else:
+        logger.info('Prepping Tabula Sapiens {} reference data for cell typing'.format(par["tissue_tabula_sapiens"]))
+        refdata_url = get_tabula_sapiens_reference_url(par["tissue_tabula_sapiens"])
+        local_refdata = 'tabula_sapiens_reference_{}.h5ad'.format(par["tissue_tabula_sapiens"])
+        download_tabula_sapiens_reference_h5ad(refdata_url, local_refdata)
+        adata_reference = scanpy.read_h5ad(local_refdata)
+    adata_reference.obs_names_make_unique()
+    adata_reference.var_names_make_unique()
     adata_reference = check_nonnegative_integers(adata_reference)
     ref_cell_types = adata_reference.obs['cell_type_tissue'].unique().to_list()
     logger.info('Reference data includes {} cell types {}'.format(len(ref_cell_types), ', '.join(ref_cell_types)))
@@ -130,20 +141,72 @@ def main():
     min_celltype_size = np.min(adata_reference.obs.groupby('cell_ontology_class').size())
     n_samples_per_label = np.max((min_celltype_size, 100))
     
+    common_markers = list(set(adata_reference.var.index).intersection(set(adata_query.var.index)))
+    logger.info(('Common features between query and reference data: {}'.format(len(common_markers))))
+    adata_query_intersection = adata_query[:, common_markers]
+    adata_reference_intersection = adata_reference[:, common_markers]
+    assert adata_query_intersection.var.index.shape == adata_reference_intersection.var.index.shape, 'Something went wrong in the process of finding common markers!'
     
+    # Lower mem footprint
+    del adata_reference
     
+    # TODO: Add highly variable genes param to component
+    # TODO: add training_mode to component
+    adata = popv.process_query(
+        adata_query_intersection,
+        adata_reference_intersection,
+        save_folder='.',
+        query_batch_key='batch',
+        query_labels_key=None,
+        unknown_celltype_label='unknown',
+        pretrained_scvi_path=None,
+        training_mode='offline',
+        ref_labels_key="cell_ontology_class",
+        ref_batch_key=['donor', 'method'],
+        n_samples_per_label=n_samples_per_label,
+        hvg=True)
+    
+    # Lower mem footprint
+    del query_adata_intersection
+    del ref_adata_intersection
+
+    popv.annotate_data(
+        adata,
+        methods,
+        '.',
+        pretrained_scvi_path=pretrained_scvi_path,
+        pretrained_scanvi_path=pretrained_scanvi_path,
+        onclass_ontology_file="{}/cl.ontology".format(par["ontology_files_path"]),
+        onclass_obo_fp="{}/cl.obo".format(par["ontology_files_path"]),
+        onclass_emb_fp="{}/cl.ontology.nlp.emb".format(par["ontology_files_path"]),
+        scvi_max_epochs=None,
+        scanvi_max_epochs=None,
+        use_gpu=(cuda_is_available() or mps_is_available())
+        )
     
 if __name__ == "__main__":
     logger.info('PopV cell type annotation component')
+    
+    if len(par["tissue_tabula_sapiens"]) >= 1:
+        logger.info('Cell typing done on Tabula Sapiens reference data.')
+        own_data = False
+    elif len(par["tissue_reference_file"]) >= 1:
+        logger.info('Cell typing done on user-provided provided reference data.')
+        own_data = True
+    else:
+        raise BaseException("Please, tissue_tabula_sapiens or tissue_reference_file...")
     
     if not isinstance(par["methods"], list):
         par["methods"] = par["methods"].replace(" ", "").split(',')
     assert len(par["methods"]) >= 1, 'Please, specify at least one method for cell typing.'
     logger.info("Cell typing methods: {}".format(par["methods"]))
 
-    if not par["obs_cell_type_key"] == 'none':
-        assert isinstance(par["obs_cell_type_unknown_label"], str), 'Please, specify unknown cell type label in your .obs obs_cell_type_key.'
+    if not par["query_obs_cell_type_key"] == 'none':
+        assert isinstance(par["query_obs_cell_type_unknown_label"], str), 'Please, specify unknown cell type label in your .obs obs_cell_type_key.'
     else:
-        par["obs_cell_type_key"] = None
+        par["query_obs_cell_type_key"] = None
+        
+    if len(par["ontology_files_path"]) == 0:
+        raise BaseException("Please, specify location of CL ontology files for PopV.")
 
     main()
--- src/annotate/popv/test.py
+++ src/annotate/popv/test.py
@@ -8,11 +8,12 @@ import numpy as np
 ## VIASH START
 meta = {
     'executable': './target/native/annotate/popv',
-    'resources_dir': './resources_test/pbmc_1k_protein_v3/'
+    'resources_dir': './resources_test/annotation_test_data/'
 }
 ## VIASH END
 
 input_file = f"{meta['resources_dir']}/pbmc_1k_protein_v3_filtered_feature_bc_matrix_rna_anndata.h5ad"
+ontology_files_path = f"{meta['resources_dir']}/popv_cl_ontology"
 
 class TestHarmonyPy(unittest.TestCase):
     def _run_and_check_output(self, args_as_list):
@@ -28,13 +29,15 @@ class TestHarmonyPy(unittest.TestCase):
             "--input", input_file,
             "--output", "output.h5mu",
             "--compression", "gzip",
-            "--tissue", "Blood",
+            "--tissue_tabula_sapiens", "Blood",
+            "--tissue_reference_file", "./resources_test/annotation_test_data/TS_Blood-subsampled.h5ad",
             "--methods", "scvi,scanvi",
-            "--obs_covariate", "batch",
-            "--obs_cell_type_key": "none",
-            "--obs_cell_type_unknown_label": "unknown",
-            "--reference_cell_type_key": "cell_ontology_class",
-            "--reference_covariate": "donor,method"
+            "--query_obs_covariate", "batch",
+            "--query_obs_cell_type_key": "none",
+            "--query_obs_cell_type_unknown_label": "unknown",
+            "--reference_obs_cell_type_key": "cell_ontology_class",
+            "--reference_obs_covariate": "donor,method",
+            "--ontology_files_path": ontology_files_path
             ])
         #self.assertTrue(Path("output.h5mu").is_file())
         #output_data = mudata.read_h5mu("output.h5mu")
