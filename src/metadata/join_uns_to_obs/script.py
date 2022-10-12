53b84fac801dea47de1e253b601bf09028699fda
Robrecht Cannoodt <rcannood@gmail.com>
2022-10-12 15:43:52 +0200

    fix join uns component

--- src/metadata/join_uns_to_obs/script.py
+++ src/metadata/join_uns_to_obs/script.py
@@ -1,6 +1,5 @@
 from mudata import read_h5mu
 import pandas as pd
-
 import logging
 from sys import stdout
 
@@ -14,7 +13,7 @@ logger.addHandler(console_handler)
 ### VIASH START
 par = {
     "input": "work/f5/5f6365898ca5a42a360301a0c9e200/TSP15_Eye_ScleraEtc_10X_2_1.add_id.output.h5mu",
-    "uns_key": "data",
+    "uns_key": "metrics_cellranger",
     "output": "foo.h5mu",
     "modality": "rna"
 }
@@ -26,7 +25,19 @@ mdata = read_h5mu(par['input'])
 mod_data = mdata.mod[par['modality']]
 
 logger.info("Joining uns to obs")
-mod_data.obs = mod_data.obs.join(mod_data.uns[par['uns_key']])
+# get data frame
+uns_df = mod_data.uns[par['uns_key']]
+
+# check for overlapping colnames
+intersect_keys = uns_df.keys().intersection(mod_data.obs.keys())
+obs_drop = mod_data.obs.drop(intersect_keys, axis=1)
+
+# create data frame to join
+uns_df_rep = uns_df.loc[uns_df.index.repeat(mod_data.n_obs)]
+uns_df_rep.index = mod_data.obs_names
+
+# create new obs
+mod_data.obs = pd.concat([obs_drop, uns_df_rep], axis=1)
 
 logger.info("Write output to mudata file")
 mdata.write_h5mu(par['output'])
