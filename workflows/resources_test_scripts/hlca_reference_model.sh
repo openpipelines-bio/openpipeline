43dd57687eb1dce8524dd0b4f9d4ec41c5728359
Vladimir Shitov <vladimir.shitov@helmholtz-muenchen.de>
2022-10-26 12:14:52 +0200

    Preprocess test query file

--- workflows/resources_test_scripts/hlca_reference_model.sh
+++ workflows/resources_test_scripts/hlca_reference_model.sh
@@ -27,6 +27,33 @@ wget https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM5230nnn/GSM5230027/suppl/GSM523
   -O "${OUT}_query_test.h5.gz"
 gzip -d "${OUT}_query_test.h5.gz"
 
+# Prepare test data as in scvi-tools tutorial: https://docs.scvi-tools.org/en/stable/tutorials/notebooks/query_hlca_knn.html
+python <<HEREDOC
+import pandas as pd
+import scanpy as sc
+
+geo_metadata_url = "https://ftp.ncbi.nlm.nih.gov/geo/series/GSE171nnn/GSE171668/suppl/GSE171668_lung_metadata.csv.gz"
+metadata = pd.read_csv(geo_metadata_url, index_col=0)
+
+DATA_PATH = "${OUT}_query_test.h5"
+query_data = sc.read_10x_h5(DATA_PATH)
+# clean up .var.index (gene names)
+query_data.var['gene_names'] = query_data.var.index
+query_data.var.index = [idx.split("___")[-1] for idx in query_data.var.gene_ids]
+# clean up cell barcodes:
+query_data.obs.index = query_data.obs.index.str.rstrip("-1")
+# read in metadata (to select only cells of interest and remove empty drops)
+# subset to cells from our sample
+metadata = metadata.loc[metadata.donor == "D12_4",:].copy()
+# clean up barcodes:
+metadata.index = [idx.split("-")[-1] for idx in metadata.index]
+# subset adata to cells in metadata:
+query_data = query_data[metadata.index,:].copy()
+# add dataset information:
+query_data.obs['dataset'] = "test_dataset_delorey_regev"
+sc.write(DATA_PATH, query_data)
+HEREDOC
+
 # convert 10x h5 to h5mu
 bin/viash run src/convert/from_h5ad_to_h5mu/config.vsh.yaml -- \
   --input "${OUT}_query_test.h5" \
