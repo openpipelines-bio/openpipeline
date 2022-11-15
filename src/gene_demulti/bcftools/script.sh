a9f08801b636dcdc9290b5b4175bca6a8b797219
wxicu <xichenwu@outlook.com>
2022-11-16 00:19:24 +0100

    add bcftools

---
+++ src/gene_demulti/bcftools/script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ ! -d "$par_output" ]; then
+  mkdir $par_output
+fi
+
+IFS="," read -a vcf_list <<< $par_vcf
+echo ${vcf_list[@]} > ${par_output}out.txt
+
+
+if [ "$par_concat" = true ] && [ "$par_filter" = true ] ; then
+  bcftools concat -o ${par_output}concated_chroms.vcf ${vcf_list[@]}
+  bcftools sort ${par_output}concated_chroms.vcf -o ${par_output}sorted_concated_chroms.vcf
+  bcftools filter -i '%QUAL>30' ${par_output}sorted_concated_chroms.vcf -o ${par_output}filtered_sorted_concated_chroms.vcf
+  
+elif [ "$par_filter" = true ] ; then
+    bcftools filter -i '%QUAL>30' ${vcf_list[@]} -o ${par_output}filtered.vcf
+    
+else
+    bcftools concat  -o ${par_output}concated_chroms.vcf ${vcf_list[@]}
+    bcftools sort ${par_output}concated_chroms.vcf -o ${par_output}sorted_concated_chroms.vcf
+
+fi
