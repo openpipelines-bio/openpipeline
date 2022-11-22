e3c3b47522712a5ac884b8d4baeec6f20ea89851
wxicu <xichenwu@outlook.com>
2022-11-22 10:22:36 +0100

    fix samtools bug

--- src/gene_demulti/samtools/script.sh
+++ src/gene_demulti/samtools/script.sh
@@ -2,9 +2,9 @@
 if [ ! -d "$par_output" ]; then
   mkdir $par_output
 fi
-
-samtools view -S -b -q 10 -F 3844 $par_bam > ${par_output}filtered.bam
-samtools index ${par_output}filtered.bam ${par_output}filtered.bam.bai
-umi_tools dedup --stdin=${par_output}filtered.bam --extract-umi-method=tag --umi-tag=UR --cell-tag=CB --log=logfile > ${par_output}no_dup.bam
-samtools sort ${par_output}no_dup.bam -o ${par_output}sorted.bam
-samtools index ${par_output}sorted.bam ${par_output}sorted.bam.bai
+cd $par_output
+samtools view -S -b -q 10 -F 3844 ../$par_bam > filtered.bam
+samtools index filtered.bam filtered.bam.bai
+umi_tools dedup --stdin=filtered.bam --extract-umi-method=tag --umi-tag=UR --cell-tag=CB --log=logfile > no_dup.bam
+samtools sort no_dup.bam -o sorted.bam
+samtools index sorted.bam sorted.bam.bai
