b44f6782bb3dcb3da6e08e8a6f5cf9c5d3ac4255
wxicu <xichenwu@outlook.com>
2023-01-20 02:10:40 +0100

    use smaller genome ref for test

--- src/gene_demulti/freebayes/test.sh
+++ src/gene_demulti/freebayes/test.sh
@@ -3,10 +3,10 @@ echo "$meta_resources_dir"
 echo ">>> Running executable"
 $meta_executable \
     --bam "$meta_resources_dir/demuxafy_test_data/pooled.sorted.bam" \
-    --fasta_reference "$meta_resources_dir/demuxafy_test_data/genome.fa" \
-    --region 1 --output freebayes_result/ \
-    --vcf mixed_variant_chr1.vcf
+    --fasta_reference "$meta_resources_dir/cellranger_tiny_fastq/cellranger_tiny_ref/fasta/genome.fa" \
+    --output freebayes_result/ --region chr21.part \
+    --vcf mixed_variant.vcf
 
-[[ ! -f freebayes_result/mixed_variant_chr1.vcf ]] && echo "Output VCF file could not be found!" && exit 1
+[[ ! -f freebayes_result/mixed_variant.vcf ]] && echo "Output VCF file could not be found!" && exit 1
 
 echo ">>> Test finished successfully"
--- src/gene_demulti/souporcell/test.sh
+++ src/gene_demulti/souporcell/test.sh
@@ -4,7 +4,7 @@ echo ">>> Running executable"
 $meta_executable \
     --bam "$meta_resources_dir/demuxafy_test_data/pooled.sorted.bam" \
     --bam_index "$meta_resources_dir/demuxafy_test_data/pooled.sorted.bam.bai" \
-    --fasta "$meta_resources_dir/demuxafy_test_data/genome.fa" \
+    --fasta "$meta_resources_dir/cellranger_tiny_fastq/cellranger_tiny_ref/fasta/genome.fa" \
     --barcodes "$meta_resources_dir/demuxafy_test_data/barcodes.tsv" \
     --common_variants "$meta_resources_dir/demuxafy_test_data/test_dataset.vcf" \
     --clusters 14  --threads 3 --skip_remap \
