2e9498677c096327471297ac8b16693c17bb0a58
DriesSchaumont <5946712+DriesSchaumont@users.noreply.github.com>
2023-03-23 15:26:21 +0100

    Fix missing backslash

--- workflows/ingestion/cellranger_multi/integration_test.sh
+++ workflows/ingestion/cellranger_multi/integration_test.sh
@@ -16,5 +16,5 @@ nextflow \
   -entry test_wf \
   -resume \
   -profile docker,no_publish \
-  -c workflows/utils/labels_ci.config
+  -c workflows/utils/labels_ci.config \
   -with-trace work/trace.txt
