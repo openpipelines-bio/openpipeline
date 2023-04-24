762575a8f5e52add5ed46f45d4895000873acd8e
Dries Schaumont <5946712+DriesSchaumont@users.noreply.github.com>
2023-04-24 12:45:54 +0200

    Add allow_automatic_threshold_detection_fail to scrublet (#370)
    * Add allow_automatic_threshold_detection_fail to scrublet
    
    * switch to pytest
    
    ---------
    
    Co-authored-by: Robrecht Cannoodt <rcannood@gmail.com>

--- src/filter/filter_with_scrublet/script.py
+++ src/filter/filter_with_scrublet/script.py
@@ -3,6 +3,7 @@ import mudata as mu
 import numpy as np
 import logging
 from sys import stdout
+import pandas as pd 
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -13,10 +14,11 @@ logger.addHandler(console_handler)
 
 ### VIASH START
 par = {
-    # "input": "resources_test/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5mu",
-    "input": "output/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5mu.h5mu",
-    "modality": [ "rna" ],
+    "input": "resources_test/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5mu",
+    # "input": "output/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5mu.h5mu",
+    "modality": "rna",
     "output": "output.h5mu",
+    "output_compression": "gzip",
     "obs_name_filter": "filter_with_scrublet",
     # "expected_doublet_rate": 0.05,
     "min_counts": 2,
@@ -51,16 +53,33 @@ doublet_scores, predicted_doublets = scrub.scrub_doublets(
     distance_metric=par["distance_metric"],
     use_approx_neighbors=False
 )
-keep_cells = np.invert(predicted_doublets)
+
+try:
+    keep_cells = np.invert(predicted_doublets)
+except TypeError:
+    if par['allow_automatic_threshold_detection_fail']:
+        # Scrublet might not throw an error and return None if it fails to detect doublets...
+        logger.info("\tScrublet could not automatically detect the doublet score threshold. Setting output columns to NA.")
+        keep_cells = np.nan
+        doublet_scores = np.nan
+    else:
+        raise RuntimeError("Scrublet could not automatically detect the doublet score threshold. "
+                           "--allow_automatic_threshold_detection_fail can be used to ignore this failure "
+                           "and set the corresponding output columns to NA.")
 
 logger.info("\tStoring output into .obs")
 if par["obs_name_doublet_score"] is not None:
     data.obs[par["obs_name_doublet_score"]] = doublet_scores
+    data.obs[par["obs_name_doublet_score"]] = data.obs[par["obs_name_doublet_score"]].astype("float64")
 if par["obs_name_filter"] is not None:
     data.obs[par["obs_name_filter"]] = keep_cells
+    data.obs[par["obs_name_filter"]] = data.obs[par["obs_name_filter"]].astype(pd.BooleanDtype())
 
 if par["do_subset"]:
-    mdata.mod[mod] = data[keep_cells, :]
+    if pd.api.types.is_scalar(keep_cells) and pd.isna(keep_cells):
+        logger.warning("Not subsetting beacuse doublets were not predicted")
+    else: 
+        mdata.mod[mod] = data[keep_cells, :]
 
 logger.info("Writing h5mu to %s", par["output"])
 mdata.write_h5mu(par["output"], compression=par["output_compression"])
