bba03cb93e2d39167be55297f1df8fbc687b6b0f
Vladimir Shitov <vladimir.shitov@helmholtz-muenchen.de>
2023-05-05 15:26:15 +0200

    Test mapping of data on itself

--- src/integrate/totalvi/test.py
+++ src/integrate/totalvi/test.py
@@ -10,13 +10,11 @@ import pandas as pd
 ## VIASH START
 meta = {
     'executable': './target/docker/integrate/totalvi/totalvi',
-    'resources_dir': './resources_test/scvi_tools/'
+    'resources_dir': './resources_test/pbmc_1k_protein_v3/'
 }
 ## VIASH END
 
-query = f"{meta['resources_dir']}/scvi_tools_pancreas_query_test.h5mu"
-reference = f"{meta['resources_dir']}/scvi_tools_pancreas_ref_test.h5mu"
-
+input_file = f"{meta['resources_dir']}/pbmc_1k_protein_v3_mms.h5mu"
 
 class TesttotalVI(unittest.TestCase):
     def _run_and_check_output(self, args_as_list):
@@ -26,33 +24,25 @@ class TesttotalVI(unittest.TestCase):
             print(e.stdout.decode("utf-8"))
             raise e
 
-    def test_totalvi(self):   
-        # Create some fake protein data
-        with NamedTemporaryFile('w', suffix=".h5mu") as tempfile_ref:
-            ref_data = mudata.read_h5mu(reference)
-            protein_data = pd.DataFrame(
-                columns=["a", "b", "c"],
-                index=ref_data.mod["rna"].obs_names,
-                data=np.random.randint(1, 10, size=(ref_data.n_obs, 3))
-            )
+    def test_totalvi(self):
+        """Map data containing proteins on itself"""
+        self._run_and_check_output([
+            "--query", input_file,
+            "--reference", input_file,
+            "--query_proteins_key", "prot",
+            "--reference_proteins_key", "prot",
+            "--var_input" "filter_with_hvg",
+            "--reference_model_path", "totalvi_reference_model/",
+            "--query_model_path", "totalvi_query_model/",
+            "--max_epochs", 1,
+            "--max_query_epochs", 1,
+            "--output", "output.h5mu"])
 
-            ref_data.mod["rna"].obsm["protein_expression"] = protein_data
-            mudata.write(tempfile_ref.name, ref_data)
-
-            self._run_and_check_output([
-                "--query", query,
-                "--reference", tempfile_ref.name,
-                "--modality", "rna",
-                "--obs_batch", "batch",
-                "--output", "output.h5mu",
-                "--query_proteins_key", "protein_expression",
-                "--max_epochs", "1"])
-
-            self.assertTrue(Path("output.h5mu").is_file())
-            output_data = mudata.read_h5mu("output.h5mu")
-            self.assertIn('X_scvi_integrated', output_data.mod['rna'].obsm)
-            self.assertIn('_scvi_batch', output_data.mod['rna'].obs.columns.tolist())
-            self.assertIn('_scvi_labels',  output_data.mod['rna'].obs.columns.tolist())
+        self.assertTrue(Path("output.h5mu").is_file())
+        output_data = mudata.read_h5mu("output.h5mu")
+        self.assertIn('X_integrated_totalvi', output_data.mod['rna'].obsm)
+        self.assertIn('_scvi_batch', output_data.mod['rna'].obs.columns.tolist())
+        self.assertIn('_scvi_labels',  output_data.mod['rna'].obs.columns.tolist())
 
 
 if __name__ == '__main__':
