965b391f449d2ed27f265eed505378354285ba33
DriesSchaumont <5946712+DriesSchaumont@users.noreply.github.com>
2023-06-08 15:00:44 +0200

    Rework mermaid component

---
+++ src/report/mermaid/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mmdc -p "$meta_resources_dir/puppeteer-config.json" \
+    -i "$par_input" \
+    -o "$par_output" \
+    --width "$par_width" \
+    --height "$par_height" \
+    ${par_background_color:+--backgroundColor $par_background_color} \
+    ${output_format:+--outputFormat $output_format}
+
