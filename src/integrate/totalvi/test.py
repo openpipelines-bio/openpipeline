17d23a4f9f797e6e2fd543bef08c906397e3caf2
Vladimir Shitov <vladimir.shitov@helmholtz-muenchen.de>
2023-06-12 15:34:43 +0200

    Unify quotes style

--- src/integrate/totalvi/test.py
+++ src/integrate/totalvi/test.py
@@ -6,8 +6,8 @@ import mudata
 
 ## VIASH START
 meta = {
-    'executable': './target/docker/integrate/totalvi/totalvi',
-    'resources_dir': './resources_test/pbmc_1k_protein_v3/'
+    "executable": "./target/docker/integrate/totalvi/totalvi",
+    "resources_dir": "./resources_test/pbmc_1k_protein_v3/"
 }
 ## VIASH END
 
@@ -16,7 +16,7 @@ input_file = f"{meta['resources_dir']}/pbmc_1k_protein_v3_mms.h5mu"
 class TesttotalVI(unittest.TestCase):
     def _run_and_check_output(self, args_as_list):
         try:
-            subprocess.check_output([meta['executable']] + args_as_list)
+            subprocess.check_output([meta["executable"]] + args_as_list)
         except subprocess.CalledProcessError as e:
             print(e.stdout.decode("utf-8"))
             raise e
@@ -37,12 +37,12 @@ class TesttotalVI(unittest.TestCase):
 
         self.assertTrue(Path("output.h5mu").is_file())
         output_data = mudata.read_h5mu("output.h5mu")
-        self.assertIn('X_integrated_totalvi', output_data.mod['rna'].obsm)
-        self.assertIn('_scvi_batch', output_data.mod['rna'].obs.columns.tolist())
-        self.assertIn('_scvi_labels',  output_data.mod['rna'].obs.columns.tolist())
-        self.assertIn('X_totalvi_normalized_rna', output_data.mod['rna'].obsm)
-        self.assertIn('X_totalvi_normalized_protein', output_data.mod['rna'].obsm)
+        self.assertIn("X_integrated_totalvi", output_data.mod["rna"].obsm)
+        self.assertIn("_scvi_batch", output_data.mod["rna"].obs.columns.tolist())
+        self.assertIn("_scvi_labels",  output_data.mod["rna"].obs.columns.tolist())
+        self.assertIn("X_totalvi_normalized_rna", output_data.mod["rna"].obsm)
+        self.assertIn("X_totalvi_normalized_protein", output_data.mod["rna"].obsm)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
