dc5dab2da858b172b0f4d9f5b70e3a6b35eabb25
Matthias Beyens <mbeyens@its.jnj.com>
2023-06-15 10:40:35 +0200

    extend test cases

--- src/query/cellxgene_census/test.py
+++ src/query/cellxgene_census/test.py
@@ -1,7 +1,8 @@
 import sys
 import os
 import pytest
-import unittest
+import mudata as md
+
 
 ## VIASH START
 meta = {
@@ -12,7 +13,7 @@ meta = {
 OUTPUT_FILE = "output.h5mu"
 
 
-def test_cellxgene(run_component):
+def test_cellxgene_extract_metadata(run_component):
     run_component([
         "--input_database", "CellxGene",
         "--modality", "rna",
@@ -27,5 +28,29 @@ def test_cellxgene(run_component):
     # check whether file exists
     assert os.path.exists(OUTPUT_FILE), "Output file does not exist"
 
+    component_data = md.read(OUTPUT_FILE)
+    obs = component_data.mod['rna'].obs
+    assert "soma_joinid" in obs
+
+def test_cellxgene_extract_metadata_expression(run_component):
+    run_component([
+        "--input_database", "CellxGene",
+        "--modality", "rna",
+        "--cellxgene_release", "2023-05-15",
+        "--species", "homo_sapiens",
+        "--tissue", "lung",
+        "--obs_column_names", "disease",
+        "--output", OUTPUT_FILE,
+        "--metadata_only", "False"
+    ])
+
+    # check whether file exists
+    assert os.path.exists(OUTPUT_FILE), "Output file does not exist"
+
+    component_data = md.read(OUTPUT_FILE)
+    var, obs = component_data.mod['rna'].var, component_data.mod['rna'].obs
+    assert "soma_joinid" in var
+    assert "soma_joinid" in obs
+    
 if __name__ == '__main__':
     sys.exit(pytest.main([__file__]))
