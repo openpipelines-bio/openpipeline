32f2e2ab74fc667ed1ccbcc1831c018c4160890c
Robrecht Cannoodt <rcannood@gmail.com>
2023-07-14 12:13:01 +0200

    `velocity/velocyto`: Resolve symbolic link before checking whether the transcriptome is a gzip.

--- src/velocity/velocyto/script.sh
+++ src/velocity/velocyto/script.sh
@@ -27,7 +27,9 @@ fi
 output_dir=`dirname "$par_output"`
 sample_id=`basename "$par_output" .loom`
 
-if (file "$par_transcriptome" | grep -q compressed ) ; then
+
+
+if (file `readlink -f "$par_transcriptome"` | grep -q compressed ) ; then
   # create temporary directory
   tmpdir=$(mktemp -d "$meta_temp_dir/$meta_functionality_name-XXXXXXXX")
   function clean_up {
