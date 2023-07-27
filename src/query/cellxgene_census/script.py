c824b89f63d69585e4b42761947c8437e862ce17
Dries De Maeyer <ddemaeyer@gmail.com>
2023-07-27 15:17:34 +0200

    Removed querying logic from component

--- src/query/cellxgene_census/script.py
+++ src/query/cellxgene_census/script.py
@@ -22,15 +22,9 @@ par = {
     "modality": "rna",
     "cellxgene_release": "2023-05-15",
     "species": "homo_sapiens",
-    "cell_type": ["mesothelial fibroblast"],
-    "tissue": None,
-    "technology": None,
-    "suspension": None,
-    "is_primary_data": True,
-    "obs_column_names": ["disease"],
+    "cell_query": "is_primary_data == True and cell_type_ontology_term_id in ['CL:0000136', 'CL:1000311', 'CL:0002616', 'CL:0002617', 'CL:0002615', 'CL:0001070', 'CL:1000310', 'CL:0000449', 'CL:1000309', 'CL:0002521', 'CL:0000448'] and assay_ontology_term_id in ['EFO:0009922', 'EFO:0009899', 'EFO:0011025', 'EFO:0030004', 'EFO:0030003'] and development_stage_ontology_term_id in ['HsapDv:0000084', 'HsapDv:0000098', 'HsapDv:0000097', 'HsapDv:0000099', 'HsapDv:0000096'] and suspension_type == 'cell'",
     "output": "output.h5mu",
     "output_compression": "gzip",
-    "metadata_only": False
 }
 
 meta = {'resources_dir': os.path.abspath('./src/query/cellxgene_census/')}
@@ -56,98 +50,9 @@ def connect_census(input_database, release):
         )
 
 
-def read_cell_ontology(obo_file):
-    """Reads Cell Type OBO Foundry file
-
-    Returns:
-        graph: cell type obo ontology
-    """
-    return obonet.read_obo(
-        obo_file,
-        encoding="utf-8"
-    )
-
-
-def get_child_terms_from_ontology(cell_types, ontology, relations_to_consider = None):
-    
-    if relations_to_consider:
-        def filter_edge(n1, n2, e):
-            return e in relations_to_consider
-        
-        ontology = networkx.subgraph_view(ontology, filter_edge = filter_edge)
-    
-    id_to_name = {id_: data.get('name') for id_, data in ontology.nodes(data=True)}
-    name_to_id = {data['name']: id_ for id_, data in ontology.nodes(data=True) if 'name' in data}
-
-    def flatten_extend(matrix):
-        flat_list = []
-        for row in matrix:
-            flat_list.extend(row)
-        return flat_list
-    
-    return list(set([cell_types[0]] + flatten_extend([sorted(subterm for subterm in networkx.ancestors(ontology, name_to_id[ct])) for ct in cell_types ])))
-
-# TODO: function to explore cell types available in query data
-# def view_available_cell_types(lower_hierarchy_cell_of_interest_map, cell_of_interest_terms):
-#     cells_of_interest_query_terms = list(adata.obs.cell_type_ontology_term_id.unique())
-#     avail_cells_overview = []
-#     for term in cell_of_interest_terms:
-#         if term in cells_of_interest_query_terms:
-#             avail_cells_overview.append("Available: {}-{}".format(term, lower_hierarchy_cell_of_interest_map[term]))
-#         else:
-#             avail_cells_overview.append("Unavailable: {}-{}".format(term, lower_hierarchy_cell_of_interest_map[term]))
-#     avail_cells_overview.sort()
-#     logging.info(avail_cells_overview)
-
-
-def build_census_query(par, obo_file):
-    _query = f'is_primary_data == {par["is_primary_data"]}'
-    query_builder = {
-        'cell_type': f' and cell_type_ontology_term_id in {get_child_terms_from_ontology(cell_types = par["cell_type"], ontology = obo_file, relations_to_consider = ["is_a"])}',
-        'tissue': f' and tissue in {par["tissue"]}',
-        'technology': f' and assay in {par["technology"]}',
-        'suspension': f' and suspension_type in {par["suspension"]}',
-    }
-    for parameter_name, query_part in query_builder.items():
-        if par[parameter_name]:
-            _query += query_part
-
-    return _query
-
-
-def extract_metadata(census_connection, query, species, obs_column_names):
-    logger.info("Extracting only metadata")
-
-    query_data = census_connection["census_data"][species].obs.read(
-        value_filter=query,
-        column_names=obs_column_names).concat().to_pandas()
-
-    return ad.AnnData(obs=query_data)
-
-
-def extract_metadata_expression(
-    census_connection, query,species, obs_column_names):
-    logger.info("Extracting metadata and gene expression matrix")
-
-    query_data = cellxgene_census.get_anndata(
-        census_connection,
-        organism = species,
-        obs_value_filter = query,
-        column_names = {"obs": obs_column_names}
-        )
-
-    query_data.X = csr_matrix(query_data.X)
-    query_data.var_names_make_unique()
-
-    return query_data
-
-
 def write_mudata(mdata, output_location, compression):
     logger.info("Writing %s", output_location)
-    mdata.write_h5mu(
-        output_location,
-        compression = compression
-        )
+    
 
 
 def main():
@@ -155,19 +60,13 @@ def main():
     census_connection = connect_census(
         par["input_database"],
         par["cellxgene_release"]
-        )
-    query = build_census_query(par, f"{meta['resources_dir']}/cl-base.obo")
+        ) 
 
-    if par["metadata_only"]:
-        query_data = extract_metadata(
-            census_connection, query, par["species"], par["obs_column_names"]
-            )
-
-    else:
-        query_data = extract_metadata_expression(
-            census_connection, query, par["species"], par["obs_column_names"]
-            )
-
+    query_data = cellxgene_census.get_anndata(
+        census = census_connection,
+        obs_value_filter = par["cell_query"],
+        organism = par["organism"]
+    )
     census_connection.close()
     del census_connection
 
--- src/query/cellxgene_census/test.py
+++ src/query/cellxgene_census/test.py
@@ -41,11 +41,8 @@ def test_cellxgene_extract_metadata_expression(run_component):
         "--modality", "rna",
         "--cellxgene_release", "2023-05-15",
         "--species", "homo_sapiens",
-        "--tissue", "eye",
-        "--obs_column_names", "disease",
-        "--cell_type", "neuron",
+        "--cell_query", "is_primary_data == True and cell_type_ontology_term_id in ['CL:0000136', 'CL:1000311', 'CL:0002616', 'CL:0002617', 'CL:0002615', 'CL:0001070', 'CL:1000310', 'CL:0000449', 'CL:1000309', 'CL:0002521', 'CL:0000448'] and assay_ontology_term_id in ['EFO:0009922', 'EFO:0009899', 'EFO:0011025', 'EFO:0030004', 'EFO:0030003'] and development_stage_ontology_term_id in ['HsapDv:0000084', 'HsapDv:0000098', 'HsapDv:0000097', 'HsapDv:0000099', 'HsapDv:0000096'] and suspension_type == 'cell'"
         "--output", OUTPUT_FILE,
-        "--metadata_only", "False"
     ])
 
     # check whether file exists
