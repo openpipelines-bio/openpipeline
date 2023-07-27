e8c8783678f4f3b1dd20fcb4845bd66fdf46216a
Dries De Maeyer <ddemaeyer@gmail.com>
2023-07-27 16:59:16 +0200

    Removed metadata test

--- src/query/cellxgene_census/test.py
+++ src/query/cellxgene_census/test.py
@@ -14,27 +14,6 @@ meta = {
 OUTPUT_FILE = "output.h5mu"
 
 
-def test_cellxgene_extract_metadata(run_component):
-    run_component([
-        "--input_database", "CellxGene",
-        "--modality", "rna",
-        "--cellxgene_release", "2023-05-15",
-        "--species", "homo_sapiens",
-        "--tissue", "mesothelial fibroblast",
-        "--obs_column_names", "disease",
-        "--output", OUTPUT_FILE,
-        "--metadata_only", "True"
-    ])
-
-    # check whether file exists
-    assert os.path.exists(OUTPUT_FILE), "Output file does not exist"
-
-    component_data = md.read(OUTPUT_FILE)
-    assert 'rna' in component_data.mod, "Output should contain 'rna' modality."
-    obs = component_data.mod['rna'].obs
-    var = component_data.mod['rna'].var
-    assert var.empty
-
 def test_cellxgene_extract_metadata_expression(run_component):
     run_component([
         "--input_database", "CellxGene",
