2594d070c3ee978d49513d54bd0f35a3d1644f53
Robrecht Cannoodt <rcannood@gmail.com>
2023-08-17 12:49:05 +0200

    simplify svgs

--- images/concepts/generate_subimages.sh
+++ images/concepts/generate_subimages.sh
@@ -5,4 +5,5 @@ rm images/concepts/fig_*.svg
 
 for id in cell modality_rna modality_adt modality_vdj modality_atac workflow_multiomics_rna_singlesample workflow_multiomics_rna_multisample workflow_multiomics_adt_singlesample workflow_multiomics_adt_multisample; do
   inkscape --export-type="svg" --export-id="$id" --export-id-only images/concepts/fig.svg
+  svgo images/concepts/fig_${id}.svg
 done
