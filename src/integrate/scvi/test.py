234b988f62fceb288584ed8c4ed649674665cc10
Robrecht Cannoodt <rcannood@gmail.com>
2023-08-30 10:49:21 +0200

    rework unit test

--- src/integrate/scvi/test.py
+++ src/integrate/scvi/test.py
@@ -18,79 +18,61 @@ from subset_vars import subset_vars
 
 input_file = f"{meta['resources_dir']}/pbmc_1k_protein_v3_mms.h5mu"
 
-def test_scvi(run_component):
-    with NamedTemporaryFile('w', suffix=".h5mu") as tempfile_input_file:
-        input_data = mudata.read_h5mu(input_file)
-        input_rna = input_data.mod['rna']
-        input_rna.obs['batch'] = 'A'
-        column_index = input_rna.obs.columns.get_indexer(['batch'])
-        input_rna.obs.iloc[slice(input_rna.n_obs//2, None), column_index] = 'B'
-        input_data.write(tempfile_input_file.name)
+@pytest.fixture
+def mudata_with_mod_rna_obs_batch(tmp_path, request):
+    obs_batch, var_input, obsm_output = request.param
 
-        run_component([
-            "--input", tempfile_input_file.name,
-            "--modality", "rna",
-            "--obs_batch", "batch",
-            "--output", "output.h5mu",
-            "--model_output", "test/",
-            "--max_epochs", "1",
-            "--n_obs_min_count", "10",
-            "--n_var_min_count", "10",
-            "--output_compression", "gzip"])
-        
-        # check files
-        assert Path("output.h5mu").is_file(), "Output file does not exist"
-        assert Path("test").is_dir()
-        assert Path("test/model.pt").is_file()
+    new_input_file = tmp_path / "input.h5mu"
+    
+    input_data = mudata.read_h5mu(input_file)
+    input_rna = input_data.mod['rna']
+    input_rna.obs[obs_batch] = 'A'
+    column_index = input_rna.obs.columns.get_indexer([obs_batch])
+    input_rna.obs.iloc[slice(input_rna.n_obs//2, None), column_index] = 'B'
+    input_data.write(new_input_file.name)
 
-        # check output h5mu
-        output_data = mudata.read_h5mu("output.h5mu")
-        output_rna = output_data.mod['rna']
-        assert output_rna.n_obs == input_rna.n_obs, f"Number of observations changed\noutput_data: {output_data}"
-        assert output_rna.n_vars == input_rna.n_vars, f"Number of variables changed\noutput_data: {output_data}"
-        assert 'X_scvi_integrated' in output_rna.obsm, f"X_scvi_integrated not added\noutput_data: {output_data}"
+    return new_input_file.name, input_rna, obs_batch, var_input, obsm_output
 
-        # assert that nothing else has changed
-        del output_rna.obsm["X_scvi_integrated"]
-        assert_equal(input_rna, output_rna)
+@pytest.mark.parametrize("mudata_with_mod_rna_obs_batch", [("batch", None, None), ("batch2", "filter_with_hvg", "X_int")], indirect=True)
+def test_scvi(run_component, mudata_with_mod_rna_obs_batch):
+    new_input_file, input_rna, obs_batch, var_input, obsm_output = mudata_with_mod_rna_obs_batch
 
-def test_scvi_after_hvg(run_component):
-    with NamedTemporaryFile('w', suffix=".h5mu") as tempfile_input_file:
-        input_data = mudata.read_h5mu(input_file)
-        input_rna = input_data.mod['rna']
-        input_rna.obs['batch2'] = 'A'
-        column_index = input_rna.obs.columns.get_indexer(['batch2'])
-        input_rna.obs.iloc[slice(input_rna.n_obs//2, None), column_index] = 'B'
-        input_data.write(tempfile_input_file.name)
+    args = [
+        "--input", new_input_file,
+        "--modality", "rna",
+        "--obs_batch", obs_batch,
+        "--output", "output.h5mu",
+        "--model_output", "test/",
+        "--max_epochs", "1",
+        "--n_obs_min_count", "10",
+        "--n_var_min_count", "10",
+        "--output_compression", "gzip"
+    ]
 
-        run_component([
-            "--input", tempfile_input_file.name,
-            "--modality", "rna",
-            "--obs_batch", "batch2",
-            "--output", "output.h5mu",
-            "--model_output", "test/",
-            "--var_input", "filter_with_hvg",
-            "--max_epochs", "1",
-            "--n_obs_min_count", "10",
-            "--n_var_min_count", "10",
-            "--output_compression", "gzip",
-            "--obsm_output", "X_scvi_integrated2"])
-        
-        # check files
-        assert Path("output.h5mu").is_file(), "Output file does not exist"
-        assert Path("test").is_dir()
-        assert Path("test/model.pt").is_file()
+    if var_input is not None:
+        args.extend(["--var_input", var_input])
+    if obsm_output is not None:
+        args.extend(["--obsm_output", obsm_output])
 
-        # check output h5mu
-        output_data = mudata.read_h5mu("output.h5mu")
-        output_rna = output_data.mod['rna']
-        assert output_rna.n_obs == input_rna.n_obs, f"Number of observations changed\noutput_data: {output_data}"
-        assert output_rna.n_vars == input_rna.n_vars, f"Number of variables changed\noutput_data: {output_data}"
-        assert 'X_scvi_integrated2' in output_rna.obsm, f"X_scvi_integrated not added\noutput_data: {output_data}"
+    run_component(args)
+    
+    # check files
+    assert Path("output.h5mu").is_file(), "Output file does not exist"
+    assert Path("test").is_dir()
+    assert Path("test/model.pt").is_file()
 
-        # assert that nothing else has changed
-        del output_rna.obsm["X_scvi_integrated2"]
-        assert_equal(input_rna, output_rna)
+    # check output h5mu
+    output_data = mudata.read_h5mu("output.h5mu")
+    output_rna = output_data.mod['rna']
+    assert output_rna.n_obs == input_rna.n_obs, f"Number of observations changed\noutput_data: {output_data}"
+    assert output_rna.n_vars == input_rna.n_vars, f"Number of variables changed\noutput_data: {output_data}"
+
+    expected_obsm_output = "X_scvi_integrated" if obsm_output is None else obsm_output
+    assert expected_obsm_output in output_rna.obsm, f".obsm['{expected_obsm_output}'] not added\noutput_data: {output_data}"
+
+    # assert that nothing else has changed
+    del output_rna.obsm[expected_obsm_output]
+    assert_equal(input_rna, output_rna)
 
 def test_hvg_subsetting_helper():
     input_data = mudata.read_h5mu(input_file)
