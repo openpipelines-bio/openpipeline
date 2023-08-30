59b4596041412b92f64c302eff549a96f07224b2
Robrecht Cannoodt <rcannood@gmail.com>
2023-08-30 12:21:02 +0200

    refactor pytests

--- src/labels_transfer/knn/test.py
+++ src/labels_transfer/knn/test.py
@@ -1,6 +1,5 @@
 import pytest
 from pathlib import Path
-from tempfile import NamedTemporaryFile
 import anndata
 import mudata
 import numpy as np
@@ -12,97 +11,70 @@ meta = {
 }
 ## VIASH END
 
-@pytest.fixture
-def reference_file():
-    return f"{meta['resources_dir']}/annotation_test_data/TS_Blood_filtered.h5ad"
+reference_file = f"{meta['resources_dir']}/annotation_test_data/TS_Blood_filtered.h5ad"
+input_file = f"{meta['resources_dir']}/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5mu"
 
 @pytest.fixture
-def input_file():
-    return f"{meta['resources_dir']}/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5mu"
+def test_args(tmp_path, request):
+    obsm_features, obs_targets, output_uns_parameters = request.param
 
-def test_one_class(run_component, input_file, reference_file):
-    with NamedTemporaryFile("w", suffix=".h5ad") as tempfile_reference_file:
-        reference_adata = anndata.read_h5ad(reference_file)
-        reference_adata.obsm["X_integrated_scanvi"] = np.random.normal(size=(reference_adata.n_obs, 30))
-        reference_adata.obs["ann_level_1"] = np.random.choice(["CD4 T cells", "CD8 T cells"], size=reference_adata.n_obs)
-        reference_adata.write(tempfile_reference_file.name)
-        
-        with NamedTemporaryFile("w", suffix=".h5mu") as tempfile_input_file:
-            input_data = mudata.read_h5mu(input_file)
-            adata = input_data.mod["rna"]
+    # read reference
+    tempfile_reference_file = tmp_path / "reference.h5ad"
+    reference_adata = anndata.read_h5ad(reference_file)
 
-            # Simulate a latent embedding
-            # Later, we can use a real one by calling `integrate` component in advamnce
-            adata.obsm["X_integrated_scanvi"] = np.random.normal(size=(adata.n_obs, 30))
+    # generate reference obs targets
+    for i, target in enumerate(obs_targets):
+        class_names = [str(idx) for idx in range(i + 2)]  # e.g. ["0", "1", "2"], the higher the level, the more the classes
+        reference_adata.obs[target] = np.random.choice(class_names, size=reference_adata.n_obs)
 
-            input_data.write(tempfile_input_file.name)
-
-            run_component([
-                "--input", tempfile_input_file.name,
-                "--modality", "rna",
-                "--input_obsm_features", "X_integrated_scanvi",
-                "--reference", tempfile_reference_file.name,
-                "--reference_obsm_features", "X_integrated_scanvi",
-                "--reference_obs_targets", "ann_level_1",
-                "--output", "output.h5mu",
-                "--n_neighbors", "5"
-            ])
-
-    assert Path("output.h5mu").is_file()
-
-    output_data = mudata.read_h5mu("output.h5mu")
-    assert "ann_level_1_pred" in output_data.mod["rna"].obs, f"Predictions are missing from output\noutput: {output_data.mod['rna'].obs}"
-    assert "ann_level_1_uncertainty" in output_data.mod["rna"].obs, f"Uncertainties are missing from output\noutput: {output_data.mod['rna'].obs}"
-    assert "labels_transfer" in output_data.mod["rna"].uns, f"Parameters are missing from output\noutput: {output_data.mod['rna'].uns}"
-    assert "ann_level_1" in output_data.mod["rna"].uns["labels_transfer"], f"Parameters are missing from output\noutput: {output_data.mod['rna'].uns}"
-    assert output_data.mod["rna"].uns["labels_transfer"]["ann_level_1"].get("method") == "KNN_pynndescent", f"Wrong method in parameters\noutput: {output_data.mod['rna'].uns}"
-    assert output_data.mod["rna"].uns["labels_transfer"]["ann_level_1"].get("n_neighbors") == 5, f"Wrong number of neighbors in parameters\noutput: {output_data.mod['rna'].uns}"
-    assert "ann_level_2" not in output_data.mod["rna"].obs, f"Level 2 should not be in output\noutput: {output_data.mod['rna'].obs}"
-
-def test_multiple_classes(run_component, reference_file, input_file,):
-    targets = ["ann_level_1", "ann_level_2", "ann_level_3", "ann_level_4", "ann_level_5", "ann_finest_level"]
+    # read input query
+    tempfile_input_file = tmp_path / "input.h5mu"
+    input_data = mudata.read_h5mu(input_file)
+    adata = input_data.mod["rna"]
     
-    with NamedTemporaryFile("w", suffix=".h5ad") as tempfile_reference_file:
-        reference_adata = anndata.read_h5ad(reference_file)
-        reference_adata.obsm["X_integrated_scvi"] = np.random.normal(size=(reference_adata.n_obs, 30))
-        for i, target in enumerate(targets):
-            class_names = [str(idx) for idx in range(i + 1)]  # e.g. ["0", "1", "2"], the higher the level, the more the classes
-            reference_adata.obs[target] = np.random.choice(class_names, size=reference_adata.n_obs)
+    # generate features
+    reference_adata.obsm[obsm_features] = np.random.normal(size=(reference_adata.n_obs, 30))
+    adata.obsm[obsm_features] = np.random.normal(size=(adata.n_obs, 30))
 
-        reference_adata.write(tempfile_reference_file.name)
+    # write files
+    reference_adata.write(tempfile_reference_file.name)
+    input_data.write(tempfile_input_file.name)
 
-        with NamedTemporaryFile("w", suffix=".h5mu") as tempfile_input_file:
-            input_data = mudata.read_h5mu(input_file)
-            adata = input_data.mod["rna"]
+    return tempfile_reference_file, reference_adata, tempfile_input_file, adata, obsm_features, obs_targets, output_uns_parameters
 
-            # Simulate a latent embedding
-            # Later, we can use a real one by calling `integrate` component in advance
-            adata.obsm["X_integrated_scvi"] = np.random.normal(size=(adata.n_obs, 30))
+@pytest.mark.parametrize("test_args", [("X_integrated_scvi", ["celltype"], None), ("X_int", ["ann_level_1", "ann_level_2", "ann_level_3"], "lab_tran")], indirect=True)
+def test_label_transfer(run_component, test_args):
+    tempfile_reference_file, reference_adata, tempfile_input_file, query_adata, obsm_features, obs_targets, output_uns_parameters = test_args
 
-            input_data.write(tempfile_input_file.name)
+    args = [
+        "--input", tempfile_input_file.name,
+        "--modality", "rna",
+        "--input_obsm_features", obsm_features,
+        "--reference", tempfile_reference_file.name,
+        "--reference_obsm_features", obsm_features,
+        "--reference_obs_targets", ",".join(obs_targets),
+        "--output", "output.h5mu",
+        "--n_neighbors", "5"
+    ]
 
-            run_component([
-                "--input", tempfile_input_file.name,
-                "--modality", "rna",
-                "--input_obsm_features", "X_integrated_scvi",
-                "--reference", tempfile_reference_file.name,
-                "--reference_obsm_features", "X_integrated_scvi",
-                "--reference_obs_targets", ",".join(targets),
-                "--output", "output.h5mu",
-                "--n_neighbors", "5",
-            ])
+    if output_uns_parameters is not None:
+        args.extend(["--output_uns_parameters", output_uns_parameters])
+
+    run_component(args)
 
     assert Path("output.h5mu").is_file()
 
     output_data = mudata.read_h5mu("output.h5mu")
 
-    for target in targets:
+    exp_uns = "labels_transfer" if output_uns_parameters is None else output_uns_parameters
+
+    for target in obs_targets:
         assert f"{target}_pred" in output_data.mod["rna"].obs, f"Predictions are missing from output\noutput: {output_data.mod['rna'].obs}"
         assert f"{target}_uncertainty" in output_data.mod["rna"].obs, f"Uncertainties are missing from output\noutput: {output_data.mod['rna'].obs}"
-        assert "labels_transfer" in output_data.mod["rna"].uns, f"Parameters are missing from output\noutput: {output_data.mod['rna'].uns}"
-        assert target in output_data.mod["rna"].uns["labels_transfer"], f"Parameters are missing from output\noutput: {output_data.mod['rna'].uns}"
-        assert output_data.mod["rna"].uns["labels_transfer"][target].get("method") == "KNN_pynndescent", f"Wrong method in parameters\noutput: {output_data.mod['rna'].uns}"
-        assert output_data.mod["rna"].uns["labels_transfer"][target].get("n_neighbors") == 5, f"Wrong number of neighbors in parameters\noutput: {output_data.mod['rna'].uns}"
+        assert exp_uns in output_data.mod["rna"].uns, f"Parameters are missing from output\noutput: {output_data.mod['rna'].uns}"
+        assert target in output_data.mod["rna"].uns[exp_uns], f"Parameters are missing from output\noutput: {output_data.mod['rna'].uns}"
+        assert output_data.mod["rna"].uns[exp_uns][target].get("method") == "KNN_pynndescent", f"Wrong method in parameters\noutput: {output_data.mod['rna'].uns}"
+        assert output_data.mod["rna"].uns[exp_uns][target].get("n_neighbors") == 5, f"Wrong number of neighbors in parameters\noutput: {output_data.mod['rna'].uns}"
 
 if __name__ == '__main__':
     exit(pytest.main([__file__]))
--- src/labels_transfer/xgboost/test.py
+++ src/labels_transfer/xgboost/test.py
@@ -12,184 +12,124 @@ meta = {
 }
 ## VIASH END
 
-@pytest.fixture
-def reference_file():
-    return f"{meta['resources_dir']}/annotation_test_data/TS_Blood_filtered.h5ad"
+reference_file = f"{meta['resources_dir']}/annotation_test_data/TS_Blood_filtered.h5ad"
+input_file = f"{meta['resources_dir']}/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5mu"
+
 
 @pytest.fixture
-def input_file():
-    return f"{meta['resources_dir']}/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5mu"
+def test_args(tmp_path, request):
+    obsm_features, obs_targets, output_uns_parameters = request.param
 
-def test_one_class(run_component, input_file, reference_file):
-    with NamedTemporaryFile("w", suffix=".h5ad") as tempfile_reference_file:
-        reference_adata = anndata.read_h5ad(reference_file)
-        reference_adata.obsm["X_integrated_scanvi"] = np.random.normal(size=(reference_adata.n_obs, 30))
-        reference_adata.obs["ann_level_1"] = np.random.choice(["CD4 T cells", "CD8 T cells"], size=reference_adata.n_obs)
-        reference_adata.write(tempfile_reference_file.name)
-        
-        with NamedTemporaryFile("w", suffix=".h5mu") as tempfile_input_file:
-            input_data = mudata.read_h5mu(input_file)
-            adata = input_data.mod["rna"]
+    # read reference
+    tempfile_reference_file = tmp_path / "reference.h5ad"
+    reference_adata = anndata.read_h5ad(reference_file)
 
-            # Simulate a latent embedding
-            # Later, we can use a real one by calling `integrate` component in advamnce
-            adata.obsm["X_integrated_scanvi"] = np.random.normal(size=(adata.n_obs, 30))
+    # generate reference obs targets
+    for i, target in enumerate(obs_targets):
+        class_names = [str(idx) for idx in range(i + 2)]  # e.g. ["0", "1", "2"], the higher the level, the more the classes
+        reference_adata.obs[target] = np.random.choice(class_names, size=reference_adata.n_obs)
 
-            input_data.write(tempfile_input_file.name)
+    # read input query
+    tempfile_input_file = tmp_path / "input.h5mu"
+    input_data = mudata.read_h5mu(input_file)
+    adata = input_data.mod["rna"]
+    
+    # generate features
+    reference_adata.obsm[obsm_features] = np.random.normal(size=(reference_adata.n_obs, 30))
+    adata.obsm[obsm_features] = np.random.normal(size=(adata.n_obs, 30))
 
-            run_component([
-                "--input", tempfile_input_file.name,
-                "--modality", "rna",
-                "--input_obsm_features", "X_integrated_scanvi",
-                "--reference", tempfile_reference_file.name,
-                "--reference_obsm_features", "X_integrated_scanvi",
-                "--reference_obs_targets", "ann_level_1",
-                "--output", "output.h5mu",
-                "--model_output", "model_one_class",
-                "--use_gpu", "false",
-                "--max_depth", "6"
-            ])
+    # write files
+    reference_adata.write(tempfile_reference_file.name)
+    input_data.write(tempfile_input_file.name)
 
-    assert Path("output.h5mu").is_file()
+    return tempfile_reference_file, reference_adata, tempfile_input_file, adata, obsm_features, obs_targets, output_uns_parameters
 
-    output_data = mudata.read_h5mu("output.h5mu")
-    assert "ann_level_1_pred" in output_data.mod["rna"].obs, f"Predictions are missing from output\noutput: {output_data.mod['rna'].obs}"
-    assert "ann_level_1_uncertainty" in output_data.mod["rna"].obs, f"Uncertainties are missing from output\noutput: {output_data.mod['rna'].obs}"
-    assert "labels_transfer" in output_data.mod["rna"].uns, f"Parameters are missing from output\noutput: {output_data.mod['rna'].uns}"
-    assert "ann_level_1" in output_data.mod["rna"].uns["labels_transfer"], f"Parameters are missing from output\noutput: {output_data.mod['rna'].uns}"
-    assert output_data.mod["rna"].uns["labels_transfer"]["ann_level_1"].get("method") == "XGBClassifier", f"Wrong method in parameters\noutput: {output_data.mod['rna'].uns}"
-    assert output_data.mod["rna"].uns["labels_transfer"]["ann_level_1"].get("max_depth") == 6, f"Wrong max depth in parameters\noutput: {output_data.mod['rna'].uns}"
-    assert "ann_level_2" not in output_data.mod["rna"].obs, f"Level 2 should not be in output\noutput: {output_data.mod['rna'].obs}"
 
-    # Remove output file to prevent errors in the next tests
-    Path("output.h5mu").unlink()
+@pytest.mark.parametrize("test_args", [("X_integrated_scvi", ["celltype"], None), ("X_int", ["ann_level_1", "ann_level_2", "ann_level_3"], "lab_tran")], indirect=True)
+def test_label_transfer(run_component, test_args):
+    tempfile_reference_file, reference_adata, tempfile_input_file, query_adata, obsm_features, obs_targets, output_uns_parameters = test_args
 
-def test_multiple_classes(run_component, input_file, reference_file):
-    targets = ["ann_level_1", "ann_level_2", "ann_level_3", "ann_level_4", "ann_level_5", "ann_finest_level"]
+    args = [
+        "--input", tempfile_input_file.name,
+        "--modality", "rna",
+        "--input_obsm_features", obsm_features,
+        "--reference", tempfile_reference_file.name,
+        "--reference_obsm_features", obsm_features,
+        "--reference_obs_targets", ",".join(obs_targets),
+        "--output", "output.h5mu",
+        "--model_output", "model_one_class",
+        "--use_gpu", "false",
+        "--max_depth", "6"
+    ]
 
-    with NamedTemporaryFile("w", suffix=".h5ad") as tempfile_reference_file:
-        reference_adata = anndata.read_h5ad(reference_file)
-        reference_adata.obsm["X_integrated_scvi"] = np.random.normal(size=(reference_adata.n_obs, 30))
-        for i, target in enumerate(targets):
-            class_names = [str(idx) for idx in range(i + 2)]  # e.g. ["0", "1", "2"], the higher the level, the more the classes
-            reference_adata.obs[target] = np.random.choice(class_names, size=reference_adata.n_obs)
+    if output_uns_parameters is not None:
+        args.extend(["--output_uns_parameters", output_uns_parameters])
 
-        reference_adata.write(tempfile_reference_file.name)
-
-        with NamedTemporaryFile("w", suffix=".h5mu") as tempfile_input_file:
-            input_data = mudata.read_h5mu(input_file)
-            adata = input_data.mod["rna"]
-
-            # Simulate a latent embedding
-            # Later, we can use a real one by calling `integrate` component in advamnce
-            adata.obsm["X_integrated_scvi"] = np.random.normal(size=(adata.n_obs, 30))
-
-            input_data.write(tempfile_input_file.name)
-
-            run_component([
-                "--input", tempfile_input_file.name,
-                "--modality", "rna",
-                "--input_obsm_features", "X_integrated_scvi",
-                "--reference", tempfile_reference_file.name,
-                "--reference_obsm_features", "X_integrated_scvi",
-                "--reference_obs_targets", ",".join(targets),
-                "--output", "output.h5mu",
-                "--model_output", "model_multiple_classes",
-                "--max_depth", "6",
-                "--use_gpu", "false"
-            ])
+    run_component(args)
 
     assert Path("output.h5mu").is_file()
 
     output_data = mudata.read_h5mu("output.h5mu")
 
-    for target in targets:
+    exp_uns = "labels_transfer" if output_uns_parameters is None else output_uns_parameters
+
+    for target in obs_targets:
         assert f"{target}_pred" in output_data.mod["rna"].obs, f"Predictions are missing from output\noutput: {output_data.mod['rna'].obs}"
         assert f"{target}_uncertainty" in output_data.mod["rna"].obs, f"Uncertainties are missing from output\noutput: {output_data.mod['rna'].obs}"
-        assert "labels_transfer" in output_data.mod["rna"].uns, f"Parameters are missing from output\noutput: {output_data.mod['rna'].uns}"
-        assert target in output_data.mod["rna"].uns["labels_transfer"], f"Parameters are missing from output\noutput: {output_data.mod['rna'].uns}"
-        assert output_data.mod["rna"].uns["labels_transfer"][target].get("method") == "XGBClassifier", f"Wrong method in parameters\noutput: {output_data.mod['rna'].uns}"
-        assert output_data.mod["rna"].uns["labels_transfer"][target].get("max_depth") == 6, f"Wrong max depth in parameters\noutput: {output_data.mod['rna'].uns}"
+        assert exp_uns in output_data.mod["rna"].uns, f"Parameters are missing from output\noutput: {output_data.mod['rna'].uns}"
+        assert target in output_data.mod["rna"].uns[exp_uns], f"Parameters are missing from output\noutput: {output_data.mod['rna'].uns}"
+        assert output_data.mod["rna"].uns[exp_uns][target].get("method") == "XGBClassifier", f"Wrong method in parameters\noutput: {output_data.mod['rna'].uns}"
+        assert output_data.mod["rna"].uns[exp_uns][target].get("max_depth") == 6, f"Wrong number of neighbors in parameters\noutput: {output_data.mod['rna'].uns}"
 
-    # Remove output file to prevent errors in the next tests
-    Path("output.h5mu").unlink()
 
-def test_retraining(run_component, input_file, reference_file):
-    with NamedTemporaryFile("w", suffix=".h5ad") as tempfile_reference_file:
-        reference_adata = anndata.read_h5ad(reference_file)
-        reference_adata.obsm["X_integrated_scanvi"] = np.random.normal(size=(reference_adata.n_obs, 30))
-        reference_adata.obs["ann_level_1"] = np.random.choice(["CD4 T cells", "CD8 T cells"], size=reference_adata.n_obs)
-        reference_adata.obs["ann_level_2"] = np.random.choice(["CD4 naive T cells", "CD4 memory T cells", "CD8 naive T cells", "CD8 memory T cells"], size=reference_adata.n_obs)
-        reference_adata.obs["ann_level_3"] = np.random.choice(["CD4 naive T cells", "CD4 memory T cells", "CD8 naive T cells", "CD8 memory T cells", "T Rex cells"], size=reference_adata.n_obs)
-        reference_adata.write(tempfile_reference_file.name)
+@pytest.mark.parametrize("test_args", [("X_int", ["ann_level_1", "ann_level_2", "ann_level_3"], "lab_tran")], indirect=True)
+def test_retraining(run_component, test_args):
+    tempfile_reference_file, reference_adata, tempfile_input_file, query_adata, obsm_features, obs_targets, output_uns_parameters = test_args
 
-        targets = ["ann_level_1", "ann_level_2"]
+    # Train first 2 targets
+    args = [
+        "--modality", "rna",
+        "--input_obsm_features", obsm_features,
+        "--reference", tempfile_reference_file.name,
+        "--reference_obsm_features", obsm_features,
+        "--model_output", "model_retraining"]
     
-        with NamedTemporaryFile("w", suffix=".h5mu") as tempfile_input_file:
-            input_data = mudata.read_h5mu(input_file)
-            adata = input_data.mod["rna"]
-
-            # Simulate a latent embedding
-            # Later, we can use a real one by calling `integrate` component in advance
-            adata.obsm["X_integrated_scanvi"] = np.random.normal(size=(adata.n_obs, 30))
-
-            input_data.write(tempfile_input_file.name)
-
-            # Train first 2 targets
-            run_component([
-                "--input", tempfile_input_file.name,
-                "--modality", "rna",
-                "--input_obsm_features", "X_integrated_scanvi",
-                "--reference", tempfile_reference_file.name,
-                "--reference_obsm_features", "X_integrated_scanvi",
-                "--output", "output.h5mu",
-                "--model_output", "model_retraining",
-                "--max_depth", "6",
-                "--reference_obs_targets", ",".join(targets)])
-
-            assert Path("output.h5mu").is_file()
-
-            output_data = mudata.read_h5mu("output.h5mu")
-
-            for target in targets:
-                assert f"{target}_pred" in output_data.mod["rna"].obs, f"Predictions are missing from output\noutput: {output_data.mod['rna'].obs}"
-                assert f"{target}_uncertainty" in output_data.mod["rna"].obs, f"Uncertainties are missing from output\noutput: {output_data.mod['rna'].obs}"
-                assert "labels_transfer" in output_data.mod["rna"].uns, f"Parameters are missing from output\noutput: {output_data.mod['rna'].uns}"
-                assert target in output_data.mod["rna"].uns["labels_transfer"], f"Parameters are missing from output\noutput: {output_data.mod['rna'].uns}"
-                assert output_data.mod["rna"].uns["labels_transfer"][target].get("method") == "XGBClassifier", f"Wrong method in parameters\noutput: {output_data.mod['rna'].uns}"
-                assert output_data.mod["rna"].uns["labels_transfer"][target].get("max_depth") == 6, f"Wrong max depth in parameters\noutput: {output_data.mod['rna'].uns}"
-
-            # Add one more target
-            # Now the code should use 2 previously trained models, and train only the third
-            targets.append("ann_level_3")
-            run_component([
-                "--input", "output.h5mu",
-                "--modality", "rna",
-                "--input_obsm_features", "X_integrated_scanvi",
-                "--reference", tempfile_reference_file.name,
-                "--reference_obsm_features", "X_integrated_scanvi",
-                "--model_output", "model_retraining",
-                "--output", "output.h5mu",
-                "--max_depth", "4",  # Change parameter so that we could make sure if the model is old or new
-                "--reference_obs_targets", ",".join(targets)])
+    if output_uns_parameters is not None:
+        args.extend(["--output_uns_parameters", output_uns_parameters])
+    
+    args1 = args + [
+        "--input", tempfile_input_file.name,
+        "--output", "output.h5mu",
+        "--reference_obs_targets", ",".join(obs_targets[:2]),
+        "--max_depth", "6"]
+    run_component(args1)
 
     assert Path("output.h5mu").is_file()
 
-    output_data = mudata.read_h5mu("output.h5mu")
+    # Add more targets
+    # Now the code should use 2 previously trained models,
+    # and train only the remaining targets
+    args2 = args + [
+        "--input", "output.h5mu",
+        "--output", "output2.h5mu",
+        "--reference_obs_targets", ",".join(obs_targets),
+        "--max_depth", "4"]
+    run_component(args2)
 
-    for target in targets:
+    assert Path("output2.h5mu").is_file()
+
+    output_data = mudata.read_h5mu("output2.h5mu")
+
+    for target in obs_targets:
         assert f"{target}_pred" in output_data.mod["rna"].obs, f"Predictions are missing from output\noutput: {output_data.mod['rna'].obs}"
         assert f"{target}_uncertainty" in output_data.mod["rna"].obs, f"Uncertainties are missing from output\noutput: {output_data.mod['rna'].obs}"
-        assert "labels_transfer" in output_data.mod["rna"].uns, f"Parameters are missing from output\noutput: {output_data.mod['rna'].uns}"
-        assert target in output_data.mod["rna"].uns["labels_transfer"]
-        assert output_data.mod["rna"].uns["labels_transfer"][target].get("method") == "XGBClassifier"
+        assert output_uns_parameters in output_data.mod["rna"].uns, f"Parameters are missing from output\noutput: {output_data.mod['rna'].uns}"
+        assert target in output_data.mod["rna"].uns[output_uns_parameters], f"Parameters are missing from output\noutput: {output_data.mod['rna'].uns}"
+        assert output_data.mod["rna"].uns[output_uns_parameters][target].get("method") == "XGBClassifier", f"Wrong method in parameters\noutput: {output_data.mod['rna'].uns}"
     
-    assert output_data.mod["rna"].uns["labels_transfer"][targets[0]].get("max_depth") == 6
-    assert output_data.mod["rna"].uns["labels_transfer"][targets[1]].get("max_depth") == 6
-    assert output_data.mod["rna"].uns["labels_transfer"][targets[2]].get("max_depth") == 4
-
-    # Remove output file to prevent errors in the next tests
-    Path("output.h5mu").unlink()
+    assert output_data.mod["rna"].uns[output_uns_parameters][obs_targets[0]].get("max_depth") == 6, f"Wrong number of neighbors in parameters\noutput: {output_data.mod['rna'].uns}"
+    assert output_data.mod["rna"].uns[output_uns_parameters][obs_targets[1]].get("max_depth") == 6, f"Wrong number of neighbors in parameters\noutput: {output_data.mod['rna'].uns}"
+    assert output_data.mod["rna"].uns[output_uns_parameters][obs_targets[2]].get("max_depth") == 4, f"Wrong number of neighbors in parameters\noutput: {output_data.mod['rna'].uns}"
 
 
 if __name__ == '__main__':
