3005490e3805effc10052da535ef5099457a85b9
Robrecht Cannoodt <rcannood@gmail.com>
2023-09-08 15:22:17 +0200

    add logger code inline

--- src/transform/delete_layer/script.py
+++ src/transform/delete_layer/script.py
@@ -17,18 +17,25 @@ meta = {
     "functionality_name": "delete_layer",
     "resources_dir": "resources_test"
 }
-sys.path.append("src/utils/")
-
-original_input_data = read_h5mu(par["input"])
-new_layer = original_input_data.mod['rna'].X
-original_input_data.mod['rna'].layers['test'] = new_layer
-tempfile = Path("/tmp/input.h5mu")
-original_input_data.write_h5mu(str(tempfile))
-par["input"] = "/tmp/input.h5mu"
 ## VIASH END
 
 sys.path.append(meta["resources_dir"])
-from setup_logger import setup_logger
+# START TEMPORARY WORKAROUND setup_logger
+# reason: resources aren't available when using Nextflow fusion
+# from setup_logger import setup_logger
+def setup_logger():
+    import logging
+    from sys import stdout
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    console_handler = logging.StreamHandler(stdout)
+    logFormatter = logging.Formatter("%(asctime)s %(levelname)-8s %(message)s")
+    console_handler.setFormatter(logFormatter)
+    logger.addHandler(console_handler)
+
+    return logger
+# END TEMPORARY WORKAROUND setup_logger
 logger = setup_logger()
 
 from compress_h5mu import compress_h5mu
