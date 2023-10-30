e70c8d4504b5a073d0014bbf88f67bdc8c1f0bdb
Robrecht Cannoodt <rcannood@gmail.com>
2023-10-30 14:54:36 +0900

    Add genetic demultiplexing component (#343)
    * add genetic demultiplexing component
    
    * add docker file
    
    * remove docker image
    
    * add docker container image file
    
    * add bcftools
    
    * add description for demuxlet and freemuxlet
    
    * add description for samtools
    
    * fix bug of demuxlet
    
    * fix freemuxlet bug
    
    * change demuxlet and freemuxlet container image
    
    * fix samtools and freemuxlet bug
    
    * remove unused code
    
    * no longer support summary component
    
    * fix freemuxlet bug
    
    * fix samtools bug
    
    * fix demuxlet bug
    
    * add test scripts and change container configuration
    
    * Update dockerimage/rpython3/Dockerfile
    
    fix container tip error
    
    * 1. add detailed description for each component
    2. add filtering threshold of QUAL for bcftools
    
    * add test scripts
    
    * 1. remove docker images, as now the configuration is directly specified in config file
    2. use ubuntu 20.04 for popscle
    3. use python 3.9 consistently
    
    * 1. add resources_test_scripts
    2. improve docker container configuration
    3. improve test scripts
    
    * add an assignment tsv file for all demultiplexing components
    
    * uoload summary Rscript for demuxlet and freemuxlet
    
    * use smaller genome ref for test
    
    * use subsetted bam and genome for souporcell
    
    * update download scripts
    
    * update to viash 0.6.7
    
    * separate dsc-pileup from demuxlet
    
    * remove dsc-pileup from freemuxlet component
    
    * use unzip for dsc pileup
    
    * make sure output is never used as a prefix
    
    * remove nproc parameter
    
    * make sure folders are never used as a prefix
    
    * uses parameter expansion to simplify code
    
    * clean up tests and code
    
    * fix parallellization params
    
    * switch to ubuntu container, use updated arguments
    
    * add xichen as author
    
    * skip nvcr login if PAT is not available
    
    * try with double dollar
    
    * undo commit
    
    * rename folder
    
    * add entry to changelog
    
    * consensus on output file
    
    * fix pandas error
    
    * Update souporcell docker
    
    update the docker configuration of souporcell to the latest version
    
    * update author
    
    * fix author string
    
    * fix container
    
    * turn camelcase arguments into snakecase
    
    * combine bash and r script into one r script
    
    * convert camelcase to snakecase
    
    * convert camelcase to snakecase and merge into one r script
    
    * fix type
    
    ---------
    
    Co-authored-by: Xichen Wu <102925032+wxicu@users.noreply.github.com>

---
+++ src/genetic_demux/demuxlet/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+
+echo ">>> Running executable"
+$meta_executable \
+    --sam "$meta_resources_dir/demuxafy_test_data/pooled.sorted.bam" \
+    --vcf "$meta_resources_dir/demuxafy_test_data/test_dataset.vcf" \
+    --output demuxlet_result/ \
+    --out out \
+    --field GP
+
+[[ ! -f demuxlet_result/out.best ]] && echo "Output result file could not be found!" && exit 1
+[[ ! -f demuxlet_result/cell_annotation.csv ]] && echo "Output cell annotation file could not be found!" && exit 1
+
+echo ">>> Test finished successfully"
