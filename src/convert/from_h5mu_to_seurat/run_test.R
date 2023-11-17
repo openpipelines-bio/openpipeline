0de0d9ff44ae70da03381e20fbae85715ba89c07
Robrecht Cannoodt <rcannood@gmail.com>
2023-11-17 12:06:38 +0100

    change input file

--- src/convert/from_h5mu_to_seurat/run_test.R
+++ src/convert/from_h5mu_to_seurat/run_test.R
@@ -10,8 +10,7 @@ meta <- list(
 
 cat("> Checking whether output is correct\n")
 
-# TODO: this should also work for ums and mms, but it doesn't.
-in_h5mu <- paste0(meta[["resources_dir"]], "/pbmc_1k_protein_v3/pbmc_1k_protein_v3_uss.h5mu")
+in_h5mu <- paste0(meta[["resources_dir"]], "/pbmc_1k_protein_v3/pbmc_1k_protein_v3_mms.h5mu")
 out_rds <- "output.rds"
 
 cat("> Running ", meta[["functionality_name"]], "\n", sep = "")
