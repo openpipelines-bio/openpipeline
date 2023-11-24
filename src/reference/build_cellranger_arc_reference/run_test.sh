3a0322a3400755d0ada603c4165db0437e4fa6ec
Vladimir Shitov <vladimir.shitov@helmholtz-muenchen.de>
2023-11-24 16:09:37 +0100

    Upload the component

---
+++ src/reference/build_cellranger_arc_reference/run_test.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+## VIASH START
+meta_executable="bin/viash run src/reference/make_reference/config.vsh.yaml --"
+## VIASH END
+
+# create temporary directory
+tmpdir=$(mktemp -d "$meta_temp_dir/$meta_functionality_name-XXXXXXXX")
+function clean_up {
+    rm -rf "$tmpdir"
+}
+trap clean_up EXIT
+
+function seqkit_head {
+  input="$1"
+  output="$2"
+  if [[ ! -f "$output" ]]; then
+    echo "> Processing `basename $input`"
+    seqkit subseq -r 1:50000 "$input" | gzip > "$output"
+  fi
+}
+
+seqkit_head "$meta_resources_dir/reference_gencodev41_chr1/reference.fa.gz" "$tmpdir/reference_small.fa.gz"
+zcat "$meta_resources_dir/reference_gencodev41_chr1/reference.gtf.gz" | awk '$4 < 50001 {print ;}' | gzip > "$tmpdir/reference_small.gtf.gz"
+
+echo "> Running $meta_functionality_name, writing to $tmpdir."
+$meta_executable \
+  --genome_fasta "$meta_resources_dir/reference_gencodev41_chr1/reference.fa.gz" \
+  --annotation_gtf "$meta_resources_dir/reference_gencodev41_chr1/reference.gtf.gz" \
+  --motifs_file "$meta_resources_dir/reference_gencodev41_chr1/JASPAR2024_CORE_non-redundant_pfms_jaspar.txt.modified" \
+  --output "$meta_resources_dir/myreference.tar.gz" \
+  ---cpus ${meta_memory_gb:-1} \
+  ---memory ${meta_memory_gb:-5}GB
+
+exit_code=$?
+[[ $exit_code != 0 ]] && echo "Non zero exit code: $exit_code" && exit 1
+
+echo ">> Checking whether output can be found"
+[[ ! -f "$tmpdir/myreference.tar.gz" ]] && echo "Output tar file could not be found!" && exit 1
+
+# TODO:
+# - check whether output reference file is valid
+# - Check that motifs/tss files are present there
+
+echo "> Test succeeded!"
