6df277642dd76f38f3b1f66b44df9c3ae84a82f1
dorien-er <roosen.dorien@gmail.com>
2024-02-12 15:46:21 +0100

    update input output handling

--- src/annotate/score_genes/script.py
+++ src/annotate/score_genes/script.py
@@ -1,28 +1,28 @@
 import scanpy as sc
 import mudata as mu
 
-## VIASH START
-par = {
-    "input": "resources_test/merge_test_data/pbmc_1k_protein_v3_filtered_feature_bc_matrix_rna.h5mu",
-    "gene_pool": "src/annotate/score_genes_cell_cycle/test_resources/regev_lab_cell_cycle_genes.txt"
-}
-## VIASH END
 
+mudata = mu.read(f'{par["input"]}')
 
+par["use_raw"] = par.setdefault("use_raw", None)
+par["gene_pool"] = par.setdefault("gene_pool", None)
 
-adata = mu.read(f"{par["input"]}/rna")
+gene_list = [x.strip() for x in open(par["gene_list"])]
+
+if par["gene_pool"]:
+    par["gene_pool"] = [x.strip() for x in open(par["gene_pool"])]
 
 output = sc.tl.score_genes(
-    adata,
-    par["gene_list"],
-    ctrl_size = par["ctrl_size"],
-    gene_pool = par["gene_pool"],
-    n_bins = par["n_bins"],
-    score_name = par ["score_name"],
-    random_state = par["random_state"],
-    copy = par["copy"],
-    use_raw = par["use_raw"]
+    mudata.mod["rna"],
+    gene_list,
+    ctrl_size=par["ctrl_size"],
+    gene_pool=par["gene_pool"],
+    n_bins=par["n_bins"],
+    score_name=par["score_name"],
+    random_state=par["random_state"],
+    copy=True,
+    use_raw=par["use_raw"]
 )
 
-if output:
-    mu.write(f"{par["output"]}/rna", adata)
+mudata.mod["rna"] = output
+mudata.write(f'{par["output"]}')
