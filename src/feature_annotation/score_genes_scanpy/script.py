ce05d6de1b02438e87b0dfc770df6159cbdcaa04
dorien-er <roosen.dorien@gmail.com>
2024-02-16 10:05:55 +0100

    update helper

--- src/feature_annotation/score_genes_scanpy/script.py
+++ src/feature_annotation/score_genes_scanpy/script.py
@@ -50,28 +50,27 @@ def read_gene_list(
         raise ValueError(f"Either --{list_key} or --{file_key} must be set")
 
     # read gene list from parameters
-    gene_list = par[list_key] if par[list_key] else []
+    list_of_genes = par[list_key] if par[list_key] else []
 
     # read gene list from file
     if par[file_key]:
         with open(par[file_key]) as file:
             file_genes = [x.strip() for x in file]
-        gene_list.extend(file_genes)
+        list_of_genes.extend(file_genes)
 
     # check for missing genes
-    if not par["allow_missing_genes"] and list:
-        missing = set(gene_list).difference(gene_names)
+    if not par["allow_missing_genes"] and list_of_genes:
+        missing = set(list_of_genes).difference(gene_names)
         if missing:
             raise ValueError(f"The follow genes are missing from the input dataset: {missing}")
 
     # return gene list
-    if gene_list:
-        return gene_list
+    if list_of_genes:
+        return list_of_genes
     elif required:
         raise ValueError(f"No genes detected in --{list_key} or --{file_key}")
     else:
         return None
-
 
 # END TEMPORARY WORKAROUND read_gene_list
 
