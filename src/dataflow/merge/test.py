2273fcd053f5c385c1b0a8d417dc883b816585bd
jakubmajercik <jakub.majercik@gmail.com>
2024-03-04 17:38:10 +0100

    dataflow/merge test_merge refactored

--- src/dataflow/merge/test.py
+++ src/dataflow/merge/test.py
@@ -2,16 +2,17 @@ import sys
 import pytest
 from pathlib import Path
 import subprocess
-import mudata as md
+from mudata import MuData, read_h5mu
 import pandas as pd
 import numpy as np
 import re
+from openpipelinetestutils.asserters import assert_annotation_objects_equal
 
 ## VIASH START
 meta = {
     'executable': './target/docker/dataflow/merge/merge',
     'resources_dir': './resources_test/merge_test_data/',
-    'config': '/home/di/code/openpipeline/src/dataflow/merge/config.vsh.yml'
+    'config': './src/dataflow/merge/config.vsh.yml'
 }
 ## VIASH END
 
@@ -23,7 +24,7 @@ input_sample2_file = f"{meta['resources_dir']}/pbmc_1k_protein_v3_filtered_featu
 def mudata_non_overlapping_observations(request, tmp_path):
     mudata_to_change_path = request.param
     temp_h5mu = tmp_path / "duplicate_observations.h5mu"
-    mudata_to_change = md.read(mudata_to_change_path)
+    mudata_to_change = read_h5mu(mudata_to_change_path)
     # Remove 1 observation
     removed_observation_name = mudata_to_change.obs.index[-1]
     mudata_to_change = mudata_to_change[:mudata_to_change.n_obs-1]
@@ -39,7 +40,7 @@ def mudata_with_extra_var_column(tmp_path, request, extra_var_column_value):
     [sample1_path, sample2_path] = request.param
     result = []
     for sample_path, column_value in ((sample1_path, extra_var_column_value), (sample2_path, np.nan)):
-        sample = md.read_h5mu(sample_path)
+        sample = read_h5mu(sample_path)
         mod_names = list(sample.mod.keys())
         assert len(mod_names) == 1
         mod_name = mod_names[0]
@@ -55,50 +56,62 @@ def mudata_with_extra_var_column(tmp_path, request, extra_var_column_value):
     return result
 
 
-def test_merge(run_component):
+def test_merge(run_component, random_h5mu_path):
     """
     Test a simple merge with fully overlapping observations
     """
-    run_component([
+    
+    output_path = random_h5mu_path()
+    args = [
         "--input", input_sample1_file,
         "--input", input_sample2_file,
-        "--output", "merge.h5mu",
-        "--output_compression", "gzip"])
+        "--output", output_path,
+        "--output_compression", "gzip"
+    ]
+    run_component(args)
 
-    assert Path("merge.h5mu").is_file()
-    concatenated_data = md.read("merge.h5mu")
-    data_sample1 = md.read(input_sample1_file)
-    data_sample2 = md.read(input_sample2_file)
+    assert output_path.is_file()
+    concatenated_data = read_h5mu(output_path)
+    data_sample1 = read_h5mu(input_sample1_file)
+    data_sample2 = read_h5mu(input_sample2_file)
+    
+    expected_concatenated_data = MuData({'rna': data_sample1.mod['rna'], 'prot': data_sample2.mod['prot']})
+    assert_annotation_objects_equal(concatenated_data, expected_concatenated_data)
 
-    assert concatenated_data.n_mod == 2
-    assert concatenated_data.obsm_keys() == ['prot', 'rna']
-    # In this case, the observations overlap perfectly in the two input files
-    assert concatenated_data.n_obs == data_sample1.n_obs
-    assert concatenated_data.n_obs == data_sample2.n_obs
-    pd.testing.assert_index_equal(concatenated_data.obs.index, data_sample1.obs.index)
-    pd.testing.assert_index_equal(concatenated_data.obs.index, data_sample2.obs.index)
+    # assert concatenated_data.n_mod == 2
+    # assert concatenated_data.obsm_keys() == ['prot', 'rna']
+    # # In this case, the observations overlap perfectly in the two input files
+    # assert concatenated_data.n_obs == data_sample1.n_obs
+    # assert concatenated_data.n_obs == data_sample2.n_obs
+    # pd.testing.assert_index_equal(concatenated_data.obs.index, data_sample1.obs.index)
+    # pd.testing.assert_index_equal(concatenated_data.obs.index, data_sample2.obs.index)
 
-    assert set(data_sample1.var_keys()) | set(data_sample2.var_keys()) == \
-           set(concatenated_data.var_keys())
+    # assert set(data_sample1.var_keys()) | set(data_sample2.var_keys()) == \
+    #        set(concatenated_data.var_keys())
 
-    assert set(concatenated_data.var_names) ==  (set(data_sample1.var_names) | set(data_sample2.var_names))
-    assert concatenated_data.var_keys() == ['gene_id', 'feature_type', 'genome']
+    # assert set(concatenated_data.var_names) ==  (set(data_sample1.var_names) | set(data_sample2.var_names))
+    # assert concatenated_data.var_keys() == ['gene_id', 'feature_type', 'genome']
 
 @pytest.mark.parametrize("mudata_non_overlapping_observations", [input_sample1_file], indirect=["mudata_non_overlapping_observations"])
-def test_merge_non_overlapping_observations(run_component, mudata_non_overlapping_observations):
+def test_merge_non_overlapping_observations(run_component, mudata_non_overlapping_observations, random_h5mu_path):
     """
     Merge with differing observations in the samples
     """
     edited_h5mu, removed_observation_name = mudata_non_overlapping_observations
+    output_file = random_h5mu_path()
     # Remove 1 observation
     run_component([
         "--input", edited_h5mu,
         "--input", input_sample2_file,
-        "--output", "merge.h5mu"])
-    assert  Path("merge.h5mu").is_file()
-    concatenated_data = md.read("merge.h5mu", backed=False)
+        "--output", output_file])
+    assert  Path(output_file).is_file()
+    concatenated_data = md.read(output_file, backed=False)
     data_sample1 = md.read(edited_h5mu, backed=False)
     data_sample2 = md.read(input_sample2_file, backed=False)
+    
+    expected_concatenated_data = md.MuData({'rna': data_sample1.mod['rna'], 'prot': data_sample2.mod['prot']})
+    assert_annotation_objects_equal(concatenated_data, expected_concatenated_data)
+    
     assert concatenated_data[removed_observation_name:]['rna'].n_obs == 0
     assert concatenated_data[removed_observation_name:]['prot'].n_obs == 1
 
