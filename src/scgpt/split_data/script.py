eff7c085e8cab4bd1d27ee369ea568eeec91b854
jakubmajercik <jakub.majercik@gmail.com>
2024-03-24 22:58:13 +0100

    different split args and exceptions handled

--- src/scgpt/split_data/script.py
+++ src/scgpt/split_data/script.py
@@ -17,6 +17,7 @@ par = {
     "val_size": 0.0,
     "shuffle": True,
     "stratify": False,
+    "random_state": 42
 }
 ## VIASH END
 
@@ -43,4 +44,50 @@ logger = setup_logger()
 logger.info(f"Reading {par['input']}")
 mudata = mu.read_h5mu(par["input"])
 adata = mudata.mod[par["modality"]].copy()
+
+if not par["val_size"]:
+    if par["train_size"] and par["train_size"] + par["test_size"] != 1:
+        raise ValueError("The sum of all dataset partitions must be equal to 1.")
+    train_obs, test_obs = train_test_split(
+        adata.obs,
+        test_size=par["test_size"],
+        random_state=par["random_state"],
+        shuffle=par["shuffle"],
+        stratify=par["stratify"]
+    )
+    
+    adata_train = adata.copy()
+    adata_test = adata.copy()
+    adata_train.obs = train_obs
+    adata_train.obs = test_obs
+    
+else:
+    if par["val_size"] >= 1-par["test_size"]:
+        raise ValueError("Validation set size must be less than the remaining dataset size after test set partitioning.")
+    if par["train_size"] and par["train_size"] + par["test_size"] + par["val_size"] != 1:
+        raise ValueError("The sum of all dataset partitions must be equal to 1.")
+    train_obs, test_obs = train_test_split(
+        adata.obs,
+        test_size=par["test_size"],
+        random_state=par["random_state"],
+        shuffle=par["shuffle"],
+        stratify=par["stratify"]
+    )
+    train_obs, val_obs = train_test_split(
+        train_obs,
+        test_size=par["val_size"] / (1 - par["test_size"]),
+        random_state=par["random_state"],
+        shuffle=par["shuffle"],
+        stratify=par["stratify"]
+    )
+    
+    adata_train = adata.copy()
+    adata_test = adata.copy()
+    adata_val = adata.copy()
+    adata_train.obs = train_obs
+    adata_test.obs = test_obs
+    adata_val.obs = val_obs
+    
+logger.info(f"Writing {par['output']}")
+
 
