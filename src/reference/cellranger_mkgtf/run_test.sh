c02aee52790cc900f863bf4aed1a82c3d6df71d9
jakubmajercik <jakub.majercik@gmail.com>
2024-03-28 11:40:47 +0100

    test unique attribute in result

--- src/reference/cellranger_mkgtf/run_test.sh
+++ src/reference/cellranger_mkgtf/run_test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 ## VIASH START
 meta_executable="bin/viash run src/reference/cellranger_mkgtf/config.vsh.yaml --"
 ## VIASH END
@@ -19,7 +17,7 @@ echo "> Running $meta_functionality_name, writing to $tmpdir."
 $meta_executable \
   --input_gtf "$tmpdir/reference_small.gtf.gz" \
   --output_gtf "$tmpdir/myreference_filtered.gtf.gz" \
-  --attribute="gene_type:transcribed_unprocessed_pseudogene" \
+  --attribute "gene_type:transcribed_unprocessed_pseudogene" \
   ---cpus ${meta_memory_gb:-1} \
   ---memory ${meta_memory_gb:-2}GB
 
@@ -28,5 +26,12 @@ exit_code=$?
 
 echo ">> Checking whether output can be found"
 [[ ! -f "$tmpdir/myreference_filtered.gtf.gz" ]] && echo "Output gtf file could not be found!" && exit 1
+
+echo ">> Checking attribute 'gene_type' in output gtf file"
+unique_gene_types=$(zcat "$tmpdir/myreference_filtered.gtf.gz" | awk -F'\t' '$9 ~ /gene_type/ { split($9, a, ";"); for(i in a) if(a[i] ~ /gene_type/) print a[i] }' | sed 's/.*gene_type "\(.*\)".*/\1/' | sort -u)
+if [[ $unique_gene_types != "transcribed_unprocessed_pseudogene" ]]; then
+  echo "Error: unique_gene_types is not equal to 'transcribed_unprocessed_pseudogene'"
+  exit 1
+fi
 
 echo "> Test succeeded!"
