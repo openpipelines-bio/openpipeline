24dcfaf98b0399d09d015ed5c880f126745dafd2
Kai Waldrant <kai@data-intuitive.com>
2024-03-28 22:37:24 +0100

    add pytest

--- src/workflows/test_workflows/multiomics/split_modalities/script.py
+++ src/workflows/test_workflows/multiomics/split_modalities/script.py
@@ -1,12 +1,15 @@
 import csv
 import os
 import mudata as mu
+import pytest
+from openpipelinetestutils.asserters import assert_annotation_objects_equal
 
 
 ##VIASH START
 par = {
   "input": "output_test/split_modalities/foo_types.csv",
-  "mod_dir": "output_test/split_modalities/h5mu"
+  "mod_dir": "output_test/split_modalities/h5mu",
+  "orig_input": "resources_test/pbmc_1k_protein_v3/pbmc_1k_protein_v3.h5mu"
 }
 
 meta = {
@@ -21,11 +24,21 @@ with open(par["input"], "r", encoding="utf-8") as f:
     reader = csv.reader(f)
     data = list(reader)
 
-csv_lines = len(data) -1
+
+input_mu = mu.read_h5mu(par["orig_input"], "pbmc_1k_protein_v3.h5mu")
+
+num_mod = len(data) - 1
 num_files = len(os.listdir(par["mod_dir"]))
 
 # Check if the number of files is equal to the number of lines in the csv
-assert csv_lines == num_files, f"Expected {csv_lines} files, but found {num_files}."
+assert num_mod == num_files, f"Expected {num_mod} files, but found {num_files}."
+assert input_mu.n_mod == num_mod, f"Expected {num_mod} modalities in {par['orig_input']} got {input_mu.n_mod} modalities."
+
+
+rna_mod = mu.read_h5mu(os.path.join(par["mod_dir"], data[1][1]))
+prot_mod = mu.read_h5mu(os.path.join(par["mod_dir"], data[2][1]))
+
+
 
 # Check if the files exist and if the modality name is in the file name
 for i, row in enumerate(data):
@@ -39,5 +52,10 @@ for i, row in enumerate(data):
     mod_mu = mu.read_h5mu(mod_fp)
     assert mod_mu.n_mod == 1, f"Expected 1 modality in {row[1]}."
     assert row[0] in mod_mu.mod.keys(), f"Expected {row[0]} to be the mod in {row[1]}."
+
+    if row[0] == "rna":
+        assert_annotation_objects_equal(rna_mod, mod_mu.mod["rna"])
+    elif row[0] == "prot":
+        assert_annotation_objects_equal(prot_mod, mod_mu.mod["prot"])
 
 print("Test successful!", flush=True)
