5b6136d173f47f54a61c1261cfe41eb6f5759417
jakubmajercik <jakub.majercik@gmail.com>
2024-04-17 15:32:43 +0200

    obs and var columns in output tested

--- src/workflows/test_workflows/qc/script.py
+++ src/workflows/test_workflows/qc/script.py
@@ -1,6 +1,9 @@
 # import os
 from mudata import read_h5mu
-# from openpipelinetestutils.asserters import assert_annotation_objects_equal
+# import pytest
+# import sys
+from openpipelinetestutils.asserters import assert_annotation_objects_equal
+from openpipelinetestutils.utils import remove_annotation_column
 
 
 ##VIASH START
@@ -13,10 +16,31 @@ meta = {
     "resources_dir": "resources_test/concat_test_data",
 }
 ##VIASH END
-
-print("Here")
-# input_mudata = read_h5mu(par["og_input"])
+    
+    
+# def test_fields_in_output():
+input_mudata = read_h5mu(par["og_input"])
 output_mudata = read_h5mu(par["input"])
 
+assert input_mudata.n_mod == output_mudata.n_mod, "Number of modalities should be the same"
+assert input_mudata.mod.keys() == output_mudata.mod.keys(), "Modalities should be the same"
+assert list(output_mudata.mod.keys()) == ["rna", "atac"], "Modalities should be rna and atac"
+
+obs_cols_to_remove = []
 for top_n_vars in ("50", "100", "200", "500"):
-    assert f"pct_of_counts_in_top_{top_n_vars}_vars" in output_mudata.mod['rna'].obs
+    obs_cols_to_remove.append(f"pct_of_counts_in_top_{top_n_vars}_vars")
+
+obs_cols_to_remove.extend(['total_counts', 'num_nonzero_vars'])
+var_cols_to_remove = ['obs_mean', 'total_counts', 'num_nonzero_obs', 'pct_dropout']
+
+assert set(obs_cols_to_remove).issubset(set(output_mudata.mod["rna"].obs.columns.to_list()))
+assert set(var_cols_to_remove).issubset(set(output_mudata.mod["rna"].var.columns.to_list()))
+
+initial_mudata = remove_annotation_column(output_mudata, obs_cols_to_remove, axis="obs", modality_name="rna")
+initial_mudata = remove_annotation_column(initial_mudata, var_cols_to_remove, axis="var", modality_name="rna")
+
+assert_annotation_objects_equal(input_mudata, initial_mudata)
+
+    
+# if __name__ == "__main__":
+#     sys.exit(pytest.main([__file__]))
