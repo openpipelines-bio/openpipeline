e0573ba4e0243da4b2f5f8b0b755156acb21c5ac
dorien-er <roosen.dorien@gmail.com>
2024-04-18 13:31:26 +0200

    update integration metrics

--- src/qc/integration_metrics/script.py
+++ src/qc/integration_metrics/script.py
@@ -1,5 +1,4 @@
 import scib
-import scanpy as sc
 import mudata as mu
 import numpy as np
 
@@ -8,23 +7,15 @@ import numpy as np
 par = {
     "input": "resources_test/scgpt/test_resources/Kim2020_Lung_integrated.h5mu",
     "modality": "rna",
-    "var_gene_names": None,
-    "uns_neighbors": "scGPT_integration_neighbors",
-    "obsp_neighbor_distances": "scGPT_integration_distances",
-    "obsp_neighbor_connectivities": "scGPT_integration_connectivities",
     "obsm_embeddings": "X_scGPT",
-    "obsm_umap": "X_scGPT_umap",
     "obs_batch_label": "sample",
     "obs_cell_label": "cell_type",
     "output_compression": None,
     "obs_cluster": "louvain_cluster",
-    "seed": None,
     "output": "resources_test/scgpt/test_resources/Kim2020_Lung_integrated_qc.h5mu",
 }
 
 ## VIASH END
-if par["seed"]:
-    np.random.seed(par["seed"])
 
 # START TEMPORARY WORKAROUND setup_logger
 # reason: resources aren't available when using Nextflow fusion
@@ -53,75 +44,54 @@ adata = input_adata.copy()
 # Ensure all batch labels are str
 adata.obs[par["obs_batch_label"]] = adata.obs[par["obs_batch_label"]].astype(str)
 
-# Remove nan from cell labels
-if adata.obs["cell_type"].isna().any():
-    logger.warning("Cell label obs column contains nan values: removing rows with nan value")
-    adata_t = adata[~adata.obs["cell_type"].isna()].copy()
-else:
-    adata_t = adata.copy()
-
-if par["obs_cell_label"]:
-    logger.info("Generating clusters")
-    res_max, nmi_max, nmi_all = scib.metrics.clustering.cluster_optimal_resolution(
-                adata_t,
-                label_key=par["obs_cell_label"],
-                cluster_key=par["obs_cluster"],
-                cluster_function=sc.tl.louvain,
-                use_rep=par["obsm_embeddings"],
-                metric=scib.metrics.nmi,
-                verbose=False,
-                return_all=True,
-                force=True,
-            )
-
-    logger.info("Calculating NMI score")
-    nmi_score = scib.metrics.nmi(
-            adata_t,
-            cluster_key=par["obs_cluster"],
-            label_key=par["obs_cell_label"],
-            implementation="arithmetic",
-            nmi_dir=None,
-        )
-
-    logger.info("Calculating ARI score")
-    ari_score = scib.metrics.ari(
-        adata_t,
+logger.info("Calculating NMI score")
+nmi_score = scib.metrics.nmi(
+        adata,
         cluster_key=par["obs_cluster"],
-        label_key=par["obs_cell_label"]
-    )
-
-    logger.info("Calculating AWS score (cell types)")
-    asw_label = scib.metrics.silhouette(
-        adata_t,
         label_key=par["obs_cell_label"],
-        embed=par["obsm_embeddings"],
-        metric="euclidean"
-        )
-
-    logger.info("Calculating AWS score (batches)")
-    asw_batch = scib.metrics.silhouette_batch(
-            adata_t,
-            batch_key=par["obs_batch_label"],
-            label_key=par["obs_cell_label"],
-            embed=par["obsm_embeddings"],
-            metric="euclidean",
-            return_all=False,
-            verbose=False,
-        )
-
-    logger.info("Calculating PC regression")
-    pcr_score = scib.metrics.pcr_comparison(
-        adata_t,
-        adata_t,
-        embed=par["obsm_embeddings"],
-        covariate=par["obs_batch_label"],
-        verbose=False
+        implementation="arithmetic",
+        nmi_dir=None,
     )
 
-    logger.info("Calculating graph connectivity")
-    graph_conn_score = scib.metrics.graph_connectivity(
-        adata_t,
-        label_key=par["obs_cell_label"]
+logger.info("Calculating ARI score")
+ari_score = scib.metrics.ari(
+    adata,
+    cluster_key=par["obs_cluster"],
+    label_key=par["obs_cell_label"]
+    )
+
+logger.info("Calculating AWS score (cell types)")
+asw_label = scib.metrics.silhouette(
+    adata,
+    label_key=par["obs_cell_label"],
+    embed=par["obsm_embeddings"],
+    metric="euclidean"
+    )
+
+logger.info("Calculating AWS score (batches)")
+asw_batch = scib.metrics.silhouette_batch(
+    adata,
+    batch_key=par["obs_batch_label"],
+    label_key=par["obs_cell_label"],
+    embed=par["obsm_embeddings"],
+    metric="euclidean",
+    return_all=False,
+    verbose=False,
+    )
+
+logger.info("Calculating PC regression")
+pcr_score = scib.metrics.pcr_comparison(
+    adata,
+    adata,
+    embed=par["obsm_embeddings"],
+    covariate=par["obs_batch_label"],
+    verbose=False
+    )
+
+logger.info("Calculating graph connectivity")
+graph_conn_score = scib.metrics.graph_connectivity(
+    adata,
+    label_key=par["obs_cell_label"]
     )
 
 logger.info("Calculating average bio score")
@@ -129,8 +99,8 @@ logger.info("Calculating average bio score")
 avg_bio = np.mean(
     [
         nmi_score, ari_score, asw_label
-    ]
-)
+        ]
+    )
 
 logger.info("Writing output data")
 adata.uns["ari_score"] = ari_score
