b53432d592204f9ab168df0c3f6ba6de27a72355
jakubmajercik <jakub.majercik@gmail.com>
2024-04-29 14:50:37 +0200

    updated with new test resources and the workflow branch

--- src/scgpt/cell_type_inference/test.py
+++ src/scgpt/cell_type_inference/test.py
@@ -1,8 +1,6 @@
 import pytest
-import subprocess
 from mudata import read_h5mu
 import sys
-from openpipelinetestutils.asserters import assert_annotation_objects_equal
 
 ## VIASH START
 meta = {
@@ -12,24 +10,17 @@ meta = {
 }
 ## VIASH END
 
-input_path = meta["resources_dir"] + "Kim2020_Lung_tokenized.h5mu"
+input_path = meta["resources_dir"] + "Kim2020_Lung_subset_tokenized.h5mu"
 model = meta["resources_dir"] + "best_model.pt"
 model_config = meta["resources_dir"] + "args.json"
 model_vocab = meta["resources_dir"] + "vocab.json" 
 
-@pytest.fixture
-def input_mudata_subset_cpu_run(write_mudata_to_file):
-    mudata = read_h5mu(input_path)
-    mudata.mod["rna"] = mudata.mod["rna"][:100]
-    return write_mudata_to_file(mudata)
-
 def test_cell_type_inference(run_component,
-                             random_h5mu_path,
-                             input_mudata_subset_cpu_run):
+                             random_h5mu_path):
+    
     output_path = random_h5mu_path()
-
     args = [
-        "--input", input_mudata_subset_cpu_run,
+        "--input", input_path,
         "--output",  output_path,
         "--modality", "rna",
         "--input_obsm_gene_tokens", "gene_id_tokens",
@@ -38,31 +29,23 @@ def test_cell_type_inference(run_component,
         "--model_config", model_config,
         "--model_vocab", model_vocab,
         "--gene_name_layer", "gene_name",
+        "--input_obs_batch_label", "sample",
         "--predicted_cell_type_id", "predictions",
         "--pad_token", "<pad>",
-        "--dsbn", "True",
+        "--DSBN", "True",
         "--pad_value", "-2",
         "--n_cls", "8",
         "--n_input_bins", "51",
         "--batch_size", "64"
     ]
     run_component(args)
-    
+
     output_mudata = read_h5mu(output_path)
-    input_mudata = read_h5mu(input_mudata_subset_cpu_run)
+    input_mudata = read_h5mu(input_path)
     assert "predictions" in output_mudata.mod["rna"].obs.columns
     assert output_mudata.mod["rna"].obs["predictions"].isna().sum() == 0
     assert output_mudata.mod["rna"].obs["predictions"].dtype == "int64"
     
-    # print(output_mudata)
-    # print(input_mudata)
-    # output_mudata.mod["rna"].obs.drop("predictions",
-    #                                   axis=1,
-    #                                   inplace=True)
-    
-    # # print(input_mudata.mod["rna"].obs.columns)
-    # # print(output_mudata.mod["rna"].obs.columns)
-    # assert_annotation_objects_equal(output_mudata, input_mudata)
-    
+
 if __name__ == '__main__':
     sys.exit(pytest.main([__file__]))
