1d2e8102f6c2ec01b98a4ff85acb2eba49afb764
jakubmajercik <jakub.majercik@gmail.com>
2024-04-30 11:34:58 +0200

    conversion integration test added

---
+++ src/workflows/test_workflows/ingestion/conversion/script.py
@@ -0,0 +1,19 @@
+from mudata import read_h5mu
+
+##VIASH START
+par = {
+    "input": "input.h5mu"
+}
+
+meta = {
+    "resources_dir": "resources_test"
+}
+##VIASH END
+
+input_mudata = read_h5mu(par["input"])
+
+assert "rna" in input_mudata.mod.keys()
+assert input_mudata.n_obs == 713
+assert input_mudata.mod["rna"].var["feature_types"].unique() == [
+    "Gene Expression"
+], "Output X should only contain Gene Expression vars."
