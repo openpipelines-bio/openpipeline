14de12468977e55faaf77706c9c6f13b4041ff38
dorien-er <roosen.dorien@gmail.com>
2024-05-05 14:05:55 +0200

    expand unit tests

--- src/scgpt/cell_type_annotation/test.py
+++ src/scgpt/cell_type_annotation/test.py
@@ -1,6 +1,9 @@
 import pytest
 from mudata import read_h5mu
 import sys
+import numpy as np
+import subprocess
+import re
 
 ## VIASH START
 meta = {
@@ -16,19 +19,20 @@ model_config = meta["resources_dir"] + "args.json"
 model_vocab = meta["resources_dir"] + "vocab.json" 
 
 def test_cell_type_inference(run_component,
-                             random_h5mu_path):
-    
-    output_path = random_h5mu_path()
+                             tmp_path):
+
+    output_annotation_file = tmp_path / "Kim2020_Lung_subset_annotated.h5mu"
     args = [
         "--input", input_path,
-        "--output",  output_path,
+        "--output",  output_annotation_file,
         "--modality", "rna",
         "--obsm_gene_tokens", "gene_id_tokens",
         "--obsm_tokenized_values", "values_tokenized",
         "--model", model,
         "--model_vocab", model_vocab,
+        "--model_config", model_config,
         "--obs_batch_label", "sample",
-        "--predicted_cell_type_id", "predictions",
+        "--obs_predicted_cell_type", "predicted_cell_type",
         "--pad_token", "<pad>",
         "--dsbn", "True",
         "--pad_value", "-2",
@@ -38,10 +42,99 @@ def test_cell_type_inference(run_component,
     ]
     run_component(args)
 
-    output_mudata = read_h5mu(output_path)
-    assert "predicted_cell_type" in output_mudata.mod["rna"].obs.columns
-    assert output_mudata.mod["rna"].obs["predicted_cell_type"].isna().sum() == 0
-    assert output_mudata.mod["rna"].obs["predicted_cell_type"].dtype == "int64"
+    output_mudata = read_h5mu(output_annotation_file)
+    output_adata = output_mudata.mod["rna"]
+    assert "predicted_cell_type" in output_adata.obs.keys(),  "predicted_cell_type is not present in anndata obs keys"
+    assert not all(np.isnan(output_adata.obs["predicted_cell_type"])), "predicted cell types contain nan values"
+    assert output_adata.obs["predicted_cell_type"].dtype == "int64", "predicted cell types are not integers"
+
+    # run withou dsbn
+    output_annotation_file_without_dsbn = tmp_path / "Kim2020_Lung_subset_annotated_no_dsbn.h5mu"
+    args = [
+        "--input", input_path,
+        "--output",  output_annotation_file_without_dsbn,
+        "--modality", "rna",
+        "--obsm_gene_tokens", "gene_id_tokens",
+        "--obsm_tokenized_values", "values_tokenized",
+        "--model", model,
+        "--model_vocab", model_vocab,
+        "--model_config", model_config,
+        "--obs_batch_label", "sample",
+        "--obs_predicted_cell_type", "predicted_cell_type",
+        "--dsbn", "False",
+    ]
+    run_component(args)
+    # Read output file
+    output_mdata_no_dsbn = read_h5mu(output_annotation_file_without_dsbn)
+    output_adata_no_dsbn = output_mdata_no_dsbn.mod["rna"]
+
+    # Assert that embeddings without dsbn are different
+    assert not (output_adata.obs["predicted_cell_type"] == output_adata_no_dsbn.obsm["predicted_cell_type"]).all(), "Cell type predictions with and without dsbn are the same"
+
+def test_annotation_dsbn_without_batch_labels(run_component, tmp_path):
+    output_embedding_file = tmp_path / "Kim2020_Lung_subset_embedded.h5mu"
+
+    args = [
+        "--input", input_path,
+        "--output",  output_embedding_file,
+        "--modality", "rna",
+        "--obsm_gene_tokens", "gene_id_tokens",
+        "--obsm_tokenized_values", "values_tokenized",
+        "--model", model,
+        "--model_vocab", model_vocab,
+        "--model_config", model_config,
+        "--obs_predicted_cell_type", "predicted_cell_type",
+        "--dsbn", "False",
+    ]
+
+    with pytest.raises(subprocess.CalledProcessError) as err:
+        run_component(args)
+    assert re.search(
+        r"ValueError: When dsbn is set to True, you are required to provide batch labels \(input_obs_batch_labels\)\.",
+        err.value.stdout.decode('utf-8'))
+
+def test_annotation_non_existing_keys(run_component, tmp_path):
+    output_file = tmp_path / "Kim2020_Lung_subset_annotated_no_dsbn.h5mu"
+    args_1 = [
+        "--input", input_path,
+        "--output",  output_file,
+        "--modality", "rna",
+        "--obsm_gene_tokens", "gene_id_tokens",
+        "--obsm_tokenized_values", "values_tokenized",
+        "--model", model,
+        "--model_vocab", model_vocab,
+        "--model_config", model_config,
+        "--obs_batch_label", "dummy_batch_label_key",
+        "--obs_predicted_cell_type", "predicted_cell_type",
+        "--dsbn", "True",
+    ]
+    run_component(args_1)
+
+    with pytest.raises(subprocess.CalledProcessError) as err:
+        run_component(args_1)
+    assert re.search(
+        r"KeyError: \'dummy_batch_label_key\'",
+        err.value.stdout.decode('utf-8'))
+
+    # Test for non-existing tokenized values key
+    args_2 = [
+        "--input", input_path,
+        "--output",  output_file,
+        "--modality", "rna",
+        "--obsm_gene_tokens", "gene_id_tokens",
+        "--obsm_tokenized_values", "dummy_values_tokenized",
+        "--model", model,
+        "--model_vocab", model_vocab,
+        "--model_config", model_config,
+        "--obs_predicted_cell_type", "predicted_cell_type",
+        "--dsbn", "False",
+    ]
+
+    with pytest.raises(subprocess.CalledProcessError) as err:
+        run_component(args_2)
+    assert re.search(
+        r'KeyError: "The parameter \'dummy_values_tokenized\' provided for \'--obsm_tokenized_values\' could not be found in adata.obsm"',
+        err.value.stdout.decode('utf-8'))
 
 
 if __name__ == '__main__':
