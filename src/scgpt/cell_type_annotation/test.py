1c4d115d0053695c2c4577193795d1be4443e669
dorien-er <roosen.dorien@gmail.com>
2024-05-05 20:46:03 +0200

    fix unit tests

--- src/scgpt/cell_type_annotation/test.py
+++ src/scgpt/cell_type_annotation/test.py
@@ -16,7 +16,7 @@ meta = {
 input_path = meta["resources_dir"] + "Kim2020_Lung_subset_tokenized.h5mu"
 model = meta["resources_dir"] + "best_model.pt"
 model_config = meta["resources_dir"] + "args.json"
-model_vocab = meta["resources_dir"] + "vocab.json" 
+model_vocab = meta["resources_dir"] + "vocab.json"
 
 def test_cell_type_inference(run_component,
                              tmp_path):
@@ -32,7 +32,8 @@ def test_cell_type_inference(run_component,
         "--model_vocab", model_vocab,
         "--model_config", model_config,
         "--obs_batch_label", "sample",
-        "--obs_predicted_cell_type", "predicted_cell_type"
+        "--obs_predicted_cell_type", "predicted_cell_type",
+        "--dsbn", True
     ]
     run_component(args)
 
@@ -55,7 +56,7 @@ def test_cell_type_inference(run_component,
         "--model_config", model_config,
         "--obs_batch_label", "sample",
         "--obs_predicted_cell_type", "predicted_cell_type",
-        "--dsbn", "False",
+        "--dsbn", "False"
     ]
     run_component(args)
     # Read output file
@@ -65,12 +66,13 @@ def test_cell_type_inference(run_component,
     # Assert that embeddings without dsbn are different
     assert not (output_adata.obs["predicted_cell_type"] == output_adata_no_dsbn.obs["predicted_cell_type"]).all(), "Cell type predictions with and without dsbn are the same"
 
+
 def test_annotation_dsbn_without_batch_labels(run_component, tmp_path):
-    output_embedding_file = tmp_path / "Kim2020_Lung_subset_embedded.h5mu"
+    output_annotation_labels_without_dsbn = tmp_path / "Kim2020_Lung_subset_annotated_labels_without_dsbn.h5mu"
 
     args = [
         "--input", input_path,
-        "--output",  output_embedding_file,
+        "--output",  output_annotation_labels_without_dsbn,
         "--modality", "rna",
         "--obsm_gene_tokens", "gene_id_tokens",
         "--obsm_tokenized_values", "values_tokenized",
@@ -84,36 +86,18 @@ def test_annotation_dsbn_without_batch_labels(run_component, tmp_path):
     with pytest.raises(subprocess.CalledProcessError) as err:
         run_component(args)
     assert re.search(
-        r"ValueError: When dsbn is set to True, you are required to provide batch labels \(input_obs_batch_labels\)\.",
+        r"ValueError: When dsbn is set to True, you are required to provide batch labels \(obs_batch_labels\)\.",
         err.value.stdout.decode('utf-8'))
+
 
 def test_annotation_non_existing_keys(run_component, tmp_path):
-    output_file = tmp_path / "Kim2020_Lung_subset_annotated_no_dsbn.h5mu"
-    args_1 = [
-        "--input", input_path,
-        "--output",  output_file,
-        "--modality", "rna",
-        "--obsm_gene_tokens", "gene_id_tokens",
-        "--obsm_tokenized_values", "values_tokenized",
-        "--model", model,
-        "--model_vocab", model_vocab,
-        "--model_config", model_config,
-        "--obs_batch_label", "dummy_batch_label_key",
-        "--obs_predicted_cell_type", "predicted_cell_type",
-        "--dsbn", "True",
-    ]
-    run_component(args_1)
 
-    with pytest.raises(subprocess.CalledProcessError) as err:
-        run_component(args_1)
-    assert re.search(
-        r"KeyError: \'dummy_batch_label_key\'",
-        err.value.stdout.decode('utf-8'))
+    output_annotation_dummy_values = tmp_path / "Kim2020_Lung_subset_annotated_dummy_key.h5mu"
 
     # Test for non-existing tokenized values key
     args_2 = [
         "--input", input_path,
-        "--output",  output_file,
+        "--output",  output_annotation_dummy_values,
         "--modality", "rna",
         "--obsm_gene_tokens", "gene_id_tokens",
         "--obsm_tokenized_values", "dummy_values_tokenized",
