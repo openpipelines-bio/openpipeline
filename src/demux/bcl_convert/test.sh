c5a6893f6a779726cf475f94a726cce3a2830fb2
dorien-er <roosen.dorien@gmail.com>
2024-05-15 13:27:33 +0200

    add more unit tests

--- src/demux/bcl_convert/test.sh
+++ src/demux/bcl_convert/test.sh
@@ -44,7 +44,7 @@ echo ">>> Checking whether output fastq files are created"
 [[ ! -f  "$TMPDIR/output2/Undetermined_S0_R1_001.fastq.gz" ]] && echo "Output fastq files could not be found!" && exit 1
 [[ ! -f "$TMPDIR/output2/s1_S1_R1_001.fastq.gz" ]] && echo "Output fastq files could not be found!" && exit 1
 
-echo ">>> Test no lane splitting"
+echo ">>> Test no lane splitting argument"
 awk '!/NoLaneSplitting/' "$meta_resources_dir/bcl2/sample_sheet.csv" > "$TMPDIR/sample_sheet_without_LaneSplitting.csv"
 
 $meta_executable \
@@ -57,9 +57,22 @@ $meta_executable \
 echo ">>> Checking whether the output dir exists"
 [[ ! -d "$TMPDIR/output3" ]] && echo "Output dir could not be found!" && exit 1
 
-echo ">>> Checking whether output fastq files are created"
+echo ">>> Checking whether output fastq files with lane splitting are created"
+
 [[ ! -f  "$TMPDIR/output3/Undetermined_S0_R1_001.fastq.gz" ]] && echo "Output fastq files could not be found!" && exit 1
 [[ ! -f "$TMPDIR/output3/s1_S1_R1_001.fastq.gz" ]] && echo "Output fastq files could not be found!" && exit 1
+
+$meta_executable \
+  --input "$meta_resources_dir/bcl2" \
+  --sample_sheet "$TMPDIR/sample_sheet_without_LaneSplitting.csv" \
+  --output "$TMPDIR/output4" \
+  --test_mode true \
+  --no_lane_splitting false
+
+echo ">>> Checking whether output fastq files without lane splitting are created"
+
+[[ ! -f  "$TMPDIR/output4/Undetermined_S0_L001_R1_001.fastq.gz" ]] && echo "Output fastq files could not be found!" && exit 1
+[[ ! -f "$TMPDIR/output4/s1_S1_L001_R1_001.fastq.gz" ]] && echo "Output fastq files could not be found!" && exit 1
 
 # print final message
 echo ">>> Test finished successfully"
