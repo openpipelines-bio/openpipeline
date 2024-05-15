966e9b93e11db00dacdcd9e155b0da13b7b670f7
Dorien <41797896+dorien-er@users.noreply.github.com>
2024-05-15 16:52:38 +0200

    Resolve merge conflicts

--- src/demux/bcl_convert/test.sh
+++ src/demux/bcl_convert/test.sh
@@ -46,28 +46,41 @@ $meta_executable \
   --strict_mode false
 
 echo ">>> Checking whether the output dir exists"
-[[ ! -d "$TMPDIR/output1" ]] && echo "Output dir could not be found!" && exit 1
-
-echo ">>> Checking whether output fastq files are created"
-[[ ! -f  "$TMPDIR/output1/Undetermined_S0_R1_001.fastq.gz" ]] && echo "Output fastq files could not be found!" && exit 1
-[[ ! -f "$TMPDIR/output1/s1_S1_R1_001.fastq.gz" ]] && echo "Output fastq files could not be found!" && exit 1
-
-echo ">>> Test strict mode"
-cp -r "$meta_resources_dir/bcl2" "$TMPDIR/strict_input"
-rm "$TMPDIR/strict_input/Data/Intensities/L001/s_1_1101.clocs"
-
-$meta_executable \
-  --input "$TMPDIR/strict_input" \
-  --sample_sheet "$meta_resources_dir/bcl2/sample_sheet.csv" \
-  --output "$TMPDIR/output2" \
-  --strict_mode false
-
-echo ">>> Checking whether the output dir exists"
 [[ ! -d "$TMPDIR/output2" ]] && echo "Output dir could not be found!" && exit 1
 
 echo ">>> Checking whether output fastq files are created"
 [[ ! -f  "$TMPDIR/output2/Undetermined_S0_R1_001.fastq.gz" ]] && echo "Output fastq files could not be found!" && exit 1
 [[ ! -f "$TMPDIR/output2/s1_S1_R1_001.fastq.gz" ]] && echo "Output fastq files could not be found!" && exit 1
+
+echo ">>> Test no lane splitting argument"
+awk '!/NoLaneSplitting/' "$meta_resources_dir/bcl2/sample_sheet.csv" > "$TMPDIR/sample_sheet_without_LaneSplitting.csv"
+
+$meta_executable \
+  --input "$meta_resources_dir/bcl2" \
+  --sample_sheet "$TMPDIR/sample_sheet_without_LaneSplitting.csv" \
+  --output "$TMPDIR/output3" \
+  --test_mode true \
+  --no_lane_splitting true
+
+echo ">>> Checking whether the output dir exists"
+[[ ! -d "$TMPDIR/output3" ]] && echo "Output dir could not be found!" && exit 1
+
+echo ">>> Checking whether output fastq files with lane splitting are created"
+
+[[ ! -f  "$TMPDIR/output3/Undetermined_S0_R1_001.fastq.gz" ]] && echo "Output fastq files could not be found!" && exit 1
+[[ ! -f "$TMPDIR/output3/s1_S1_R1_001.fastq.gz" ]] && echo "Output fastq files could not be found!" && exit 1
+
+$meta_executable \
+  --input "$meta_resources_dir/bcl2" \
+  --sample_sheet "$TMPDIR/sample_sheet_without_LaneSplitting.csv" \
+  --output "$TMPDIR/output4" \
+  --test_mode true \
+  --no_lane_splitting false
+
+echo ">>> Checking whether output fastq files without lane splitting are created"
+
+[[ ! -f  "$TMPDIR/output4/Undetermined_S0_L001_R1_001.fastq.gz" ]] && echo "Output fastq files could not be found!" && exit 1
+[[ ! -f "$TMPDIR/output4/s1_S1_L001_R1_001.fastq.gz" ]] && echo "Output fastq files could not be found!" && exit 1
 
 # print final message
 echo ">>> Test finished successfully"
