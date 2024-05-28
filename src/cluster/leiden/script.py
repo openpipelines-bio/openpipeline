07181c7409a19c7a7b258dba3e718138f1425470
DriesSchaumont <5946712+DriesSchaumont@users.noreply.github.com>
2024-05-28 13:07:31 +0200

    Update logging

--- src/cluster/leiden/script.py
+++ src/cluster/leiden/script.py
@@ -1,7 +1,10 @@
+import logging.handlers
 import sys
 import signal
 import os
 import time
+import logging
+import warnings
 import mudata as mu
 import pandas as pd
 import scanpy as sc
@@ -27,28 +30,12 @@ par = {
     "output_compression": "gzip"
 }
 meta = {
-    "cpus": 2,
+    "cpus": 8,
     "resources_dir": '.'
 }
 ## VIASH END
 
 sys.path.append(meta["resources_dir"])
-# START TEMPORARY WORKAROUND setup_logger
-# reason: resources aren't available when using Nextflow fusion
-# from setup_logger import setup_logger
-def setup_logger():
-    import logging
-    from sys import stdout
-
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    console_handler = logging.StreamHandler(stdout)
-    logFormatter = logging.Formatter("%(asctime)s %(levelname)-8s %(message)s")
-    console_handler.setFormatter(logFormatter)
-    logger.addHandler(console_handler)
-
-    return logger
-# END TEMPORARY WORKAROUND setup_logger
 
 # START TEMPORARY WORKAROUND compress_h5mu
 # reason: resources aren't available when using Nextflow fusion
@@ -102,7 +89,7 @@ def compress_h5mu(input_path: Union[str, Path],
         f.write(b"\0" * (512 - nbytes)) 
 # END TEMPORARY WORKAROUND compress_h5mu
 
-logger = setup_logger()
+_shared_logger_name = "leiden"
 
 class SharedNumpyMatrix():
     def __init__(self, shared_memory: shared_memory.SharedMemory, dtype: npt.DTypeLike, shape: tuple[int, int]) -> None:
@@ -163,30 +150,42 @@ def create_empty_anndata_with_connectivities(connectivities, obs_names):
     return empty_anndata
 
 def run_single_resolution(shared_csr_matrix, obs_names, resolution):
-    print(f"Process with PID {os.getpid()} for resolution {resolution} started")
+    logger = logging.getLogger(_shared_logger_name)
+    logger.info("Process with PID '%s' for resolution '%s' started", os.getpid(), resolution)
     try:
         connectivities = shared_csr_matrix.to_csr_matrix()
         adata = create_empty_anndata_with_connectivities(connectivities, obs_names)
-        adata_out = sc.tl.leiden(
-            adata,
-            resolution=resolution,
-            key_added=str(resolution),
-            obsp="connectivities",
-            copy=True
-            )
-        print(f"Returning result for resolution {resolution}")
+        with warnings.catch_warnings():
+            # In the future, the default backend for leiden will be igraph instead of leidenalg.
+            warnings.simplefilter(action='ignore', category=FutureWarning)
+            adata_out = sc.tl.leiden(
+                adata,
+                resolution=resolution,
+                key_added=str(resolution),
+                obsp="connectivities",
+                copy=True
+                )
+        logger.info(f"Returning result for resolution {resolution}")
         return adata_out.obs[str(resolution)]
     finally:
         obs_names.shm.close()
         shared_csr_matrix.close() 
 
-def start_orphan_checker(parent_process_id, exit_event):
+def init_worker(parent_process_id, exit_event, log_queue, log_level):
     import os
     import threading
     import time
     pid = os.getpid()
-    
+        
+    logger = logging.getLogger(_shared_logger_name)
+    logger.setLevel(log_level)
+
+    handler = logging.handlers.QueueHandler(log_queue)
+    logger.addHandler(handler)
+
+    logger.info("Initializing process %s", pid)
     def exit_if_orphaned():
+        logger.info("Starting orphanned process checker for process %s.", pid)
         while True:
             # Check if parent process is gone
             try:
@@ -194,6 +193,7 @@ def start_orphan_checker(parent_process_id, exit_event):
                 # this can be used to check for the existence of a process ID
                 os.kill(parent_process_id, 0)
             except ProcessLookupError:
+                logger.info("Parent process is gone, shutting down %s", pid)
                 # Kill self
                 os.kill(pid, signal.SIGTERM)
             time.sleep(0.2)
@@ -201,83 +201,106 @@ def start_orphan_checker(parent_process_id, exit_event):
             try:
                 exit_event_set = exit_event.wait(timeout=1)
             except BrokenPipeError:
+                logger.info("Checking for shutdown resulted in BrokenPipeError, "
+                            "parent process is most likely gone. Shutting down %s", pid) 
                 os.kill(pid, signal.SIGTERM) 
             else:
                 if exit_event_set:
+                    logger.info("Exit event set, shutting down %s", pid)  
                     os.kill(pid, signal.SIGTERM)
             time.sleep(1)
     threading.Thread(target=exit_if_orphaned, daemon=True).start()
+    logger.info("Initialization of process %s is complete, process is now waiting for work.", pid)
 
 def main():
-    logger.info("Reading %s.", par["input"])
-    adata = mu.read_h5ad(par["input"], mod=par['modality'], backed='r')
-    logger.info("Processing modality '%s'.", par['modality'])
-    try:
-        connectivities = adata.obsp[par['obsp_connectivities']]
-    except KeyError:
-        raise ValueError(f"Could not find .obsp key \"{par['obsp_connectivities']}\" "
-                        "in modality {par['modality']}")
+    with managers.SyncManager() as syncm:
+        log_level = logging.INFO
+        log_format = "%(name)s:%(levelname)s:%(asctime)s: %(message)s"
+        formatter = logging.Formatter(log_format)
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setFormatter(formatter)
+        log_queue = syncm.Queue()
+        log_listener = logging.handlers.QueueListener(log_queue, console_handler)
+        log_listener.start()
+        
+        logger = logging.getLogger(_shared_logger_name)
+        logger.setLevel(log_level)
+        handler = logging.handlers.QueueHandler(log_queue)
+        logger.addHandler(handler)
 
-    with managers.SharedMemoryManager() as smm, managers.SyncManager() as syncm:
-        exit_early_event = syncm.Event()
-        # anndata converts the index to strings, so no worries that it cannot be stored in ShareableList
-        # because it has an unsupported dtype. It should always be string...
-        index_contents = adata.obs.index.to_list()
-        assert all([isinstance(item, str) for item in index_contents])
-        obs_names = smm.ShareableList(index_contents)
-
-        shared_csr_matrix = SharedCsrMatrix.from_csr_matrix(smm, connectivities)
-        results = {}
-        n_workers = meta['cpus'] - 2 if (meta['cpus'] and (meta['cpus'] - 2) > 0) else 1
-        print(f"Requesting {n_workers} workers", flush=True)
-        executor = ProcessPoolExecutor(max_workers=n_workers,
-                                       max_tasks_per_child=1, 
-                                       mp_context=get_context('spawn'),
-                                       initializer=start_orphan_checker,
-                                       initargs=((os.getpid(), exit_early_event)))
-        pending_futures = {executor.submit(run_single_resolution, shared_csr_matrix, obs_names, resolution): resolution
-                           for resolution in par["resolution"]}
+        logger.info("Reading %s.", par["input"])
+        adata = mu.read_h5ad(par["input"], mod=par['modality'], backed='r')
+        logger.info("Processing modality '%s'.", par['modality'])
         try:
-            print("All futures sheduled", flush=True)
-            for done_future in as_completed(pending_futures):
-                resolution = pending_futures[done_future]
-                data = done_future.result()
-                print(f"Processed resolution '{resolution}'", flush=True)
-                results[str(resolution)] = data
-        except process.BrokenProcessPool:
-            # This assumes that one of the child processses was killed by the kernel
-            # because the oom killer was activated. This the is the most likely scenario,
-            # other causes could be:
-            # * Subprocess terminates without raising a proper exception.
-            # * The code of the process handling the communication is broke (i.e. a python bug)
-            # * The return data could not be pickled.
-            print("BrokenProcessPool is raised", file=sys.stderr, flush=True)
-            executor.shutdown(wait=False, cancel_futures=True)
-            time.sleep(3)
-            exit_early_event.set()
-            time.sleep(3)
-            sys.exit(137)
-        finally:
-            print("Closing shared resources in main process", flush=True)
-            shared_csr_matrix.close()
-            obs_names.shm.close()
-            print("Shared resources closed", flush=True)
-        print("Waiting for shutdown of processes", flush=True)
-        executor.shutdown()
-        print("Executor shut down.", flush=True)
-    adata.obsm[par["obsm_name"]] = pd.DataFrame(results)
-    logger.info("Writing to %s.", par["output"])
+            connectivities = adata.obsp[par['obsp_connectivities']]
+        except KeyError:
+            raise ValueError(f"Could not find .obsp key \"{par['obsp_connectivities']}\" "
+                            "in modality {par['modality']}")
 
-    output_file = Path(par["output"])
-    logger.info('Writing output to %s.', par['output'])
-    output_file_uncompressed = output_file.with_name(output_file.stem + "_uncompressed.h5mu") \
-        if par["output_compression"] else output_file
-    shutil.copyfile(par['input'], output_file_uncompressed)
-    mu.write_h5ad(filename=output_file_uncompressed, mod=par['modality'], data=adata)
-    if par["output_compression"]:
-        compress_h5mu(output_file_uncompressed, output_file, compression=par["output_compression"])
-        output_file_uncompressed.unlink()
-    logger.info("Finished.")
+
+        # An event that, when triggered, will kill the child processes that are still running
+        exit_early_event = syncm.Event()
+        with managers.SharedMemoryManager() as smm:
+            # anndata converts the index to strings, so no worries that it cannot be stored in ShareableList
+            # because it has an unsupported dtype. It should always be string...
+            index_contents = adata.obs.index.to_list()
+            assert all([isinstance(item, str) for item in index_contents])
+            obs_names = smm.ShareableList(index_contents)
+
+            shared_csr_matrix = SharedCsrMatrix.from_csr_matrix(smm, connectivities)
+            results = {}
+            n_workers = meta['cpus'] - 2 if (meta['cpus'] and (meta['cpus'] - 2) > 0) else 1
+            logger.info(f"Requesting {n_workers} workers")
+            executor = ProcessPoolExecutor(max_workers=n_workers,
+                                        max_tasks_per_child=1, 
+                                        mp_context=get_context('spawn'),
+                                        initializer=init_worker,
+                                        initargs=((os.getpid(), exit_early_event, log_queue, log_level)))
+            pending_futures = {executor.submit(run_single_resolution, shared_csr_matrix, obs_names, resolution): resolution
+                               for resolution in par["resolution"]}
+            try:
+                logger.info("All futures sheduled")
+                for done_future in as_completed(pending_futures):
+                    resolution = pending_futures[done_future]
+                    data = done_future.result()
+                    logger.info(f"Processed resolution '{resolution}'")
+                    results[str(resolution)] = data
+            except process.BrokenProcessPool:
+                # This assumes that one of the child processses was killed by the kernel
+                # because the oom killer was activated. This the is the most likely scenario,
+                # other causes could be:
+                # * Subprocess terminates without raising a proper exception.
+                # * The code of the process handling the communication is broke (i.e. a python bug)
+                # * The return data could not be pickled.
+                logger.error("BrokenProcessPool is raised")
+                executor.shutdown(wait=False, cancel_futures=True)
+                time.sleep(3)
+                exit_early_event.set()
+                time.sleep(3)
+                sys.exit(137)
+            finally:
+                logger.info("Closing shared resources in main process")
+                shared_csr_matrix.close()
+                obs_names.shm.close()
+                logger.info("Shared resources closed")
+            logger.info("Waiting for shutdown of processes")
+            executor.shutdown()
+            logger.info("Executor shut down.")
+        adata.obsm[par["obsm_name"]] = pd.DataFrame(results)
+
+        output_file = Path(par["output"])
+        logger.info('Writing output to %s.', par['output'])
+        output_file_uncompressed = output_file.with_name(output_file.stem + "_uncompressed.h5mu") \
+            if par["output_compression"] else output_file
+        shutil.copyfile(par['input'], output_file_uncompressed)
+        mu.write_h5ad(filename=output_file_uncompressed, mod=par['modality'], data=adata)
+        if par["output_compression"]:
+            compress_h5mu(output_file_uncompressed, output_file, compression=par["output_compression"])
+            output_file_uncompressed.unlink()
+        logger.info("Finished.")
+        log_listener.enqueue_sentinel()
+        time.sleep(3)
+
 
 if __name__ == "__main__":
     main()
