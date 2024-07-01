bafa247d7bce06e14de37b3f2f9b06b884901826
dorien-er <roosen.dorien@gmail.com>
2024-07-01 10:27:39 +0200

    add unit tests

--- src/dataflow/split_samples/script.py
+++ src/dataflow/split_samples/script.py
@@ -50,6 +50,7 @@ def main():
     obs_features_s = [re.sub(r'[-\s]', "_", s) for s in obs_features]
     obs_features_s = [re.sub(r'[^A-Za-z0-9_]', "", s) for s in obs_features]
 
+    # ensure that names are unique, if not append number as suffix
     if not len(obs_features_s) == len(set(obs_features_s)):
         logger.info("Ensuring unique names for par['obs_feature']")
         counts = {}
@@ -64,23 +65,29 @@ def main():
     output_dir = Path(par["output"])
     if not output_dir.is_dir():
         output_dir.mkdir(parents=True)
-
+        
+    # split modality of mdata file base on obs_feature
     logger.info(f"Splitting file based on {par['obs_feature']} values {obs_features}")
     obs_files = []
 
     for obs_name, file_name in zip(obs_features, obs_features_s):
         logger.info(f"Filtering modality '{par['modality']}' observations by .obs['{par['obs_feature']}'] == {obs_name}")
         mdata_obs = mdata.copy()
+        adata_obs = mdata_obs.mod[par["modality"]]
 
-        mdata_obs = mdata_obs[mdata_obs.mod[par['modality']].obs[par["obs_feature"]] == obs_name]
+        # split the samples
+        adata_obs = adata_obs[adata_obs.obs[par["obs_feature"]] == obs_name]
         mdata_obs_name = f"{input_file.stem}_{file_name}.h5mu"
         obs_files.append(mdata_obs_name)
 
+        # Dropping columns that only have nan values after splitting
         if par["drop_obs_nan"]:
             logger.info(f"Dropping all .obs columns with NaN values")
-            mdata_obs.mod[par["modality"]].obs.dropna(axis=1, how='all', inplace=True)
+            adata_obs.obs.dropna(axis=1, how='all', inplace=True)
 
+        # replace mdata file with modality adata contianing split samples
         logger.info(f"Writing h5mu filtered for {par['obs_feature']} {obs_name} to file {output_dir / mdata_obs_name}")
+        mdata_obs.mod[par["modality"]] = adata_obs
         mdata_obs.write_h5mu(output_dir / mdata_obs_name, compression=par["output_compression"])
 
     logger.info(f"Writing output_files CSV file to {par['output_files']}")
---
+++ src/dataflow/split_samples/test.py
@@ -0,0 +1,113 @@
+import sys
+from textwrap import dedent
+import pytest
+import mudata as mu
+import anndata as ad
+import numpy as np
+import pandas as pd
+
+
+## VIASH START
+meta = {
+    'functionality_name': './target/native/dataflow/split_modalities/split_modalities',
+    'resources_dir': './resources_test/',
+    'config': './src/dataflow/split_modalities/config.vsh.yaml',
+    'executable': './target/docker/dataflow/split_modalities/split_modalities'
+}
+## VIASH END
+
+@pytest.fixture
+def input_modality_1():
+    df = pd.DataFrame([[1, 2, 3], [4, 5, 6]], index=["obs1", "obs2"], columns=["var1", "var2", "var3"])
+    obs = pd.DataFrame([["A"], ["B"]], index=df.index, columns=["Obs"])
+    var = pd.DataFrame([["a"], ["b"], ["c"]], index=df.columns, columns=["Feat"])
+    ad1 = ad.AnnData(df, obs=obs, var=var)
+    return ad1
+
+
+@pytest.fixture
+def input_modality_2():
+    df = pd.DataFrame([[1, 2, 3], [4, 5, 6]], index=["obs1", "obs2"], columns=["var1", "var2", "var3"])
+    var2 = pd.DataFrame(["d", "e", "g"], index=df.columns, columns=["Feat"])
+    obs2 = pd.DataFrame(["C", "D"], index=df.index, columns=["Obs"])
+    ad2 = ad.AnnData(df, obs=obs2, var=var2)
+    return ad2
+
+
+@pytest.fixture
+def input_h5mu(input_modality_1, input_modality_2):
+    tmp_mudata = mu.MuData({'mod1': input_modality_1, 'mod2': input_modality_2})
+    return tmp_mudata
+
+
+@pytest.fixture
+def input_h5mu_path(write_mudata_to_file, input_h5mu):
+    return write_mudata_to_file(input_h5mu)
+
+
+
+def test_split(run_component, random_path, input_h5mu, input_h5mu_path):
+    output_dir = random_path()
+    output_files = random_path(extension="csv")
+    args = [
+        "--input", input_h5mu_path,
+        "--output", str(output_dir),
+        "--modalitry", "mod1",
+        "--obs_feature", "Obs",
+        "--output_files", str(output_files),
+    ]
+
+    run_component(args)
+    assert output_files.is_file()
+    assert output_dir.is_dir()
+
+    # check output dir and file names
+    dir_content = [h5mu_file for h5mu_file in output_dir.iterdir() 
+                   if h5mu_file.suffix == ".h5mu" and h5mu_file != input_h5mu_path]
+    s1_file = output_dir / f"{input_h5mu_path.stem}_A.h5mu"
+    s2_file = output_dir / f"{input_h5mu_path.stem}_B.h5mu"
+    assert set(dir_content) == set([s1_file, s2_file])
+
+    # check that number of modalities, variables and observations
+    s1 = mu.read_h5mu(s1_file)
+    s2 = mu.read_h5mu(s2_file)
+    assert s1.n_mod == 2
+    assert s2.n_mod == 2
+
+    assert s1.n_obs == input_h5mu.n_obs, "number of modalities of split file does not match input file"
+    assert s2.n_obs == input_h5mu.n_obs, "number of modalities of split file does not match input file"
+
+    assert s1.mod["mod1"].n_obs == 1, "number of observations of split file s1 modality mod1 should equal 1"
+    assert s1.mod["mod2"].n_obs == input_h5mu.n_obs, "number of observations of split file s1 modality mod2 should equal input file"
+
+    assert s2.mod["mod1"].n_obs == input_h5mu.n_obs, "number of observations of split file s2 modality mod1 should equal input file"
+    assert s2.mod["mod2"].n_obs == input_h5mu.n_obs, "number of observations of split file s2 modality mod2 should equal input file"
+
+    assert s1.n_vars == input_h5mu.n_vars, "number of variables of split file s1 should equal input file"
+    assert s2.n_vars == input_h5mu.n_vars, "number of variables of split file s1 should equal input file"
+
+    assert s1.mod["mod1"].n_vars == input_h5mu.mod["mod1"].n_vars, "number of variables of split file s1 modalitty mod1 should equal input file"
+    assert s1.mod["mod2"].n_vars == input_h5mu.mod["mod1"].n_vars,  "number of variables of split file s1 modalitty mod2 should equal input file"
+    
+    assert s2.mod["mod1"].n_vars == input_h5mu.mod["mod1"].n_vars, "number of variables of split file s2 modalitty mod1 should equal input file"
+    assert s2.mod["mod2"].n_vars == input_h5mu.mod["mod1"].n_vars,  "number of variables of split file s2 modalitty mod2 should equal input file"
+    
+    # check correct sample splitting
+    assert np.all(s1.mod["mod1"].obs["Obs"] == "A")
+    assert np.all(s1.mod["mod1"].obs["Obs"] == "B")
+
+    # Check contents of csv file
+    expected_csv_output = dedent(
+        f"""\
+        name,filename
+        A,{s1_file.name}
+        B,{s2_file.name}
+        """
+    )
+    with open(output_files, 'r') as open_csv_file:
+        result = open_csv_file.read()
+        assert result == expected_csv_output
+
+
+if __name__ == "__main__":
+    sys.exit(pytest.main([__file__]))
