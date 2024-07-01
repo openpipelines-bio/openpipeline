7d59d8a73c81575adbbe8682f8fc534f1aaa844e
dorien-er <roosen.dorien@gmail.com>
2024-07-01 12:14:56 +0200

    add test for dropping nan obs

--- src/dataflow/split_samples/test.py
+++ src/dataflow/split_samples/test.py
@@ -19,7 +19,7 @@ meta = {
 @pytest.fixture
 def input_modality_1():
     df = pd.DataFrame([[1, 2, 3], [4, 5, 6]], index=["obs1", "obs2"], columns=["var1", "var2", "var3"])
-    obs = pd.DataFrame([["A"], ["B"]], index=df.index, columns=["Obs"])
+    obs = pd.DataFrame({'Obs': ["A", "B"], "Obs_nan": [np.nan, np.nan]}, index=df.index)
     var = pd.DataFrame([["a"], ["b"], ["c"]], index=df.columns, columns=["Feat"])
     ad1 = ad.AnnData(df, obs=obs, var=var)
     return ad1
@@ -27,7 +27,7 @@ def input_modality_1():
 
 @pytest.fixture
 def input_modality_2():
-    df = pd.DataFrame([[1, 2, 3], [np.nan, 5, 6]], index=["obs1", "obs2"], columns=["var1", "var2", "var3"])
+    df = pd.DataFrame([[1, 2, 3], [4, 5, 6]], index=["obs1", "obs2"], columns=["var1", "var2", "var3"])
     var2 = pd.DataFrame(["d", "e", "g"], index=df.columns, columns=["Feat"])
     obs2 = pd.DataFrame(["C", "D"], index=df.index, columns=["Obs"])
     ad2 = ad.AnnData(df, obs=obs2, var=var2)
@@ -43,7 +43,6 @@ def input_h5mu(input_modality_1, input_modality_2):
 @pytest.fixture
 def input_h5mu_path(write_mudata_to_file, input_h5mu):
     return write_mudata_to_file(input_h5mu)
-
 
 
 def test_sample_split(run_component, random_path, input_h5mu, input_h5mu_path):
@@ -74,11 +73,14 @@ def test_sample_split(run_component, random_path, input_h5mu, input_h5mu_path):
     assert s1.n_mod == 2
     assert s2.n_mod == 2
 
-    assert s1.n_obs == input_h5mu.n_obs, "number of modalities of split file does not match input file"
-    assert s2.n_obs == input_h5mu.n_obs, "number of modalities of split file does not match input file"
+    assert s1.n_obs == input_h5mu.n_obs, "number of observations of split file does not match input file"
+    assert s2.n_obs == input_h5mu.n_obs, "number of observations of split file does not match input file"
 
     assert s1.mod["mod1"].n_obs == 1, "number of observations of split file s1 modality mod1 should equal 1"
     assert s1.mod["mod2"].n_obs == input_h5mu.n_obs, "number of observations of split file s1 modality mod2 should equal input file"
+    
+    assert len(s1.mod["mod1"].obs.keys()) == 2, "number of observation keys split file s1 modality mod1 should equal 2"
+    assert len(s1.mod["mod2"].obs.keys()) == 1, "number of observation keys split file s1 modality mod2 should equal 1"
 
     assert s2.mod["mod1"].n_obs == 1, "number of observations of split file s2 modality mod1 should equal 1"
     assert s2.mod["mod2"].n_obs == input_h5mu.n_obs, "number of observations of split file s2 modality mod2 should equal input file"
@@ -108,6 +110,49 @@ def test_sample_split(run_component, random_path, input_h5mu, input_h5mu_path):
         result = open_csv_file.read()
         assert result == expected_csv_output
 
+def test_sample_split_dropna(run_component, random_path, input_h5mu, input_h5mu_path):
+    output_dir = random_path()
+    output_files = random_path(extension="csv")
+    args = [
+        "--input", input_h5mu_path,
+        "--output", str(output_dir),
+        "--modality", "mod1",
+        "--obs_feature", "Obs",
+        "--drop_obs_nan", "true",
+        "--output_files", str(output_files),
+    ]
+
+    run_component(args)
+
+    # check output dir and file names
+    s1_file = output_dir / f"{input_h5mu_path.stem}_A.h5mu"
+    s2_file = output_dir / f"{input_h5mu_path.stem}_B.h5mu"
+
+    # check that .obs columns with only nan values are dropped correctly
+    s1 = mu.read_h5mu(s1_file)
+    s2 = mu.read_h5mu(s2_file)
+
+    assert s1.n_obs == input_h5mu.n_obs, "number of observations of split file does not match input file"
+    assert s2.n_obs == input_h5mu.n_obs, "number of observations of split file does not match input file"
+
+    assert s1.mod["mod1"].n_obs == 1, "number of observations of split file s1 modality mod1 should equal 1"
+    assert s1.mod["mod2"].n_obs == input_h5mu.n_obs, "number of observations of split file s1 modality mod2 should equal input file"
+    
+    assert len(s1.mod["mod1"].obs.keys()) == 1, "number of observation keys split file s1 modality mod1 should equal 1"
+    assert len(s1.mod["mod2"].obs.keys()) == 1, "number of observation keys split file s1 modality mod2 should equal 1"
+
 
 if __name__ == "__main__":
     sys.exit(pytest.main([__file__]))
+    # df = pd.DataFrame([[1, 2, 3], [4, 5, 6]], index=["obs1", "obs2"], columns=["var1", "var2", "var3"])
+    # obs = pd.DataFrame([["A"], ["B"]], index=df.index, columns=["Obs"])
+    # var = pd.DataFrame([["a"], ["b"], ["c"]], index=df.columns, columns=["Feat"])
+    # ad1 = ad.AnnData(df, obs=obs, var=var)
+    
+    # df = pd.DataFrame([[1, 2, 3], [np.nan, np.nan, np.nan]], index=["obs1", "obs2"], columns=["var1", "var2", "var3"])
+    # var2 = pd.DataFrame(["d", "e", "g"], index=df.columns, columns=["Feat"])
+    # obs2 = pd.DataFrame(["C", "D"], index=df.index, columns=["Obs"])
+    # ad2 = ad.AnnData(df, obs=obs2, var=var2)
+
+    # tmp_mudata = mu.MuData({'mod1': ad1, 'mod2': ad2})    
+    # tmp_mudata.write_h5mu("test.h5mu")
