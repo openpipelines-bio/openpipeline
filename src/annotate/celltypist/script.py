466c2aeaf15fc3d6dbcd8df7e2d2740447e75f2a
jakubmajercik <jakub.majercik@gmail.com>
2024-07-02 16:44:45 +0200

    extended tests, added celltypist model to test resources

--- src/annotate/celltypist/script.py
+++ src/annotate/celltypist/script.py
@@ -16,18 +16,14 @@ par = {
     "reference": "resources_test/annotation_test_data/tmp_TS_Blood_filtered.h5ad",
     "model": None,
     "model_save_path": "new_model.pkl",
+    "only_train": False,
     "reference_obs_label": "cell_ontology_class",
+    "check_expression": False,
     "feature_selection": True,
     "majority_voting": True,
-    "input_layer": None,
     "input_obs_label": None,
-    "input_obs_batch": None,
-    "input_var_subset": None,
-    "reference_layer": None,
     "reference_obs_label": "cell_ontology_class",
-    "reference_obs_batch": "donor_assay",
     "output_compression": "gzip"
-    # "prediction_mode": 
 }
 meta = {}
 ## VIASH END
@@ -44,23 +40,6 @@ def setup_logger():
 
     return logger
 # END TEMPORARY WORKAROUND setup_logger
-
-# Helper functions
-def get_X(adata: ad.AnnData, layer: typing.Optional[str], var_index: typing.Optional[str]):
-    """Fetch the counts data from X or a layer. Subset columns by var_index if so desired."""
-    if var_index:
-        adata = adata[:, var_index]
-    if layer:
-        return adata.layers[layer]
-    else:
-        return adata.X
-def get_obs(adata: ad.AnnData, obs_par_names):
-    """Subset the obs dataframe to just the columns defined by the obs_label and obs_batch."""
-    obs_columns = [par[x] for x in obs_par_names if par[x]]
-    return adata.obs[obs_columns]
-def get_var(adata: ad.AnnData, var_index: list[str]):
-    """Fetch the var dataframe. Subset rows by var_index if so desired."""
-    return adata.var.loc[var_index]
 
 def check_celltypist_format(indata):
     if np.abs(np.expm1(indata[0]).sum()-10000) > 1:
@@ -90,17 +69,6 @@ def main(par):
         logger.info("  intersect n_vars: %i", len(common_ens_ids))
         assert len(common_ens_ids) >= 100, "The intersection of genes is too small."
         
-        input_modality = ad.AnnData(
-            X = get_X(input_modality, par["input_layer"], common_ens_ids),
-            obs = get_obs(input_modality, ["input_obs_label", "input_obs_batch"]),
-            var = get_var(input_modality, common_ens_ids)
-        )
-        reference_adata = ad.AnnData(
-            X = get_X(reference_adata, par["reference_layer"], common_ens_ids),
-            obs = get_obs(reference_adata, ["reference_obs_label", "reference_obs_batch"]),
-            var = get_var(reference_adata, common_ens_ids)
-        )
-        
         if not check_celltypist_format(input_modality.X):
             logger.warning("Input data is not in the reccommended format for CellTypist.")
         if not check_celltypist_format(reference_adata.X):
@@ -109,16 +77,22 @@ def main(par):
         model = celltypist.train(reference_adata,
                                  labels = par["reference_obs_label"],
                                  feature_selection=par["feature_selection"],
-                                 check_expression=False)
+                                 check_expression=par["check_expression"])
+        
+        model.write(par["model_save_path"]) if par["model_save_path"] else None
+        if par["only_train"]:
+            return
         
     else:
         raise ValueError("Either 'model' or 'reference' has to be provided.")
     
-    predictions = celltypist.annotate(input_modality, model, majority_voting=par["majority_voting"])
+    predictions = celltypist.annotate(input_modality,
+                                      model,
+                                      majority_voting=par["majority_voting"])
+    
     preds_adata = predictions.to_adata()
     input_mudata.mod[par["modality"]] = preds_adata
         
-    model.write(par["model_save_path"]) if par["model_save_path"] else None
     input_mudata.write_h5mu(par["output"], compression=par["output_compression"])
     
 if __name__ == '__main__':
--- src/annotate/celltypist/test.py
+++ src/annotate/celltypist/test.py
@@ -1,8 +1,12 @@
 import sys
 import os
 import pytest
+import subprocess
+import re
 import mudata as mu
-
+import scanpy as sc
+import anndata as ad
+from openpipelinetestutils.asserters import assert_annotation_objects_equal
 ## VIASH START
 meta = {
     "resources_dir": "resources_test"
@@ -11,18 +15,126 @@ meta = {
 
 input_file = f"{meta['resources_dir']}/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5mu"
 reference_file = f"{meta['resources_dir']}/annotation_test_data/TS_Blood_filtered.h5ad"
+model_file = f"{meta['resources_dir']}/annotation_test_data/celltypist_model_blood_filtered.pkl"
 
-def test_simple_execution(run_component, random_h5mu_path):
-    output_file = "output.h5mu"
+@pytest.fixture
+def normalize_log_transform(random_h5mu_path):
+    def wrapper(input_mudata_file, modality, target_sum=1e4):
+        input_mudata = mu.read_h5mu(input_mudata_file)
+        input_adata = input_mudata.mod[modality]
+        adata = input_adata.copy()
+        input_layer = adata.X
+        data_for_scanpy = ad.AnnData(X=input_layer.copy())
+        sc.pp.normalize_total(data_for_scanpy, target_sum=target_sum)
+        sc.pp.log1p(data_for_scanpy,
+                    base=None,
+                    layer=None, # use X
+                    copy=False) # allow overwrites in the copy that was made
+        adata.X = data_for_scanpy.X
+        adata.uns['log1p'] = data_for_scanpy.uns['log1p'].copy()
+        input_mudata.mod[modality] = adata
+        transformed_input_mudata_file = random_h5mu_path()
+        input_mudata.write_h5mu(transformed_input_mudata_file)
+        return transformed_input_mudata_file
+    return wrapper
+
+def test_simple_execution(run_component, random_h5mu_path, normalize_log_transform):
+    output_file = random_h5mu_path()
+    input_file_transformed = normalize_log_transform(input_file, "rna")
 
     run_component([
-        "--input", input_file,
+        "--input", input_file_transformed,
         "--reference", reference_file,
-        "--output", random_h5mu_path()
+        "--output", output_file
     ])
     
     assert os.path.exists(output_file), "Output file does not exist"
+    
+    input_mudata = mu.read_h5mu(input_file_transformed)
+    output_mudata = mu.read_h5mu(output_file)
+    
+    assert_annotation_objects_equal(input_mudata.mod["prot"],
+                                    output_mudata.mod["prot"])
+    
+    assert list(output_mudata.mod["rna"].obs.keys()) == ['predicted_labels',
+                                                         'over_clustering',
+                                                         'majority_voting',
+                                                         'conf_score']
 
+def test_with_model(run_component, random_h5mu_path, normalize_log_transform):
+    output_file = random_h5mu_path()
+    input_file_transformed = normalize_log_transform(input_file, "rna")
+
+    run_component([
+        "--input", input_file_transformed,
+        "--model", model_file,
+        "--output", output_file
+    ])
+    
+    assert os.path.exists(output_file), "Output file does not exist"
+    
+    input_mudata = mu.read_h5mu(input_file_transformed)
+    output_mudata = mu.read_h5mu(output_file)
+    
+    assert_annotation_objects_equal(input_mudata.mod["prot"],
+                                    output_mudata.mod["prot"])
+    
+    assert list(output_mudata.mod["rna"].obs.keys()) == ['predicted_labels',
+                                                         'over_clustering',
+                                                         'majority_voting',
+                                                         'conf_score']
+
+def test_more_params_save_model(run_component, random_h5mu_path, tmp_path, normalize_log_transform):
+    output_file = random_h5mu_path()
+    input_file_transformed = normalize_log_transform(input_file, "rna")
+    model_save_path = tmp_path / "new_model.pkl"
+
+    run_component([
+        "--input", input_file_transformed,
+        "--reference", reference_file,
+        "--modality", "rna",
+        "--reference_obs_label", "cell_ontology_class",
+        "--output", output_file,
+        "--majority_voting", "False",
+        "--model_save_path", model_save_path
+    ])
+    
+    assert os.path.exists(output_file), "Output file does not exist"
+    assert os.path.exists(model_save_path), "Model file does not exist"
+    
+    input_mudata = mu.read_h5mu(input_file_transformed)
+    output_mudata = mu.read_h5mu(output_file)
+    
+    assert_annotation_objects_equal(input_mudata.mod["prot"],
+                                    output_mudata.mod["prot"])
+    
+    assert list(output_mudata.mod["rna"].obs.keys()) == ['predicted_labels',
+                                                         'conf_score']
+
+def test_fail_check_reference_expression(run_component, random_h5mu_path, normalize_log_transform):
+    output_file = random_h5mu_path()
+
+    with pytest.raises(subprocess.CalledProcessError) as err:
+        run_component([
+            "--input", input_file,
+            "--reference", reference_file,
+            "--output", output_file,
+            "--check_expression", "True"
+        ])
+    assert re.search(r"ValueError: 🛑 Invalid expression matrix, expect log1p normalized expression to 10000 counts per cell",
+            err.value.stdout.decode('utf-8'))
+    
+def test_fail_invalid_input_expression(run_component, random_h5mu_path):
+    output_file = random_h5mu_path()
+
+    with pytest.raises(subprocess.CalledProcessError) as err:
+        run_component([
+            "--input", input_file,
+            "--reference", reference_file,
+            "--output", output_file
+        ])
+    assert re.search(r"ValueError: 🛑 Invalid expression matrix in `.X`, expect log1p normalized expression to 10000 counts per cell",
+            err.value.stdout.decode('utf-8'))
 
 if __name__ == '__main__':
     sys.exit(pytest.main([__file__]))
