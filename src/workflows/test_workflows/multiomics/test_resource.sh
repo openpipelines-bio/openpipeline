b247baa57a484234378450eeace768af798406f3
DriesSchaumont <5946712+DriesSchaumont@users.noreply.github.com>
2024-07-09 08:25:50 +0000

    Remove test script

--- src/workflows/test_workflows/multiomics/test_resource.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-viash run src/dataflow/split_modalities/config.vsh.yaml -- \
-  --input resources_test/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5mu \
-  --output output_test/split_modalities/h5mu/ \
-  --output_types output_test/split_modalities/foo_types.csv \
-  --compression gzip
