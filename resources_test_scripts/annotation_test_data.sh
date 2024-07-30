b2134fcf292e464a8d2094e14f2342e94bb66843
jakubmajercik <jakub.majercik@gmail.com>
2024-07-30 15:00:23 +0200

    unit tests added + custom model support added + test resources updated

--- resources_test_scripts/annotation_test_data.sh
+++ resources_test_scripts/annotation_test_data.sh
@@ -16,41 +16,49 @@ DIR=$(dirname "$OUT")
 [ -d "$DIR" ] || mkdir -p "$DIR"
 
 # Download Tabula Sapiens Blood reference h5ad from https://doi.org/10.5281/zenodo.7587774
-wget "https://zenodo.org/record/7587774/files/TS_Blood_filtered.h5ad?download=1" -O "${OUT}/tmp_TS_Blood_filtered.h5ad"
+# wget "https://zenodo.org/record/7587774/files/TS_Blood_filtered.h5ad?download=1" -O "${OUT}/tmp_TS_Blood_filtered.h5ad"
 
-# Download Tabula Sapiens Blood pretrained model from https://doi.org/10.5281/zenodo.7580707
-wget "https://zenodo.org/record/7580707/files/pretrained_models_Blood_ts.tar.gz?download=1" -O "${OUT}/tmp_pretrained_models_Blood_ts.tar.gz"
+# # Download Tabula Sapiens Blood pretrained model from https://doi.org/10.5281/zenodo.7580707
+# wget "https://zenodo.org/record/7580707/files/pretrained_models_Blood_ts.tar.gz?download=1" -O "${OUT}/tmp_pretrained_models_Blood_ts.tar.gz"
 
-# # Download PopV specific CL ontology files - now needed for OnClass
+# # # Download PopV specific CL ontology files - now needed for OnClass
+# # OUT_ONTOLOGY="${OUT}/ontology"
+# # [ -d "$OUT_ONTOLOGY" ] || mkdir -p "$OUT_ONTOLOGY"
+# # wget https://raw.githubusercontent.com/czbiohub/PopV/main/ontology/cl.obo \
+# # -O "${OUT_ONTOLOGY}/cl.obo"
+# # wget https://raw.githubusercontent.com/czbiohub/PopV/main/ontology/cl.ontology \
+# # -O "${OUT_ONTOLOGY}/cl.ontology"
+# # wget https://raw.githubusercontent.com/czbiohub/PopV/main/ontology/cl.ontology.nlp.emb \
+# # -O "${OUT_ONTOLOGY}/cl.ontology.nlp.emb"
+
+
+# # Process Tabula Sapiens Blood reference h5ad
+# # (Select one individual and 100 cells per cell type)
+# python <<HEREDOC
+# import anndata as ad
+# ref_adata = ad.read_h5ad("${OUT}/tmp_TS_Blood_filtered.h5ad")
+# sub_ref_adata = ref_adata[ref_adata.obs["donor_assay"] == "TSP14_10x 3' v3"] 
+# n=100
+# s=sub_ref_adata.obs.groupby('cell_ontology_class').cell_ontology_class.transform('count')
+# sub_ref_adata_final = sub_ref_adata[sub_ref_adata.obs[s>=n].groupby('cell_ontology_class').head(n).index]
+# # assert sub_ref_adata_final.shape == (500, 58870)
+# sub_ref_adata_final.write("${OUT}/TS_Blood_filtered.h5ad", compression='gzip')
+# HEREDOC
+
+# # rm "${OUT}/tmp_blood_test_reference.h5ad"
+
+# echo "Fetching OnClass data and models"
+
 # OUT_ONTOLOGY="${OUT}/ontology"
 # [ -d "$OUT_ONTOLOGY" ] || mkdir -p "$OUT_ONTOLOGY"
-# wget https://raw.githubusercontent.com/czbiohub/PopV/main/ontology/cl.obo \
-# -O "${OUT_ONTOLOGY}/cl.obo"
-# wget https://raw.githubusercontent.com/czbiohub/PopV/main/ontology/cl.ontology \
-# -O "${OUT_ONTOLOGY}/cl.ontology"
-# wget https://raw.githubusercontent.com/czbiohub/PopV/main/ontology/cl.ontology.nlp.emb \
-# -O "${OUT_ONTOLOGY}/cl.ontology.nlp.emb"
+# wget https://figshare.com/ndownloader/files/28394466 -O "${OUT_ONTOLOGY}/OnClass_data_public_minimal.tar.gz"
+# tar -xzvf "${OUT_ONTOLOGY}/OnClass_data_public_minimal.tar.gz" -C "${OUT_ONTOLOGY}" --strip-components=2
+# rm "${OUT_ONTOLOGY}/allen.ontology"
+# rm "${OUT_ONTOLOGY}/OnClass_data_public_minimal.tar.gz"
 
+wget https://figshare.com/ndownloader/files/28394541 -O "${OUT}/OnClass_models.tar.gz"
+tar -xzvf "${OUT}/OnClass_models.tar.gz" -C "${OUT}" --strip-components=1
+rm "${OUT}/OnClass_models.tar.gz"
 
-# Process Tabula Sapiens Blood reference h5ad
-# (Select one individual and 100 cells per cell type)
-python <<HEREDOC
-import anndata as ad
-ref_adata = ad.read_h5ad("${OUT}/tmp_TS_Blood_filtered.h5ad")
-sub_ref_adata = ref_adata[ref_adata.obs["donor_assay"] == "TSP14_10x 3' v3"] 
-n=100
-s=sub_ref_adata.obs.groupby('cell_ontology_class').cell_ontology_class.transform('count')
-sub_ref_adata_final = sub_ref_adata[sub_ref_adata.obs[s>=n].groupby('cell_ontology_class').head(n).index]
-# assert sub_ref_adata_final.shape == (500, 58870)
-sub_ref_adata_final.write("${OUT}/TS_Blood_filtered.h5ad", compression='gzip')
-HEREDOC
-
-rm "${OUT}/tmp_blood_test_reference.h5ad"
-
-echo "Fetching OnClass data"
-OUT_ONTOLOGY="${OUT}/ontology"
-[ -d "$OUT_ONTOLOGY" ] || mkdir -p "$OUT_ONTOLOGY"
-wget https://figshare.com/ndownloader/files/28394466 -O "${OUT_ONTOLOGY}/OnClass_data_public_minimal.tar.gz"
-tar -xzvf "${OUT_ONTOLOGY}/OnClass_data_public_minimal.tar.gz" -C "${OUT_ONTOLOGY}" --strip-components=2
-rm "${OUT_ONTOLOGY}/allen.ontology"
-rm "${OUT_ONTOLOGY}/OnClass_data_public_minimal.tar.gz"
+find "${OUT}/Pretrained_model" ! -name "example_file_model*" -type f -exec rm -f {} +
+mv "${OUT}/Pretrained_model" "${OUT}/onclass_model"
--- src/annotate/onclass/script.py
+++ src/annotate/onclass/script.py
@@ -7,25 +7,28 @@ import numpy as np
 import os
 from OnClass.OnClassModel import OnClassModel
 import obonet
+from typing import Dict, Tuple
+from tqdm import tqdm
+
 
 ## VIASH START
 par = {
     "input": "resources_test/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5mu",
     "output": "output.h5mu",
     "modality": "rna",
-    "reference": "resources_test/annotation_test_data/tmp_TS_Blood_filtered.h5mu",
+    "reference": "resources_test/annotation_test_data/TS_Blood_filtered.h5mu",
     "model": None,
-    "reference_obs_targets": "cell_ontology_class",
+    "reference_obs_targets": ["cell_ontology_class"],
     "input_layer": None,
     "reference_layer": None,
     "max_iter": 100,
     "output_obs_predictions": None,
     "output_obs_probability": None,
-    "cl_nlp_emb_file": "cl.ontology.nlp.emb",
-    "cl_ontology_file": "cl.ontology",
+    "cl_nlp_emb_file": "resources_test/annotation_test_data/ontology/cl.ontology.nlp.emb",
+    "cl_ontology_file": "resources_test/annotation_test_data/ontology/cl.ontology",
     "output_compression": "gzip"
 }
-meta = {}
+meta = {"resources_dir": "src/annotate/onclass"}
 ## VIASH END
 
 sys.path.append(meta["resources_dir"])
@@ -43,12 +46,58 @@ def setup_logger():
 
 logger = setup_logger()
 
-def map_celltype_to_ontology_id(cl_obo_file):
+def map_celltype_to_ontology_id(cl_obo_file: str) -> Tuple[Dict[str, str], Dict[str, str]]:
+    """
+    Map cell type names to ontology IDs and vice versa.
+    
+    Parameters
+    ----------
+    cl_obo_file : str
+        Path to the cell ontology file.
+        
+    Returns
+    -------
+    Tuple[Dict[str, str], Dict[str, str]]
+        A tuple of two dictionaries. The first dictionary maps cell ontology IDs to cell type names.
+        The second dictionary maps cell type names to cell ontology IDs.
+    """
     graph = obonet.read_obo(cl_obo_file)
     cl_id_to_name = {id_: data.get("name") for id_, data in graph.nodes(data=True)}
-    return cl_id_to_name
+    name_to_cl_id = {data.get("name"): id_ for id_, data in graph.nodes(data=True)}
+    return cl_id_to_name, name_to_cl_id
 
-def predict_input_data(model, input_matrix, input_modality, id_to_name, reference_obs_target, obs_prediction, obs_probability):
+def predict_input_data(model: OnClassModel,
+                       input_matrix: np.array,
+                       input_modality: ad.AnnData,
+                       id_to_name: dict,
+                       reference_obs_target: str,
+                       obs_prediction: str,
+                       obs_probability: str) -> ad.AnnData:
+    """
+    Predict cell types for input data and save results to Anndata obj.
+    
+    Parameters
+    ----------
+    model : OnClassModel
+        The OnClass model.
+    input_matrix : np.array
+        The input data matrix.
+    input_modality : ad.AnnData
+        The input data Anndata object.
+    id_to_name : dict
+        Dictionary mapping cell ontology IDs to cell type names.
+    reference_obs_target : str
+        The reference observation target.
+    obs_prediction : str
+        The obs key for the predicted cell type.
+    obs_probability : str
+        The obs key for the predicted cell type probability.
+        
+    Returns
+    -------
+    ad.AnnData
+        The input data Anndata object with the predicted cell types saved in obs.
+    """
     corr_test_feature = model.ProcessTestFeature(
         test_feature=input_matrix,
         test_genes=input_modality.var_names,
@@ -63,24 +112,23 @@ def predict_input_data(model, input_matrix, input_modality, id_to_name, referenc
     return input_modality
 
 def main():
+    logger.info("Reading input data")
     input_mudata = mu.read_h5mu(par["input"])
     input_modality = input_mudata.mod[par["modality"]].copy()
-    
 
-    id_to_name = map_celltype_to_ontology_id(os.path.join(meta["resources_dir"], "cl.obo"))
+    id_to_name, name_to_id = map_celltype_to_ontology_id(os.path.join(meta["resources_dir"], "cl.obo"))
     obs_predictions = par["output_obs_predictions"] if par["output_obs_predictions"] else ["pred"] * len(par["reference_obs_targets"])
     obs_probabilities = par["output_obs_probability"] if par["output_obs_probability"] else ["prob"] * len(par["reference_obs_targets"])
     
     if par["input_layer"]:
-        input_matrix = input_modality.layers[par["input_layer"]]
+        input_matrix = input_modality.layers[par["input_layer"]].toarray()
     else:
-        input_matrix = input_modality.X
+        input_matrix = input_modality.X.toarray()
         
     if par["reference"]:
-        
+        logger.info("Reading reference data")
         model = OnClassModel(cell_type_nlp_emb_file=par["cl_nlp_emb_file"],
-                            cell_type_network_file=par["cl_ontology_file"])
-        _ = model.EmbedCellTypes()
+                             cell_type_network_file=par["cl_ontology_file"])
         
         reference_mudata = mu.read_h5mu(par["reference"])
         reference_modality = reference_mudata.mod[par["modality"]].copy()
@@ -97,32 +145,37 @@ def main():
         assert len(common_ens_ids) >= 100, "The intersection of genes is too small."
 
         if par["reference_layer"]:
-            reference_matrix = reference_modality.layers[par["reference_layer"]]
+            reference_matrix = reference_modality.layers[par["reference_layer"]].toarray()
         else:
-            reference_matrix = reference_modality.X
+            reference_matrix = reference_modality.X.toarray()
 
-        (
-            corr_train_feature,
-            _,
-            corr_train_genes,
-            _,
-        ) = model.ProcessTrainFeature(
-            train_feature=reference_matrix,
-            train_label=reference_modality.obs[reference_obs_target],
-            train_genes=reference_modality.var_names,
-            test_feature=input_matrix,
-            test_genes=input_modality.var_names,
-            log_transform=False,
-        )
+        logger.info("Training models...")
+        for reference_obs_target, obs_prediction, obs_probability in tqdm(zip(par["reference_obs_targets"], obs_predictions, obs_probabilities)):
             
-        for reference_obs_target, obs_prediction, obs_probability in zip(par["reference_obs_targets"], obs_predictions, obs_probabilities):
-
+            logger.info(f"Training model for {reference_obs_target}")
+            labels = reference_modality.obs[reference_obs_target].tolist()
+            labels_cl = [name_to_id[label] for label in labels]
+            _ = model.EmbedCellTypes(labels_cl)
+            (
+                corr_train_feature,
+                _,
+                corr_train_genes,
+                _,
+            ) = model.ProcessTrainFeature(
+                train_feature=reference_matrix,
+                train_label=labels_cl,
+                train_genes=reference_modality.var_names,
+                test_feature=input_matrix,
+                test_genes=input_modality.var_names,
+                log_transform=False,
+            )
             model.BuildModel(ngene=len(corr_train_genes))
             model.Train(corr_train_feature,
-                        reference_modality.obs[reference_obs_target],
+                        labels_cl,
                         max_iter=par["max_iter"],
                         save_model=f"model_{reference_obs_target}")
             
+            logger.info(f"Predicting cell types for {reference_obs_target}")
             input_modality = predict_input_data(model,
                                                 input_matrix,
                                                 input_modality,
@@ -132,11 +185,15 @@ def main():
                                                 obs_probability)
 
     elif par["model"]:
+        logger.info("Predicting cell types using pre-trained models")
         for model_path, reference_obs_target, obs_prediction, obs_probability in zip(par["model"], par["reference_obs_targets"], obs_predictions, obs_probabilities):
-            model = OnClassModel(cell_type_nlp_emb_file=os.path.join(meta["resources_dir"], "cl.ontology.nlp.emb"),
-                                cell_type_network_file=os.path.join(meta["resources_dir"], "cl.ontology"))
-            model.BuildModel(use_pretrain=model_path)
+            logger.info(f"Loading model for {reference_obs_target}")
+            model = OnClassModel(cell_type_nlp_emb_file=par["cl_nlp_emb_file"],
+                                cell_type_network_file=par["cl_ontology_file"])
+            
+            model.BuildModel(use_pretrain=model_path, ngene=None)
         
+            logger.info(f"Predicting cell types for {reference_obs_target}")
             input_modality = predict_input_data(model,
                                                 input_matrix,
                                                 input_modality,
@@ -144,7 +201,11 @@ def main():
                                                 reference_obs_target,
                                                 obs_prediction,
                                                 obs_probability)
+            
+    else:
+        raise ValueError("Either reference or model must be provided")
     
+    logger.info("Writing output data")
     input_mudata.mod[par["modality"]] = input_modality
     input_mudata.write_h5mu(par["output"], compression=par["output_compression"])
     
