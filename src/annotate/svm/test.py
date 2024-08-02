42680d1266ecf3861e7a4fbbde18e899dbad88d3
jakubmajercik <jakub.majercik@gmail.com>
2024-08-02 10:29:39 +0200

    test with model input added

--- src/annotate/svm/test.py
+++ src/annotate/svm/test.py
@@ -6,6 +6,10 @@ import re
 import mudata as mu
 from openpipelinetestutils.asserters import assert_annotation_objects_equal
 import os
+from sklearn import svm
+from sklearn.calibration import CalibratedClassifierCV
+import pickle
+
 ## VIASH START
 meta = {
     "resources_dir": "resources_test"
@@ -24,7 +28,6 @@ def subset_genes(random_h5mu_path):
         reference_mudata = mu.read_h5mu(reference_mudata_file)
         reference_adata = reference_mudata.mod[modality]
         
-
         reference_mudata.var["gene_symbol"] = list(reference_mudata.var.index)
         reference_mudata.var.index = [re.sub("\\.[0-9]+$", "", s) for s in reference_mudata.var["ensemblid"]]
         reference_adata.var["gene_symbol"] = list(reference_adata.var.index)
@@ -44,6 +47,24 @@ def subset_genes(random_h5mu_path):
         reference_mudata.write_h5mu(subset_reference_mudata_file)
         return subset_input_mudata_file, subset_reference_mudata_file
     return wrapper
+
+@pytest.fixture
+def dummy_model(tmp_path, subset_genes):
+    _, subset_reference_file = subset_genes(input_file, reference_file, "rna")
+    reference_modality = mu.read_h5mu(subset_reference_file).mod["rna"]
+    
+    labels = reference_modality.obs["cell_ontology_class"].to_numpy()
+    model = CalibratedClassifierCV(svm.LinearSVC(
+        max_iter=10,
+        dual="auto",
+    ))
+    model.fit(reference_modality.X, labels)
+    
+    model_path = tmp_path / "model.pkl"
+    with open(model_path, "wb") as f:
+        pickle.dump(model, f)
+        
+    return model_path
 
 def test_simple_execution(run_component, random_h5mu_path, subset_genes):
     subset_input_file, subset_reference_file = subset_genes(input_file, reference_file, "rna")
@@ -68,7 +89,7 @@ def test_simple_execution(run_component, random_h5mu_path, subset_genes):
                                                          'svm_probability']
 
     obs_values = output_mudata.mod["rna"].obs["svm_probability"]
-    assert all(0 <= value <= 1 for value in obs_values), ".obs at cell_ontology_class_prob has values outside the range [0, 1]"
+    assert all(0 <= value <= 1 for value in obs_values), "probabilities outside the range [0, 1]"
 
 
 def test_custom_out_obs_model_params(run_component, random_h5mu_path, subset_genes):
@@ -98,8 +119,32 @@ def test_custom_out_obs_model_params(run_component, random_h5mu_path, subset_gen
                                                          'dummy_probability']
 
     obs_values = output_mudata.mod["rna"].obs["dummy_probability"]
-    assert all(0 <= value <= 1 for value in obs_values), ".obs at cell_ontology_class_prob has values outside the range [0, 1]"
+    assert all(0 <= value <= 1 for value in obs_values), "probabilities outside the range [0, 1]"
 
+
+def test_with_model(run_component, random_h5mu_path, dummy_model, subset_genes):
+    subset_input_file, _ = subset_genes(input_file, reference_file, "rna")
+    output_file = random_h5mu_path()
+
+    run_component([
+        "--input", subset_input_file,
+        "--model", dummy_model,
+        "--output", output_file
+    ])
+
+    assert os.path.exists(output_file), "Output file does not exist"
+
+    input_mudata = mu.read_h5mu(input_file)
+    output_mudata = mu.read_h5mu(output_file)
+
+    assert_annotation_objects_equal(input_mudata.mod["prot"],
+                                    output_mudata.mod["prot"])
+
+    assert list(output_mudata.mod["rna"].obs.keys()) == ['svm_pred',
+                                                         'svm_probability']
+
+    obs_values = output_mudata.mod["rna"].obs["svm_probability"]
+    assert all(0 <= value <= 1 for value in obs_values), "probabilities outside the range [0, 1]"
 
 def test_no_model_no_reference_error(run_component, random_h5mu_path):
     output_file = random_h5mu_path()
