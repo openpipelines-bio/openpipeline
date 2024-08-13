12d217257e00e1a8d6bffb1525ffdf208d98de45
Vladimir Shitov <vladimir.shitov@helmholtz-muenchen.de>
2024-08-13 17:42:42 +0200

    Change input format to h5mu

--- src/labels_transfer/xgboost/test.py
+++ src/labels_transfer/xgboost/test.py
@@ -12,7 +12,7 @@ meta = {
 }
 ## VIASH END
 
-reference_file = f"{meta['resources_dir']}/annotation_test_data/TS_Blood_filtered.h5ad"
+reference_h5ad_file = f"{meta['resources_dir']}/annotation_test_data/TS_Blood_filtered.h5ad"
 input_file = f"{meta['resources_dir']}/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5mu"
 
 
@@ -20,11 +20,11 @@ input_file = f"{meta['resources_dir']}/pbmc_1k_protein_v3/pbmc_1k_protein_v3_fil
 def test_args(tmp_path, request):
     obsm_features, obs_targets, output_uns_parameters = request.param
 
-    tempfile_reference_file = tmp_path / "reference.h5ad"
+    tempfile_reference_file = tmp_path / "reference.h5mu"
     tempfile_input_file = tmp_path / "input.h5mu"
 
     # read reference
-    reference_adata = anndata.read_h5ad(reference_file)
+    reference_adata = anndata.read_h5ad(reference_h5ad_file)
 
     # generate reference obs targets
     for i, target in enumerate(obs_targets):
@@ -39,8 +39,9 @@ def test_args(tmp_path, request):
     reference_adata.obsm[obsm_features] = np.random.normal(size=(reference_adata.n_obs, 30))
     input_rna_adata.obsm[obsm_features] = np.random.normal(size=(input_rna_adata.n_obs, 30))
 
+    reference_mdata = mudata.MuData({"rna": reference_adata})
     # write files
-    reference_adata.write_h5ad(str(tempfile_reference_file))
+    reference_mdata.write_h5mu(str(tempfile_reference_file))
     input_mudata.write_h5mu(str(tempfile_input_file))
 
     return tempfile_reference_file, reference_adata, tempfile_input_file, input_rna_adata, obsm_features, obs_targets, output_uns_parameters
