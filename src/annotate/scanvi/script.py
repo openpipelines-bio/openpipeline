dc33774f736276caf20b1c18ad4d2d59549180a8
jakubmajercik <jakub.majercik@gmail.com>
2024-08-26 11:27:16 +0200

    updated training params, fixed unit tests

--- src/annotate/scanvi/script.py
+++ src/annotate/scanvi/script.py
@@ -1,7 +1,7 @@
 import sys
 import mudata as mu
-import anndata as ad
 import scvi
+import numpy as np
 
 ## VIASH START
 par = {
@@ -33,63 +33,70 @@ def setup_logger():
 # END TEMPORARY WORKAROUND setup_logger
 logger = setup_logger()
 
-logger.info("Reading input data and SCVI model")
+logger.info("Reading the input and reference data")
 
 input_data = mu.read_h5mu(par["input"])
 query = input_data.mod[par["modality"]]
-reference = ad.read_h5ad(par["reference"])
+reference_data = mu.read_h5mu(par["reference"])
+reference = reference_data.mod[par["modality"]]
+
+logger.info(f"Loading the pretrained scVI model from {par['scvi_reference_model']}")
 scvi_reference_model = scvi.model.SCVI.load(par["scvi_reference_model"], reference)
 
 logger.info("Setting up scANVI model")
 
 scanvi_ref = scvi.model.SCANVI.from_scvi_model(
     scvi_reference_model,
-    unlabeled_category="Unknown",
+    unlabeled_category=par["unknown_celltype"],
     labels_key=par["reference_obs_label"],
     )
 
-plan_kwargs = {"lr": par["learning_rate"],
-               "reduce_lr_on_plateau": par['reduce_lr_on_plateau'],
-               "lr_patience": par['lr_patience'],
-               "lr_factor": par['lr_factor']
+reference_plan_kwargs = {"lr": par["reference_learning_rate"],
+               "reduce_lr_on_plateau": par['reference_reduce_lr_on_plateau'],
+               "lr_patience": par['reference_lr_patience'],
+               "lr_factor": par['reference_lr_factor']
                }
 
+logger.info("Training scANVI model on reference data with celltype labels")
+
 scanvi_ref.train(
-    train_size=par["train_size"],
-    max_epochs=par['max_epochs'],
-    early_stopping=par['early_stopping'],
-    early_stopping_patience=par['early_stopping_patience'],
-    plan_kwargs=plan_kwargs,
+    train_size=par["reference_train_size"],
+    max_epochs=par['reference_max_epochs'],
+    early_stopping=par['reference_early_stopping'],
+    early_stopping_patience=par['reference_early_stopping_patience'],
+    plan_kwargs=reference_plan_kwargs,
     check_val_every_n_epoch=1,
     accelerator="auto",
 )
 
-SCANVI_LATENT_KEY = "X_scANVI"
-reference.obsm[SCANVI_LATENT_KEY] = scanvi_ref.get_latent_representation()
-
-logger.info("Updating with query data")
-
+logger.info("Updating and training scANVI model with query data")
 scvi.model.SCANVI.prepare_query_anndata(query, scanvi_ref, inplace=True)
 scanvi_query = scvi.model.SCANVI.load_query_data(query, scanvi_ref)
 
+query_plan_kwargs = {"lr": par["query_learning_rate"],
+               "reduce_lr_on_plateau": par['query_reduce_lr_on_plateau'],
+               "lr_patience": par['query_lr_patience'],
+               "lr_factor": par['query_lr_factor']
+               }
 
 scanvi_query.train(
-    train_size=par["train_size"],
-    max_epochs=par['max_epochs'],
-    early_stopping=par['early_stopping'],
-    early_stopping_patience=par['early_stopping_patience'],
-    plan_kwargs=plan_kwargs,
+    train_size=par["query_train_size"],
+    max_epochs=par['query_max_epochs'],
+    early_stopping=par['query_early_stopping'],
+    early_stopping_patience=par['query_early_stopping_patience'],
+    plan_kwargs=query_plan_kwargs,
     check_val_every_n_epoch=1,
     accelerator="auto",
 )
 
-logger.info("Running prediction")
+logger.info("Adding latent representation to query data")
+query.obsm[par["output_obsm_scanvi_embedding"]] = scanvi_query.get_latent_representation()
 
-query.obsm[SCANVI_LATENT_KEY] = scanvi_query.get_latent_representation()
-query.obs[par["input_obs_label"]] = scanvi_query.predict()
+logger.info("Running predictions on query data")
+query.obs[par["output_obs_predictions"]] = scanvi_query.predict(query)
+query.obs[par["output_obs_probability"]] = np.max(scanvi_query.predict(query, soft=True), axis=1)
 
 logger.info("Saving output and model")
-
 input_data.mod[par["modality"]] = query
 input_data.write_h5mu(par["output"], compression=par["output_compression"])
 
--- src/annotate/scanvi/test.py
+++ src/annotate/scanvi/test.py
@@ -14,21 +14,24 @@ meta = {
 ## VIASH END
 
 input_file = f"{meta['resources_dir']}/pbmc_1k_protein_v3/pbmc_1k_protein_v3_mms.h5mu"
-reference_file = f"{meta['resources_dir']}/annotation_test_data/TS_Blood_filtered.h5ad"
+reference_file = f"{meta['resources_dir']}/annotation_test_data/TS_Blood_filtered.h5mu"
 
 @pytest.fixture
 def create_scvi_model(random_path, tmp_path):
     def wrapper(input_file, reference_file):
         input_data = mu.read_h5mu(input_file)
         input_modality = input_data.mod["rna"]
-        reference_data = ad.read_h5ad(reference_file)
+        reference_data = mu.read_h5mu(reference_file)
+        reference_modality = reference_data.mod["rna"]
         
         reference_data.var["gene_symbol"] = list(reference_data.var.index)
         reference_data.var.index = [re.sub("\\.[0-9]+$", "", s) for s in reference_data.var["ensemblid"]]
+        reference_modality.var["gene_symbol"] = list(reference_modality.var.index)
+        reference_modality.var.index = [re.sub("\\.[0-9]+$", "", s) for s in reference_modality.var["ensemblid"]]
 
-        common_ens_ids = list(set(reference_data.var.index).intersection(set(input_modality.var.index)))
+        common_ens_ids = list(set(reference_modality.var.index).intersection(set(input_modality.var.index)))
 
-        reference = reference_data[:, common_ens_ids].copy()
+        reference = reference_modality[:, common_ens_ids].copy()
         query = input_modality[:, common_ens_ids].copy()
 
         scvi.model.SCVI.setup_anndata(reference,
@@ -46,13 +49,14 @@ def create_scvi_model(random_path, tmp_path):
         scvi_model.train(max_epochs=10)
         
         input_data.mod["rna"] = query
+        reference_data.mod["rna"] = reference
         
         input_data_file = random_path(extension="h5mu")
-        reference_file = random_path(extension="h5ad")
+        reference_file = random_path(extension="h5mu")
         scvi_model_file = tmp_path
         
         input_data.write_h5mu(input_data_file)
-        reference.write_h5ad(reference_file)
+        reference_data.write_h5mu(reference_file)
         scvi_model.save(scvi_model_file, overwrite=True)
                 
         return scvi_model_file, input_data_file, reference_file
@@ -66,7 +70,9 @@ def test_simple_execution(run_component, random_h5mu_path, create_scvi_model):
         "--input", input_file_scvi,
         "--reference", reference_file_scvi,
         "--scvi_reference_model", scvi_model_file,
-        "--max_epochs", "10",
+        "--reference_obs_label", "cell_ontology_class",
+        "--reference_max_epochs", "10",
+        "--query_max_epochs", "10",
         "--output", output_file
     ])
 
@@ -77,8 +83,9 @@ def test_simple_execution(run_component, random_h5mu_path, create_scvi_model):
     
     assert input_mudata.mod["rna"].n_obs == output_mudata.mod["rna"].n_obs, f"Number of observations changed"
     assert input_mudata.mod["rna"].n_vars == output_mudata.mod["rna"].n_vars, f"Number of variables changed"
-    assert "X_scANVI" in output_mudata.mod["rna"].obsm, "Latent representation not added"
-    assert "predictions_scanvi" in output_mudata.mod["rna"].obs, "Predictions not added"
+    assert "scanvi_embedding" in output_mudata.mod["rna"].obsm.keys(), "Latent representation not added"
+    assert "scanvi_pred" in output_mudata.mod["rna"].obs.keys(), "Predictions not added"
+    assert "scanvi_probability" in output_mudata.mod["rna"].obs.keys(), "Probabilities not added"
 
     assert_annotation_objects_equal(input_mudata.mod["prot"],
                                     output_mudata.mod["prot"])
@@ -91,16 +98,26 @@ def test_multiple_arguments(run_component, random_h5mu_path, create_scvi_model, 
         "--input", input_file_scvi,
         "--reference", reference_file_scvi,
         "--scvi_reference_model", scvi_model_file,
-        "--max_epochs", "10",
+        "--reference_obs_label", "cell_ontology_class",
         "--output", output_file,
-        "--input_obs_label", "scanvi_predictions",
-        "--reduce_lr_on_plateau", "True",
-        "--lr_patience", "5",
-        "--lr_factor", "0.5",
-        "--train_size", "0.8",
-        "--early_stopping", "True",
-        "--early_stopping_patience", "5",
-        "--early_stopping_min_delta", "0.01",
+        "--reference_max_epochs", "10",
+        "--reference_reduce_lr_on_plateau", "True",
+        "--reference_lr_patience", "5",
+        "--reference_lr_factor", "0.5",
+        "--reference_train_size", "0.8",
+        "--reference_early_stopping", "True",
+        "--reference_early_stopping_patience", "5",
+        "--reference_early_stopping_min_delta", "0.01",
+        "--query_max_epochs", "10",
+        "--query_reduce_lr_on_plateau", "True",
+        "--query_lr_patience", "5",
+        "--query_lr_factor", "0.5",
+        "--query_train_size", "0.8",
+        "--query_early_stopping", "True",
+        "--query_early_stopping_patience", "5",
+        "--query_early_stopping_min_delta", "0.01",
+        "--output_obs_predictions", "scanvi_pred",
+        "--output_obs_probabilities", "scanvi_probabilitity",
         "--output_compression", "gzip",
         "--output_model", tmp_path
     ])
@@ -113,8 +130,10 @@ def test_multiple_arguments(run_component, random_h5mu_path, create_scvi_model, 
     
     assert input_mudata.mod["rna"].n_obs == output_mudata.mod["rna"].n_obs, f"Number of observations changed"
     assert input_mudata.mod["rna"].n_vars == output_mudata.mod["rna"].n_vars, f"Number of variables changed"
-    assert "X_scANVI" in output_mudata.mod["rna"].obsm, "Latent representation not added"
-    assert "scanvi_predictions" in output_mudata.mod["rna"].obs, "Predictions not added"
+    assert "scanvi_embedding" in output_mudata.mod["rna"].obsm.keys(), "Latent representation not added"
+    assert "scanvi_pred" in output_mudata.mod["rna"].obs.keys(), "Predictions not added"
+    assert "scanvi_probability" in output_mudata.mod["rna"].obs.keys(), "Probabilities not added"
+
 
     assert_annotation_objects_equal(input_mudata.mod["prot"],
                                     output_mudata.mod["prot"])
