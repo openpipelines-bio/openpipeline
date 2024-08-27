9a2c2a7af461c263ef03fe94b146c35cca579202
Robrecht Cannoodt <rcannood@gmail.com>
2024-08-27 09:59:41 +0200

    review code

--- src/transform/bpcells_regress_out/script.R
+++ src/transform/bpcells_regress_out/script.R
@@ -1,9 +1,8 @@
 cat("Loading libraries\n")
 library(glue)
 library(BPCells)
-requireNamespace("anndata", quietly = TRUE, convert = FALSE)
+requireNamespace("anndata", quietly = TRUE)
 requireNamespace("reticulate", quietly = TRUE)
-library(reticulate)
 mudata <- reticulate::import("mudata")
 
 ## VIASH START
@@ -19,19 +18,20 @@ mdata <- mudata$read_h5mu(par$input)
 
 # Regress out
 if (!is.null(par$obs_keys) && length(par$obs_keys) > 0) {
-  glue("Regress out variables {par$obs_keys} on modality {par$modality}")
+  cat("Regress out variables ", par$obs_keys, " on modality ", par$modality, "\n", sep = "")
 
   # Fetch modality AnnData and convert to an iterable matrix
   adata <- mdata$mod[[par$modality]]
 
   # Fetch the input layer
-  if (is.null(par$input_layer)) {
-    cat("Using .X as input layer")
-    mat <- adata$X
-  } else {
-    glue("Using .layers {par$input_layer} as input layer")
-    mat <- adata$layers[[par$input_layer]]
-  }
+  mat <-
+    if (is.null(par$input_layer)) {
+      cat("Using .X as input layer\n")
+      adata$X
+    } else {
+      cat("Using .layers ", par$input_layer, " as input layer\n", sep = "")
+      adata$layers[[par$input_layer]]
+    }
 
   imat <- as(as(mat, "CsparseMatrix"), "IterableMatrix")
   dimnames(imat) <- NULL
@@ -42,21 +42,21 @@ if (!is.null(par$obs_keys) && length(par$obs_keys) > 0) {
   # Regress out using BPCells
   regressed_data <- regress_out(imat, latent_data, prediction_axis = "col")
 
-  # Convert iterable matrix back to R sparse matrix
+  # Convert iterable matrix back to C sparse matrix
   rmat <- as(regressed_data, "dgCMatrix")
 
   # Assign regressed out data back to AnnData object
   if (is.null(par$output_layer)) {
-    cat("Using .X as output layer")
+    cat("Using .X as output layer\n")
     adata$X <- rmat
   } else {
-    glue("Using .layers {par$output_layer} as output layer")
-    adata$layers[par$output_layer] <- rmat
+    cat("Using .layers ", par$output_layer, " as output layer\n", sep = "")
+    adata$layers[[par$output_layer]] <- rmat
   }
 
 } else {
-  glue("No obs_keys provided, skipping regression")
+  cat("No obs_keys provided, skipping regression\n")
 }
 
 # Write to output h5mu file
-mdata$write(par$output, compression=par$output_compression)
+mdata$write(par$output, compression = par$output_compression)
