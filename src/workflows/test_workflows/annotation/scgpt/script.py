bea7c9eaed32812db9c384ec4a413f5d08c1d45e
dorien-er <roosen.dorien@gmail.com>
2024-09-06 14:54:02 +0200

    expand test workflow

--- src/workflows/test_workflows/annotation/scgpt/script.py
+++ src/workflows/test_workflows/annotation/scgpt/script.py
@@ -8,7 +8,7 @@ import pytest
 
 ##VIASH START
 par = {
-    "input": "input.h5mu"
+    "input": "work/b7/69cf6938cae28741cdb0c881ce41c8/simple_execution_test.scgpt_celltype_annotation.output.h5mu"
 }
 
 meta = {
@@ -19,14 +19,18 @@ meta = {
 def test_run():
     input_mudata = read_h5mu(par["input"])
     expected_layers = ["binned"]
-    expected_obsm = ["gene_id_tokens", "values_tokenized", "padding_mask"]
+    expected_obsm = ["gene_id_tokens", "values_tokenized", "padding_mask", "X_pca", "X_umap", "bin_edges"]
+    expected_var = ["filter_with_hvg", "id_in_vocab"]
+    expected_obs = ["scgpt_pred", "scgpt_probability"]
 
     assert "rna" in list(input_mudata.mod.keys()), "Input should contain rna modality."
 
-    assert all(key in list(input_mudata.mod["rna"].layers) for key in expected_layers), f"Input mod['rna'] var columns should be: {expected_layers}, found: {input_mudata.mod["rna"].var.keys()}."
-    assert all(key in list(input_mudata.mod["rna"].obsm) for key in expected_obsm), f"Input mod['rna'] obs columns should be: {expected_obsm}, found: {input_mudata.mod["rna"].obs.keys()}."
+    assert all(key in list(input_mudata.mod["rna"].layers) for key in expected_layers), f"Input mod['rna'] var columns should be: {expected_layers}, found: {input_mudata.mod['rna'].var.keys()}."
+    assert all(key in list(input_mudata.mod["rna"].obsm) for key in expected_obsm), f"Input mod['rna'] obs columns should be: {expected_obsm}, found: {input_mudata.mod['rna'].obsm.keys()}."
+    assert all(key in list(input_mudata.mod["rna"].var) for key in expected_var), f"Input mod['rna'] var columns should be: {expected_var}, found: {input_mudata.mod['rna'].var.keys()}."
+    assert all(key in list(input_mudata.mod["rna"].obs) for key in expected_obs), f"Input mod['rna'] obs columns should be: {expected_obs}, found: {input_mudata.mod['rna'].obs.keys()}."
+    assert input_mudata.shape[1] <= par["n_hvg"], f"Input shape should be lower or equal than --n_hvg {par['n_hvg']}, found: {input_mudata.shape[1]}."
 
-    #TODO: check size based on hvg
 
 if __name__ == "__main__":
     HERE_DIR = Path(__file__).resolve().parent
