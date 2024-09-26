458e3fc24e0ddc669e7440a3fa671bdd5fc3fe5f
Vladimir Shitov <35199218+VladimirShitov@users.noreply.github.com>
2024-09-26 09:01:15 +0200

    Add TF-IDF normalization (#870)
    Co-authored-by: DriesSchaumont <5946712+DriesSchaumont@users.noreply.github.com>

---
+++ src/transform/tfidf/script.py
@@ -0,0 +1,64 @@
+import sys
+import mudata
+import muon
+
+## VIASH START
+par = {
+    "input": "work/d9/3adbd080e0de618d44b59b1ec81685/run.output.h5mu",
+    "output": "output.h5mu",
+    "scale_factor": 10000,
+    "modality": "atac",
+    "input_layer": None,
+    "output_layer": None,
+    "output_compression": "gzip",
+    "log_idf": True,
+    "log_tf": True,
+    "log_tfidf": False
+}
+meta = {"name": "tfidf"}
+## VIASH END
+
+sys.path.append(meta["resources_dir"])
+# START TEMPORARY WORKAROUND setup_logger
+# reason: resources aren't available when using Nextflow fusion
+# from setup_logger import setup_logger
+def setup_logger():
+    import logging
+    from sys import stdout
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    console_handler = logging.StreamHandler(stdout)
+    logFormatter = logging.Formatter("%(asctime)s %(levelname)-8s %(message)s")
+    console_handler.setFormatter(logFormatter)
+    logger.addHandler(console_handler)
+
+    return logger
+# END TEMPORARY WORKAROUND setup_logger
+logger = setup_logger()
+
+logger.info("Reading input mudata")
+mdata = mudata.read_h5mu(par["input"])
+
+logger.info(par)
+
+mod = par["modality"]
+logger.info("Performing TF-IDF normalization on modality %s", mod)
+adata = mdata.mod[mod].copy()
+
+muon.atac.pp.tfidf(
+    adata,
+    log_tf=par["log_tf"],
+    log_idf=par["log_idf"],
+    log_tfidf=par["log_tfidf"],
+    scale_factor=par["scale_factor"],
+    inplace=True,
+    copy=False,
+    from_layer=par["input_layer"],
+    to_layer=par["output_layer"],
+)
+
+mdata.mod[mod].layers[par["output_layer"]] = adata.layers[par["output_layer"]]
+
+logger.info("Writing to file")
+mdata.write_h5mu(filename=par["output"], compression=par["output_compression"])
