5be28e0e9fa2feecea055947fb8e6f27855733c8
dorien-er <roosen.dorien@gmail.com>
2024-10-10 17:11:43 +0200

    fix tests

--- src/filter/subset_obsp/test.py
+++ src/filter/subset_obsp/test.py
@@ -8,9 +8,21 @@ meta = {
 }
 ## VIASH END
 
-input_path = f"{meta['resources_dir']}/pbmc_1k_protein_v3_mms.h5mu"
 
-def test_subset_obsp(run_component, tmp_path):
+@pytest.fixture
+def input_h5mu():
+    input = mu.read_h5mu(f"{meta['resources_dir']}/pbmc_1k_protein_v3_mms.h5mu")
+    input.mod["rna"].obs["filter_column"] = "group_2"
+    input.mod["rna"].obs["filter_column"][:50] = "group_1"
+    return input
+
+
+@pytest.fixture
+def input_path(write_mudata_to_file, input_h5mu):
+    return write_mudata_to_file(input_h5mu)
+
+
+def test_subset_obsp(input_path, run_component, tmp_path):
     output_path = tmp_path / "output.h5mu"
 
     # run component
@@ -18,9 +30,9 @@ def test_subset_obsp(run_component, tmp_path):
         "--input", input_path,
         "--output", str(output_path),
         "--input_obsp_key", "distances",
-        "--input_obs_key", "leiden",
-        "--input_obs_value", "1",
-        "--output_obsm_key", "leiden_1"
+        "--input_obs_key", "filter_column",
+        "--input_obs_value", "group_1",
+        "--output_obsm_key", "group_1"
     ])
 
     assert output_path.is_file(), "Output file not found"
@@ -28,8 +40,8 @@ def test_subset_obsp(run_component, tmp_path):
     # check output file
     mu_out = mu.read_h5mu(output_path)
 
-    assert "leiden_1" in mu_out.mod["rna"].obsm, "Output should contain leiden_1 in .obsm"
-    assert mu_out.mod["rna"].obsm["leiden_1"].shape[1] < mu_out.mod["rna"].obsp["distances"].shape[1], "Obsm should only contain a subset of the original obsp matrix"
+    assert "group_1" in mu_out.mod["rna"].obsm, "Output should contain group_1 in .obsm"
+    assert mu_out.mod["rna"].obsm["group_1"].shape[1] == 50, "Obsm should only contain a subset of the original obsp matrix"
 
 
 if __name__ == "__main__":
