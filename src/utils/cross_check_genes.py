4f60b6c9f05a63331c4edd177958857ccf8f72f2
dorien-er <roosen.dorien@gmail.com>
2024-11-29 14:05:19 +0100

    update svm

---
+++ src/utils/cross_check_genes.py
@@ -0,0 +1,21 @@
+from typing import List
+
+def cross_check_genes(query_genes: List[str], reference_genes: List[str], min_gene_overlap: int = 100) -> List[str]:
+    """Cross check the overlap between two lists of genes
+
+    Parameters
+    ----------
+    query_genes : List[str]
+        List of gene names
+    reference_genes : List[str]
+       List of gene names
+
+    Returns
+    -------
+    List[str]
+        List of overlapping genes
+    """
+    common_ens_ids = list(set(reference_genes).intersection(set(query_genes)))
+    assert len(common_ens_ids) >= min_gene_overlap, f"The intersection of genes between the query and reference dataset is too small, expected at least {min_gene_overlap}."
+
+    return common_ens_ids
---
+++ src/utils/set_var_index.py
@@ -0,0 +1,24 @@
+import anndata as ad
+import re
+
+def set_var_index(adata: ad.AnnData, var_name: str | None = None) -> ad.AnnData:
+    """Sanitize gene names and set the index of the .var DataFrame.
+    
+    Parameters
+    ----------
+    adata : AnnData
+        Annotated data object
+    var_name : str | None
+        Name of the column in `adata.var` that contains the gene names, if None, the existing index will be sanitized but not replaced.
+
+    Returns
+    -------
+    AnnData
+        Copy of `adata` with sanitized and replaced index
+    """
+    if var_name:
+        adata.var.index = [re.sub("\\.[0-9]+$", "", s) for s in adata.var[var_name]]
+    else:
+        adata.var.index = [re.sub("\\.[0-9]+$", "", s) for s in adata.var.index]
+    return adata
+
