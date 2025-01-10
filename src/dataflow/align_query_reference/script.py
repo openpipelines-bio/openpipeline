6d8e249dffbab988e1d49bcd84dbede3258fb31a
dorien-er <roosen.dorien@gmail.com>
2025-01-10 17:06:47 +0100

    wip

---
+++ src/dataflow/align_query_reference/script.py
@@ -0,0 +1,166 @@
+import sys
+import mudata as mu
+
+
+## VIASH START
+par = {
+    "input": "resources_test/pbmc_1k_protein_v3/pbmc_1k_protein_v3_filtered_feature_bc_matrix.h5mu",
+    "modality": "rna",
+    "input_layer": None,
+    "input_var_gene_names": None,
+    "input_obs_batch": "sample_id",
+    "input_obs_labels": None,
+    "reference": "resources_test/annotation_test_data/TS_Blood_filtered.h5mu",
+    "reference_layer": None,
+    "reference_var_gene_names": "ensemblid",
+    "reference_obs_batch": "sample_id",
+    "reference_obs_label": "cell_ontology_class",
+    "output_query": "pbmc_1k_protein_v3_filtered_feature_bc_matrix_aligned.h5mu",
+    "output_reference": "TS_Blood_filtered_aligned.h5mu",
+    "output_layer": "_counts",
+    "output_var_gene_names": "_gene_names",
+    "output_obs_batch": "_sample_id",
+    "output_obs_label": "_cell_type",
+    "input_reference_gene_overlap": 100,
+    "unkown_celltype_label": "Unknown",
+    "overwrite_existing_key": False,
+}
+
+meta = {"resources_dir": "src/utils"}
+## VIASH END
+
+sys.path.append(meta["resources_dir"])
+from setup_logger import setup_logger
+from cross_check_genes import cross_check_genes
+
+logger = setup_logger()
+
+
+def copy_layer(adata, input_layer, output_layer, overwrite=False):
+    if output_layer not in adata.layers.keys():
+        logger.info(f"Copying {input_layer} to {output_layer}")
+        adata.layers[output_layer] = (
+            adata.layers[input_layer].copy() if input_layer else adata.X.copy()
+        )
+
+    else:
+        if not overwrite:
+            raise ValueError(
+                f"Layer {output_layer} already exists. Data can not be copied."
+            )
+
+        logger.warning(f"Layer {output_layer} already exists. Overwriting data.")
+        adata.layers[output_layer] = (
+            adata.layers[input_layer].copy() if input_layer else adata.X.copy()
+        )
+
+    return adata
+
+
+def copy_obs(adata, input_obs_key, output_obs_key, overwrite=False, fill_value=None):
+    if output_obs_key not in adata.layers.keys():
+        logger.info(f"Copying {input_obs_key} to {output_obs_key}")
+        adata.layers[output_obs_key] = (
+            adata.layers[input_obs_key].copy() if input_obs_key else fill_value
+        )
+
+    else:
+        if not overwrite:
+            raise ValueError(
+                f"Layer {output_obs_key} already exists. Data can not be copied."
+            )
+
+        logger.warning(f"Layer {output_obs_key} already exists. Overwriting data.")
+        adata.layers[output_obs_key] = (
+            adata.layers[input_obs_key].copy() if input_obs_key else fill_value
+        )
+
+    return adata
+
+
+def copy_and_sanitize_var_gene_names(
+    adata, input_var_key, output_var_key, overwrite=False
+):
+    pass
+
+
+def main():
+    logger.info("Reading query and reference data")
+    input_mudata = mu.read_h5mu(par["input"])
+    input_adata = input_mudata.mod[par["modality"]]
+    input_modality = input_adata.copy()
+
+    reference_mudata = mu.read_h5mu(par["reference"])
+    reference_adata = reference_mudata.mod[par["modality"]]
+    reference_modality = reference_adata.copy()
+
+    logger.info("Aligning layers")
+    input_modality = copy_layer(
+        input_modality,
+        par["input_layer"],
+        par["output_layer"],
+        overwrite=par["overwrite_existing_key"],
+    )
+    reference_modality = copy_layer(
+        reference_modality,
+        par["reference_layer"],
+        par["output_layer"],
+        overwrite=par["overwrite_existing_key"],
+    )
+
+    logger.info("Aligning batch labels")
+    input_modality = copy_obs(
+        input_modality,
+        par["input_obs_batch"],
+        par["output_obs_batch"],
+        par["overwrite_existing_key"],
+        fill_value=None,
+    )
+    reference_modality = copy_obs(
+        reference_modality,
+        par["reference_obs_batch"],
+        par["output_obs_batch"],
+        overwrite=par["overwrite_existing_key"],
+        fill_value=None,
+    )
+
+    logger.info("Aligning cell type labels")
+    input_modality = copy_obs(
+        input_modality,
+        par["input_obs_celltype_label"],
+        par["output_obs_celltype_label"],
+        par["overwrite_existing_key"],
+        fill_vallue=par["unkown_celltype_label"],
+    )
+    reference_modality = copy_obs(
+        reference_modality,
+        par["reference_obs_celltype_label"],
+        par["output_obs_celltype_label"],
+        overwrite=par["overwrite_existing_key"],
+        fill_vallue=par["unkown_celltype_label"],
+    )
+
+    logger.info("Aligning and sanitizing gene names")
+    input_modality = copy_and_sanitize_var_gene_names(
+        input_modality,
+        par["input_var_gene_names"],
+        par["output_var_gene_names"],
+        overwrite=par["overwrite_existing_key"],
+    )
+    reference_modality = copy_and_sanitize_var_gene_names(
+        reference_modality,
+        par["reference_var_gene_names"],
+        par["output_var_gene_names"],
+        overwrite=par["overwrite_existing_key"],
+    )
+
+    cross_check_genes()
+
+    logger.info("Writing output data")
+    input_mudata.mod[par["modality"]] = input_modality
+    input_mudata.write_h5mu(par["output_query"], compression=par["output_compression"])
+
+    reference_mudata.mod[par["modality"]] = reference_modality
+    reference_mudata.write_h5mu(
+        par["output_reference"], compression=par["output_compression"]
+    )
