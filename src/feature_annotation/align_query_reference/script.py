2b7cc8b1f514c5a2d42c21249445a38274568558
Dorien <41797896+dorien-er@users.noreply.github.com>
2025-01-20 11:25:56 +0100

    add common vars to aligned datasets (#958)
    * add common vars to aligned datasets
    
    * update changelog
    
    * fix test

--- src/feature_annotation/align_query_reference/script.py
+++ src/feature_annotation/align_query_reference/script.py
@@ -28,7 +28,8 @@ par = {
     "overwrite_existing_key": False,
     "output_compression": None,
     "preserve_var_index": False,
-    "output_var_index": "ori_var_index",
+    "output_var_index": "_ori_var_index",
+    "output_var_common_genes": "_common_vars",
 }
 
 meta = {"resources_dir": "src/utils"}
@@ -214,11 +215,19 @@ def main():
 
     # Cross check genes
     logger.info("### Cross checking genes")
-    cross_check_genes(
+    common_vars = cross_check_genes(
         input_modality.var[par["output_var_gene_names"]],
         reference_modality.var[par["output_var_gene_names"]],
         min_gene_overlap=par["input_reference_gene_overlap"],
     )
+
+    # Add common vars to the output
+    input_modality.var[par["output_var_common_genes"]] = input_modality.var[
+        par["output_var_gene_names"]
+    ].isin(common_vars)
+    reference_modality.var[par["output_var_common_genes"]] = reference_modality.var[
+        par["output_var_gene_names"]
+    ].isin(common_vars)
 
     # Adding an id to the query and reference datasets
     logger.info("### Adding an id to the query and reference datasets")
