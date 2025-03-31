65d2937cedad82ed956ca0076a087ccb0d9cb797
DriesSchaumont <5946712+DriesSchaumont@users.noreply.github.com>
2025-03-31 16:38:20 +0000

    deploy: 0c4733b69044a6a40b2ff3f6def4b77d17d16dc4

--- src/workflows/test_workflows/ingestion/cellranger_postprocessing/script.py
+++ src/workflows/test_workflows/ingestion/cellranger_postprocessing/script.py
@@ -1,8 +1,5 @@
 from mudata import read_h5mu
 from openpipeline_testutils.asserters import assert_annotation_objects_equal
-from pathlib import Path
-import shutil
-import os
 import sys
 import pytest
 
@@ -38,10 +35,4 @@ def test_run():
 
 
 if __name__ == "__main__":
-    HERE_DIR = Path(__file__).resolve().parent
-    from importlib import resources
-    shutil.copyfile(
-        resources.files("openpipeline_testutils").joinpath("conftest.py"),
-        os.path.join(HERE_DIR, "conftest.py"),
-    )
-    sys.exit(pytest.main(["--import-mode=importlib"]))
+    sys.exit(pytest.main([__file__, "--import-mode=importlib"]))
