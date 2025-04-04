49d7059fcd635d33df07669351b056aede92702d
DriesSchaumont <5946712+DriesSchaumont@users.noreply.github.com>
2025-04-04 10:13:54 +0000

    deploy: 96ec861a0cc4c420c1d741153833f1531bceca4d

---
+++ src/workflows/multiomics/split_h5mu/integration_test.sh
@@ -0,0 +1,13 @@
+# get the root of the directory
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+# ensure that the command below is run from the root of the repository
+cd "$REPO_ROOT"
+
+nextflow \
+  run . \
+  -main-script src/workflows/multiomics/split_h5mu/test.nf \
+  -entry test_wf \
+  -profile docker,no_publish \
+  -c src/workflows/utils/labels_ci.config \
+  -c src/workflows/utils/integration_tests.config
