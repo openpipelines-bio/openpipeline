fcfc4537a7f1043bee2555e751478dac2fd36bbe
DriesSchaumont <5946712+DriesSchaumont@users.noreply.github.com>
2025-04-25 07:18:07 +0000

    deploy: 06b15840d6ba55353fa65bd623100e055a0a7e98

---
+++ target/nextflow/annotate/random_forest_annotation/subset_vars.py
@@ -0,0 +1,21 @@
+def subset_vars(adata, subset_col):
+    """Subset AnnData object on highly variable genes
+
+    Parameters
+    ----------
+    adata : AnnData
+        Annotated data object
+    subset_col : str
+        Name of the boolean column in `adata.var` that contains the information if features should be used or not
+
+    Returns
+    -------
+    AnnData
+        Copy of `adata` with subsetted features
+    """
+    if subset_col not in adata.var.columns:
+        raise ValueError(
+            f"Requested to use .var column '{subset_col}' as a selection of genes, but the column is not available."
+        )
+
+    return adata[:, adata.var[subset_col]].copy()
