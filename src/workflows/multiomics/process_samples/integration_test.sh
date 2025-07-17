7a0d744d1312947f8d9d04807c8856d6b5972417
dorien-er <roosen.dorien@gmail.com>
2025-07-17 08:55:12 +0200

    wip

--- src/workflows/multiomics/process_samples/integration_test.sh
+++ src/workflows/multiomics/process_samples/integration_test.sh
@@ -8,91 +8,91 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 # ensure that the command below is run from the root of the repository
 cd "$REPO_ROOT"
 
-# nextflow \
-#   run . \
-#   -main-script src/workflows/multiomics/process_samples/test.nf \
-#   -entry test_wf \
-#   -profile docker,no_publish \
-#   -c src/workflows/utils/labels_ci.config \
-#   -c src/workflows/utils/integration_tests.config
+nextflow \
+  run . \
+  -main-script src/workflows/multiomics/process_samples/test.nf \
+  -entry test_wf \
+  -profile docker,no_publish \
+  -c src/workflows/utils/labels_ci.config \
+  -c src/workflows/utils/integration_tests.config
 
-# # Same as above but with remote yaml file.
-# nextflow \
-#   run . \
-#   -main-script src/workflows/multiomics/process_samples/test.nf \
-#   -entry test_wf \
-#   -profile docker,no_publish \
-#   -c src/workflows/utils/labels_ci.config \
-#   -c src/workflows/utils/integration_tests.config \
-#   --param_list s3://openpipelines-data/remote_param_list/test_param_list.yaml
+# Same as above but with remote yaml file.
+nextflow \
+  run . \
+  -main-script src/workflows/multiomics/process_samples/test.nf \
+  -entry test_wf \
+  -profile docker,no_publish \
+  -c src/workflows/utils/labels_ci.config \
+  -c src/workflows/utils/integration_tests.config \
+  --param_list s3://openpipelines-data/remote_param_list/test_param_list.yaml
 
-# # Same as above but with remote json file.
-# nextflow \
-#   run . \
-#   -main-script src/workflows/multiomics/process_samples/test.nf \
-#   -entry test_wf \
-#   -profile docker,no_publish \
-#   -c src/workflows/utils/labels_ci.config \
-#   -c src/workflows/utils/integration_tests.config \
-#   --param_list s3://openpipelines-data/remote_param_list/test_param_list.json
+# Same as above but with remote json file.
+nextflow \
+  run . \
+  -main-script src/workflows/multiomics/process_samples/test.nf \
+  -entry test_wf \
+  -profile docker,no_publish \
+  -c src/workflows/utils/labels_ci.config \
+  -c src/workflows/utils/integration_tests.config \
+  --param_list s3://openpipelines-data/remote_param_list/test_param_list.json
 
-# # Same as above but with remote csv file.
-# nextflow \
-#   run . \
-#   -main-script src/workflows/multiomics/process_samples/test.nf \
-#   -entry test_wf \
-#   -profile docker,no_publish \
-#   -c src/workflows/utils/labels_ci.config \
-#   -c src/workflows/utils/integration_tests.config \
-#   --param_list s3://openpipelines-data/remote_param_list/test_param_list.csv
+# Same as above but with remote csv file.
+nextflow \
+  run . \
+  -main-script src/workflows/multiomics/process_samples/test.nf \
+  -entry test_wf \
+  -profile docker,no_publish \
+  -c src/workflows/utils/labels_ci.config \
+  -c src/workflows/utils/integration_tests.config \
+  --param_list s3://openpipelines-data/remote_param_list/test_param_list.csv
 
-# nextflow \
-#   run . \
-#   -main-script src/workflows/multiomics/process_samples/test.nf \
-#   -profile docker,no_publish \
-#   -entry test_wf2 \
-#   -c src/workflows/utils/labels_ci.config \
-#   -c src/workflows/utils/integration_tests.config
+nextflow \
+  run . \
+  -main-script src/workflows/multiomics/process_samples/test.nf \
+  -profile docker,no_publish \
+  -entry test_wf2 \
+  -c src/workflows/utils/labels_ci.config \
+  -c src/workflows/utils/integration_tests.config
 
-# nextflow \
-#   run . \
-#   -main-script src/workflows/multiomics/process_samples/test.nf \
-#   -entry test_wf3 \
-#   -profile docker,no_publish \
-#   -c src/workflows/utils/labels_ci.config \
-#   -c src/workflows/utils/integration_tests.config
+nextflow \
+  run . \
+  -main-script src/workflows/multiomics/process_samples/test.nf \
+  -entry test_wf3 \
+  -profile docker,no_publish \
+  -c src/workflows/utils/labels_ci.config \
+  -c src/workflows/utils/integration_tests.config
 
-# nextflow \
-#   run . \
-#   -main-script src/workflows/multiomics/process_samples/test.nf \
-#   -entry test_wf4 \
-#   -profile docker,no_publish \
-#   -c src/workflows/utils/labels_ci.config \
-#   -c src/workflows/utils/integration_tests.config
+nextflow \
+  run . \
+  -main-script src/workflows/multiomics/process_samples/test.nf \
+  -entry test_wf4 \
+  -profile docker,no_publish \
+  -c src/workflows/utils/labels_ci.config \
+  -c src/workflows/utils/integration_tests.config
 
-# nextflow \
-#   run . \
-#   -main-script src/workflows/multiomics/process_samples/test.nf \
-#   -entry test_wf5 \
-#   -profile docker,no_publish \
-#   -c src/workflows/utils/labels_ci.config \
-#   -c src/workflows/utils/integration_tests.config
+nextflow \
+  run . \
+  -main-script src/workflows/multiomics/process_samples/test.nf \
+  -entry test_wf5 \
+  -profile docker,no_publish \
+  -c src/workflows/utils/labels_ci.config \
+  -c src/workflows/utils/integration_tests.config
 
-# nextflow \
-#   run . \
-#   -main-script src/workflows/multiomics/process_samples/test.nf \
-#   -entry test_wf6 \
-#   -profile docker,no_publish \
-#   -c src/workflows/utils/labels_ci.config \
-#   -c src/workflows/utils/integration_tests.config
+nextflow \
+  run . \
+  -main-script src/workflows/multiomics/process_samples/test.nf \
+  -entry test_wf6 \
+  -profile docker,no_publish \
+  -c src/workflows/utils/labels_ci.config \
+  -c src/workflows/utils/integration_tests.config
 
-# nextflow \
-#   run . \
-#   -main-script src/workflows/multiomics/process_samples/test.nf \
-#   -entry test_wf7 \
-#   -profile docker,no_publish \
-#   -c src/workflows/utils/labels_ci.config \
-#   -c src/workflows/utils/integration_tests.config
+nextflow \
+  run . \
+  -main-script src/workflows/multiomics/process_samples/test.nf \
+  -entry test_wf7 \
+  -profile docker,no_publish \
+  -c src/workflows/utils/labels_ci.config \
+  -c src/workflows/utils/integration_tests.config
 
 nextflow \
   run . \
