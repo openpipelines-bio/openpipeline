a3da42b13e0cecebfab8c833b38faa71583cdd9e
dorien-er <roosen.dorien@gmail.com>
2025-10-21 13:48:13 +0200

    linting

--- src/integrate/totalvi/test.py
+++ src/integrate/totalvi/test.py
@@ -1,6 +1,5 @@
-import sys
-import pytest
 import mudata
+from pathlib import Path
 
 ## VIASH START
 meta = {
@@ -10,6 +9,7 @@ meta = {
 ## VIASH END
 
 input_file = f"{meta['resources_dir']}/pbmc_1k_protein_v3_mms.h5mu"
+
 
 def test_simple_execution(run_component, tmp_path):
     output_path = tmp_path / "output.h5mu"
@@ -33,23 +33,23 @@ def test_simple_execution(run_component, tmp_path):
     assert output_path.is_file()
     assert Path(output_path).is_dir()
     assert Path(f"{output_path}/model.pt").is_file()
-    
+
     output_data = mudata.read_h5mu(output_path)
     input_data = mudata.read_h5mu(input_file)
-    
+
     assert output_data.mod["rna"].n_obs == input_data.mod["rna"].n_obs, (
-      "Number of observations in RNA modality does not match input data."
+        "Number of observations in RNA modality does not match input data."
     )
     assert output_data.mod["rna"].n_vars == input_data.mod["rna"].n_vars, (
-      "Number of variables in RNA modality does not match input data."
+        "Number of variables in RNA modality does not match input data."
     )
     assert output_data.mod["prot"].n_obs == input_data.mod["prot"].n_obs, (
-      "Number of observations in Protein modality does not match input data."
+        "Number of observations in Protein modality does not match input data."
     )
     assert output_data.mod["prot"].n_vars == input_data.mod["prot"].n_vars, (
-      "Number of variables in Protein modality does not match input data."
+        "Number of variables in Protein modality does not match input data."
     )
-    
+
     assert "X_integrated_totalvi" in output_data.mod["rna"].obsm
     assert output_data.mod["rna"].obsm["X_integrated_totalvi"].dtype == "f"
     assert output_data.mod["rna"].obsm["X_integrated_totalvi"].shape[1] == 20
@@ -80,30 +80,30 @@ def test_covariates(run_component, tmp_path):
     )
 
     assert output_path.is_file()
-    
+
     output_data = mudata.read_h5mu(output_path)
     input_data = mudata.read_h5mu(input_file)
-    
+
     assert output_data.mod["rna"].n_obs == input_data.mod["rna"].n_obs, (
-      "Number of observations in RNA modality does not match input data."
+        "Number of observations in RNA modality does not match input data."
     )
     assert output_data.mod["rna"].n_vars == input_data.mod["rna"].n_vars, (
-      "Number of variables in RNA modality does not match input data."
+        "Number of variables in RNA modality does not match input data."
     )
     assert output_data.mod["prot"].n_obs == input_data.mod["prot"].n_obs, (
-      "Number of observations in Protein modality does not match input data."
+        "Number of observations in Protein modality does not match input data."
     )
     assert output_data.mod["prot"].n_vars == input_data.mod["prot"].n_vars, (
-      "Number of variables in Protein modality does not match input data."
+        "Number of variables in Protein modality does not match input data."
     )
-    
+
     assert "X_integrated_totalvi" in output_data.mod["rna"].obsm
     assert output_data.mod["rna"].obsm["X_integrated_totalvi"].dtype == "f"
     assert "X_totalvi_normalized_rna" in output_data.mod["rna"].obsm
     assert output_data.mod["rna"].obsm["X_totalvi_normalized_rna"].dtype == "f"
     assert "X_totalvi_normalized_protein" in output_data.mod["prot"].obsm
     assert output_data.mod["prot"].obsm["X_totalvi_normalized_protein"].dtype == "f"
-    
+
 
 def test_parameters(run_component, tmp_path):
     output_path = tmp_path / "output.h5mu"
@@ -138,9 +138,9 @@ def test_parameters(run_component, tmp_path):
     )
 
     assert output_path.is_file()
-    
+
     output_data = mudata.read_h5mu(output_path)
-    
+
     assert "test_obsm_output" in output_data.mod["rna"].obsm
     assert output_data.mod["rna"].obsm["test_obsm_output"].dtype == "f"
     assert output_data.mod["rna"].obsm["test_obsm_output"].shape[1] == 10
@@ -148,4 +148,3 @@ def test_parameters(run_component, tmp_path):
     assert output_data.mod["rna"].obsm["test_rna"].dtype == "f"
     assert "test_prot" in output_data.mod["prot"].obsm
     assert output_data.mod["prot"].obsm["test_prot"].dtype == "f"
-    
