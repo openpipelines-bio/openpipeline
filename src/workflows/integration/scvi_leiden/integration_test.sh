0db41f217899aa4f5d50434028c985cd9cf581a0
Dries Schaumont <5946712+DriesSchaumont@users.noreply.github.com>
2025-10-23 13:34:13 +0000

    Undo change to integration_test script

--- src/workflows/integration/scvi_leiden/integration_test.sh
+++ src/workflows/integration/scvi_leiden/integration_test.sh
@@ -6,13 +6,13 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 # ensure that the command below is run from the root of the repository
 cd "$REPO_ROOT"
 
-# nextflow \
-#   run . \
-#   -main-script src/workflows/integration/scvi_leiden/test.nf \
-#   -entry test_wf \
-#   -profile docker,no_publish \
-#   -c src/workflows/utils/labels_ci.config \
-#   -c src/workflows/utils/integration_tests.config
+nextflow \
+  run . \
+  -main-script src/workflows/integration/scvi_leiden/test.nf \
+  -entry test_wf \
+  -profile docker,no_publish \
+  -c src/workflows/utils/labels_ci.config \
+  -c src/workflows/utils/integration_tests.config
 
 
 nextflow \
