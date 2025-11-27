05fe5ec7454b8632eb75bd9f6b4de4b878fccf7c
dorien-er <dorienroosen@openpipelines-scvi-tools-vm.europe-west4-a.c.di-compute-engine.internal>
2025-11-27 09:13:29 +0000

    expand tests

--- src/workflows/test_workflows/embedding_model/scanvi_model/script.py
+++ src/workflows/test_workflows/embedding_model/scanvi_model/script.py
@@ -2,6 +2,7 @@ from mudata import read_h5mu
 import sys
 import pytest
 import pandas
+import scvi
 
 ##VIASH START
 par = {"input": "harmony_knn/output.h5mu"}
@@ -26,7 +27,14 @@ def test_run():
     assert pandas.api.types.is_float_dtype(
         input_mudata.mod["rna"].obs["scanvi_probabilities"].dtype
     )
-
+    
+    registry = scvi.model.base.BaseModelClass.load_registry(par["model"])
+    model_args = registry["setup_args"]
+    assert registry["model_name"] == "SCANVI", f"Expected model to be SCANVI, found: {registry['model_name']}."
+    assert model_args["unlabeled_category"] == "Unknown", f"Expected unlabeled_category to be 'Unkown', found: {model_args['unlabeled_category']}."
+    assert model_args["labels_key"] == "cell_type", f"Expected labels_key to be 'cell_type', found: {model_args['labels_key']}."
+    assert model_args["batch_key"] == "donor_assay", f"Expected batch_key to be 'donor_assay', found: {model_args['batch_key']}."
+    assert model_args["categorical_covariate_keys"] == par["obs_covariate"], f"Expected categorical_covariate_keys to be {par["obs_covariate"]}, found: {model_args['categorical_covariate_keys']}."
 
 if __name__ == "__main__":
     sys.exit(pytest.main([__file__, "--import-mode=importlib"]))
