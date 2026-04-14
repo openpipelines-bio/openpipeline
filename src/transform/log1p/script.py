ef9f2e1c228a112063df89a33e06df2186c260c1
Dries Schaumont <5946712+DriesSchaumont@users.noreply.github.com>
2026-04-14 11:58:13 +0200

    transform/log1p: avoid creating extra AnnData object. (#1159)

--- src/transform/log1p/script.py
+++ src/transform/log1p/script.py
@@ -1,6 +1,5 @@
 import scanpy as sc
 import mudata as mu
-import anndata as ad
 import sys
 
 ## VIASH START
@@ -26,23 +25,20 @@ assert data.var_names.is_unique, "Expected var_names to be unique."
 
 logger.info("Performing log transformation")
 # Make our own copy with not a lot of data
-# this avoid excessive memory usage and accidental overwrites
+# this avoid excessive memory usage
 input_layer = data.layers[par["input_layer"]] if par["input_layer"] else data.X
-data_for_scanpy = ad.AnnData(X=input_layer.copy())
-sc.pp.log1p(
-    data_for_scanpy,
-    base=par["base"],
-    layer=None,  # use X
-    copy=False,
-)  # allow overwrites in the copy that was made
 
-# Scanpy will overwrite the input layer.
-# So fetch input layer from the copy and use it to populate the output slot
+transformed_matrix = sc.pp.log1p(
+    input_layer,
+    base=par["base"],
+    copy=True,  # Avoid accidental overwrites
+)
+
 if par["output_layer"]:
-    data.layers[par["output_layer"]] = data_for_scanpy.X
+    data.layers[par["output_layer"]] = transformed_matrix
 else:
-    data.X = data_for_scanpy.X
-data.uns["log1p"] = data_for_scanpy.uns["log1p"].copy()
+    data.X = transformed_matrix
+data.uns["log1p"] = {"base": par["base"]}
 
 logger.info("Writing to file %s", par["output"])
 write_h5ad_to_h5mu_with_compression(
